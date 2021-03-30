52c9ceef19b6e75eb99fcec369b4d6cc9547d819
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-03-30 20:44:58 -0300

    Lint Review 2

--- src/challenges.js
+++ src/challenges.js
@@ -16,21 +16,21 @@ function splitSentence(sentence) {
 // Desafio 4
 function concatName(arrayStrings) {
   let posicao1 = arrayStrings[0];
-  let posicao2 = arrayStrings[arrayStrings.length - 1] ;
+  let posicao2 = arrayStrings[arrayStrings.length - 1];
   return `${posicao2}, ${posicao1}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (3 * wins + ties) ;
+  return (3 * wins + ties);
 }
 
 // Desafio 6
 function highestCount(arrayValores) {
   let countHigh = 0;
-  function highestNumber(arrayValores) {
-    return Math.max.apply(null, arrayValores);
-  } for (let index = 0;index < arrayValores.length;index += 1) {
+  function highestNumber(arrayHigh) {
+    return Math.max.apply(null, arrayHigh);
+  } for (let index = 0; index < arrayValores.length; index += 1) {
     if (highestNumber(arrayValores) === arrayValores[index]) {
       countHigh += 1;
     }
@@ -39,29 +39,28 @@ function highestCount(arrayValores) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (Math.abs(Math.abs(mouse) - Math.abs(cat1)) > Math.abs(Math.abs(mouse) - Math.abs(cat2))) {
+  let dist1 = Math.abs(Math.abs(mouse) - Math.abs(cat1));
+  let dist2 = Math.abs(Math.abs(mouse) - Math.abs(cat2));
+  if (dist1 > dist2) {
     return 'cat2';
-  } else if (Math.abs(Math.abs(mouse) - Math.abs(cat1)) === Math.abs(Math.abs(mouse) - Math.abs(cat2))) {
+  } else if (dist1 === dist2) {
     return 'os gatos trombam e o rato foge';
-  } else {
-    return 'cat1';
-  }
+  } return 'cat1';
 }
 
 // Desafio 8
 function fizzBuzz(numeros) {
   let arrayFizzBuzz = [];
-  for (let index = 0; index < numeros.length; index+= 1) {
+  for (let index = 0; index < numeros.length; index += 1) {
     if (((numeros[index] % 3) === 0) && ((numeros[index] % 5) === 0)) {
       arrayFizzBuzz.push('fizzBuzz');
     } else if (numeros[index] % 3 === 0) {
       arrayFizzBuzz.push('fizz');
     } else if (numeros[index] % 5 === 0) {
       arrayFizzBuzz.push('buzz');
-    } else {
-      arrayFizzBuzz.push('bug!');
     }
-  } return arrayFizzBuzz;
+      arrayFizzBuzz.push('bug!');
+    } return arrayFizzBuzz;
 }
 
 // Desafio 9
