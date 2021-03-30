b2a9e05fbda4764974e17c1772bd297e6ae9f393
YanParoni <yan.almeida.paroni@gmail.com>
2021-03-30 19:32:35 -0300

    Challenge1 completo

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,140 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  // como são testados dois valores booleanos e o operador && ja retorna
+  // uma resposta de false ou true apenas retornar os dois ja faz a função
+  return value1 && value2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(width, height) {
+  // calculo de um triangulo qualquer que sabemos a altura e largura
+  return width * height / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  // Quando for feita a chamada da funcão ja vai ser passada uma string então
+  // pela tipagem dinamica a string chama o metodo split que faz a separação
+  return frase.split();
 }
 
 // Desafio 4
-function concatName() {
+function concatName(list = [], anotherList = []) {
   // seu código aqui
+  anotherList.push(list[list.length - 1]);
+  anotherList.push(list[list.length - 2]);
+  return anotherList;
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  // a função recebera dois numeros basta multiplicar pelo peso depois soma-las
+  wins *= 3;
+  ties *= 1;
+  return wins + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+function highestCount(list = []) {
+  let number = 0;
+  let count = 0;
+  for (let index = 0; index <= list.length; index += 1) {
+    if (number < list[index]) number = list[index];}
+  for (let o = 1; o <= list.length; o += 1) {
+    if (list[o] === number) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(x, y, z) {
+  let cat1 = Math.abs(z - x);
+  let cat2 = Math.abs(z - y);
+  if (cat1 > cat2) {
+    return 'cat2';
+  } if (cat1 < cat2) {
+    return 'cat1';
+  }
+  return 'os dois gatos se trombam';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// eslint-disable-next-line complexity
+function fizzBuzz(list = []) {
+  for (let index = 0; index < list.length; index += 1) {
+    if (list[index] % 3 === 0 && list[index] % 5 === 0) {
+      console.log('fizzBuzz');
+    } if (list[index] % 3 === 0) {
+      console.log('fizz');
+    } if (list[index] % 5 === 0) {
+      console.log('buzz');
+    }
+    console.log('bug');
+  }
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
+function encode(frase) {
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index].toLowerCase() === string[index]) {
+      switch (string[index]) {
+      case 'a':
+        encodedMessage += '1';
+        break;
+      case 'e':
+        encodedMessage += '2';
+        break;
+      case 'i':
+        encodedMessage += '3';
+        break;
+      case 'o':
+        encodedMessage += '4';
+        break;
+      case 'u':
+        encodedMessage += '5';
+        break;
+      default:
+        encodedMessage += string[index];
+      }
+    } else {
+      encodedMessage += message[index];
+      encodedMessage += string[index];
+    }
+  }
+  return encodedMessage;
+
+
 function decode() {
-  // seu código aqui
+  // seu código 
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index].toLowerCase() === string[index]) {
+      switch (string[index]) {
+      case '1':
+        decodedMessage += 'a';
+        break;
+      case '2':
+        decodedMessage += 'e';
+        break;
+      case '3':
+        decodedMessage += 'i';
+        break;
+      case '4':
+        decodedMessage += 'o';
+        break;
+      case '5':
+        decodedMessage += 'u';
+        break;
+      default:
+        decodedMessage += string[index];
+      }
+    } else {
+      decodedMessage += message[index];
+      decodedMessage += string[index];
+    }
+  }
+  return decodedMessage;
 }
 
 module.exports = {
