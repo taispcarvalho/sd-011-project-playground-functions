c23b8bb7e99255da6ca394d6caa076bd9dc1f0ef
Ruben Lopes <rubenssouss@hotmail.com>
2021-03-30 15:58:31 -0300

    Desafio 2 completed

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,11 @@ function compareTrue(val1, val2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  
+  let area = (base * height) / 2;
+  
+  return area;
 }
 
 // Desafio 3
