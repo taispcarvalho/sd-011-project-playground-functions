1fb5a37ed2a253ad10c16233f05475e75038a2a2
Júlio Mota <juliomotacampos@gmail.com>
2021-03-30 19:34:22 -0300

    Ajuste exercício 10 adicionado função sortByTech

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,6 +14,18 @@ function createObject(techs, name) {
   return arrayObject;
 }
 
+function sortByTech(arrayObject) {
+  arrayObject.sort(function(a, b) {
+    if(a.tech < b.tech) {
+      return -1;
+    } else {
+      return true;
+    }
+  });
+
+  return arrayObject;
+}
+
 function techList(techs, name) {
   let arrayObject = [];
 
@@ -23,8 +35,14 @@ function techList(techs, name) {
     arrayObject = createObject(techs, name);
   }
 
+  arrayObject = sortByTech(arrayObject);
+
   return arrayObject;
 }
+
+// let tech1 = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// let name1 = 'Lucas';
+// console.log(techList(tech1, name1));
 
 // Desafio 11
 function generatePhoneNumber() {
