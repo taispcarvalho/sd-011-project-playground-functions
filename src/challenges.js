3886a152b49c28016c4250c7b5f7b830b01bbdbe
Antonio Malato <malatoneto@gmail.com>
2021-03-30 14:58:23 -0300

    Desafio 2 - Concluido

--- src/challenges.js
+++ src/challenges.js
@@ -9,9 +9,10 @@ function compareTrue(value1, value2) {
 console.log(compareTrue(true, true))
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2
 }
+console.log(calcArea(51, 1))
 
 // Desafio 3
 function splitSentence() {
