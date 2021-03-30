d8360b345861a167bfd7a99758d26af9a83759aa
lucasSCsantos <luc.cristovam10@gmail.com>
2021-03-30 20:02:41 -0300

    tenth-function-5

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,10 +14,10 @@ techs = techs.sort()
     
     lista[i] = { tech: techs[i], name: name}
 
-  }
+  };
 
   return lista;
-  }
+  };
   
 }
 
@@ -26,19 +26,55 @@ console.log(techList([], "Lucas"));
 // Desafio 11
 function generatePhoneNumber(numbers) {
 
-
   let phoneNumber = ''
-  for(let i = 0; i < 2; i += 1){
-    if(i === 0){
-      phoneNumber += '('
-      phoneNumber += numbers[i]
-    }else{
-      phoneNumber += numbers[i]
-      phoneNumber += ')'
-    }
-  }
+  let count = 0;
+  let canBePhoneNumber = true;
+  
+    for(let i = 0; i < 11; i += 1){
 
+      for(let number of numbers){
+        let count = 0;
+        if(number === numbers[i]){
+          count += 1
+        }
+      }
+
+      if(numbers[i] > 9 || numbers[i] < 0 || count === 3){
+        canBePhoneNumber = false
+      }
+
+      if(i === 0){
+        phoneNumber += '('
+        phoneNumber += numbers[i]
+      }else if(i === 1){
+        phoneNumber += numbers[i]
+        phoneNumber += ') '
+      }else if(i === 6){
+        phoneNumber += numbers[i]
+        phoneNumber += '-'
+      }else{
+        phoneNumber += numbers[i]
+      };
+  
+    }
+
+    if(numbers.length === 11 && canBePhoneNumber === true){
+      return phoneNumber
+    }else if(numbers.length !== 11){
+      return "Array com tamanho incorreto."
+    }else if(canBePhoneNumber === false){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+
+  
+
+ 
+  
 }
+
+
+
+console.log(generatePhoneNumber([10,2,3,4,5,6,7,8,9,1,0]));
 
 // Desafio 12
 function triangleCheck() {
@@ -50,9 +86,10 @@ function hydrate() {
   // seu código aqui
 }
 
-module.exports = {
-  generatePhoneNumber,
-  techList,
-  hydrate,
-  triangleCheck,
-};
+    module.exports = {
+      generatePhoneNumber,
+      techList,
+      hydrate,
+      triangleCheck,
+
+    }
