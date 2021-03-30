f9b8d9b1b4911504d0f8bf5aaf9ccbed508f76e1
Diego Figueiredo <8kball@gmail.com>
2021-03-30 15:50:45 -0300

    Finalizando parte 1

--- src/challenges.js
+++ src/challenges.js
@@ -17,7 +17,7 @@ function splitSentence(sentence) {
 function concatName(arr) {
   let firstValue = arr[0];
   let lastValue = arr[arr.length - 1];
-  return [firstValue, lastValue];
+  return `${firstValue}, ${lastValue}`;
 }
 
 // Desafio 5
@@ -33,21 +33,35 @@ function highestCount(arr) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let firstCat = Math.abs(mouse - cat1);
+  let secondCat = Math.abs(mouse - cat2);
+  if (firstCat < secondCat) return 'cat1';
+  if (firstCat > secondCat) return 'cat2'; // Linter não permite else if :(
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arr) {
+  return arr.map((value) => {
+    let response;
+    if (value % 3 === 0 && value % 5 === 0) response = 'fizzBuzz';
+    else if (value % 3 === 0) response = 'fizz';
+    else if (value % 5 === 0) response = 'buzz';
+    else response = 'bug!';
+    return response;
+  });
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  const vowels = ' aeiou';
+  return str.replace(/[aeiou]/g, (m) => vowels.indexOf(m));
 }
-function decode() {
-  // seu código aqui
+
+function decode(str) {
+  const vowels = ' aeiou';
+  return str.replace(/[1-5]/g, (m) => vowels[m]);
 }
 
 module.exports = {
