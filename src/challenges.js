4d6c8b3c3ee6b723b983ee0e18c5e25b6187cabb
Antonio Malato <malatoneto@gmail.com>
2021-03-30 15:09:40 -0300

    Desafio 3 - Concluido

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,10 @@ function calcArea(base, height) {
 console.log(calcArea(51, 1))
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
+console.log(splitSentence("vamo que vamo go Trybe"))
 
 // Desafio 4
 function concatName() {
