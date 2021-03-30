992a3a97644847a28be8b19fce443639e5ebc553
Rubia Emilly <rubiaemilly29@gmail.com>
2021-03-30 15:52:22 -0300

    Atualizado desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue(value1,value2) {
+function compareTrue(value1, value2) {
   if(value1===true && value2===true){
     return true
   }  else{
@@ -8,9 +8,9 @@ function compareTrue(value1,value2) {
 } console.log( compareTrue(true,true))
 
 // Desafio 2
-function calcArea(base,height) {
- 
-}
+function calcArea(base, height) {
+ return (base * height) / 2.
+} console.log(calcArea(10, 50))
 
 // Desafio 3
 function splitSentence() {
