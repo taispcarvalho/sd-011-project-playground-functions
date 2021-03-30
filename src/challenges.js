e29d2229be855124cddff41e2f413e93d4a7216d
Uriel Bochi <urielbochi@gmail.com>
2021-03-30 16:08:49 -0300

    Segundo e terceiro desafio

--- src/challenges.js
+++ src/challenges.js
@@ -8,15 +8,18 @@ function compareTrue(a, b) {
   }
  
 }
-
 // Desafio 2
-function calcArea() {
   // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
+  palavra = string.split('');
+  return palavra;
+
 }
 
 // Desafio 4
