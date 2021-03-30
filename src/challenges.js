e9f0caedce447be90db27070801ee35d063bba79
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-03-30 14:48:05 -0300

    Commit Inicial + 1 ex

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(pinky, cerebro) {
+  if(pink === true && cerebro === true){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 // Desafio 2
