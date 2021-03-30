32a9d5f52848cf73b7952392c02639b818b7663d
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-03-30 15:04:55 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -4,33 +4,59 @@ function compareTrue(b1, b2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  return str.split(' ')
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arr) {
+  return arr.join(', ')
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arr) {
+  let highest = 0
+  let count = 0
+
+  for (num of arr) {
+    if (num > highest) {
+      highest = num
+    }
+  }
+
+  for (num of arr) {
+    if (num === highest) {
+      count += 1
+    }
+  }
+
+  return count
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1m = cat1 - mouse
+  let cat2m = cat2 - mouse
+
+  if (cat1m > cat2m) {
+    return 'cat1'
+  }
+  else if (cat1m < cat2m) {
+    return 'cat2'
+  }
+  else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
