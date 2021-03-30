7442d7dd407675cd72c65cce77c198e3f1b3f994
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-03-30 14:58:57 -0300

    Conclusão Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,9 +9,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(" ");
 }
+
+console.log(splitSentence("vame que vae"));
 
 // Desafio 4
 function concatName() {
