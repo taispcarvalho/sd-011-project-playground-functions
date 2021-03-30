78ebb0e97a95ed1d88e8a4c482be0e52c1ec9fb3
Hugo Mouto <hmoutol@gmail.com>
2021-03-30 19:19:27 -0300

    Desafio 7: CHECK

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,5 @@
 // Desafio 1
 function compareTrue(input1, input2) {
-  // seu código aqui
   if (input1 && input2) {
     return true;
   }
@@ -10,7 +9,6 @@ function compareTrue(input1, input2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  // seu código aqui
   let area = (base * height) / 2;
   return area;
 }
@@ -18,7 +16,6 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  // seu código aqui
   let stringArray = string.split(' ');
   return stringArray;
 }
@@ -34,54 +31,46 @@ function concatName(arrayD4) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  // seu código aqui
   let totalPoints = (wins * 3) + ties;
   return totalPoints;
 }
 // console.log(footballPoints(0,0))
 
 // Desafio 6
-let testArrayD6 = [0,0,0];
-// let biggest = testArrayD6[0];
-// let biggestCount = 0;
-// // Descobrir qual é o maior
-//   //Se n>n-1, maior = n
-//   for (let index = 1; index < testArrayD6.length; index +=1) {
-//     if (testArrayD6[index]>biggest) {
-//       biggest = testArrayD6[index];      
-//     }
-//   }
-//   for (index = 0; index < testArrayD6.length; index +=1) {
-//     if (testArrayD6[index] === biggest) {
-//       biggestCount +=1;
-//     }
-//   }
-//   console.log(biggest);
-//   console.log(biggestCount);
-//Contar quantas vezes se repete
-
+// let testArrayD6 = [0,0,0]; // Array de teste
 function highestCount(arrayD6) {
-  // seu código aqui
   let biggest = arrayD6[0];
   let biggestCount = 0;
-    for (let index = 1; index < arrayD6.length; index +=1) {
-      if (arrayD6[index]>biggest) {
-        biggest = arrayD6[index];      
-      }
+  for (let index = 1; index < arrayD6.length; index += 1) {
+    if (arrayD6[index] > biggest) {
+      biggest = arrayD6[index];
     }
-    for (index = 0; index < arrayD6.length; index +=1) {
-      if (arrayD6[index] === biggest) {
-        biggestCount +=1;        
-      }
+  }
+  for (let index = 0; index < arrayD6.length; index += 1) {
+    if (arrayD6[index] === biggest) {
+      biggestCount += 1;
     }
-    return biggestCount;  
+  }
+  return biggestCount;
 }
-console.log(highestCount(testArrayD6))
+// console.log(highestCount(testArrayD6))
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(cat1, cat2, mouse) {
+  let distCat1 = mouse - cat1;
+  let distCat2 = mouse - cat2;
+  switch (true) {
+  case distCat1 > distCat2:
+    return 'cat2';
+  case distCat2 > distCat1:
+    return 'cat1';
+  case distCat2 === distCat1:
+    return 'os gatos trombam e o rato foge';
+  default:
+    break;
+  }
 }
+console.log(catAndMouse(1, 2, 3));
 
 // Desafio 8
 function fizzBuzz() {
