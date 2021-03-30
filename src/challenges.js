6adc4059ae0d1997494ba7b042d9287d5eaedae7
Jessica Arruda <sicarruda@gmail.com>
2021-03-30 14:44:20 -0300

    Inicinado Projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1,valor2) {
+  if(valor1 && valor2){
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
