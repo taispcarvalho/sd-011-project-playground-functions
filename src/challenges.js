ba527f879962871b5ce7aae8e02c5fd76002e4d2
IsleneGomes <islenegomes@gmail.com>
2021-03-30 18:37:43 -0300

    Analisando erros-5

--- src/challenges.js
+++ src/challenges.js
@@ -16,30 +16,24 @@ function splitSentence() {
 
 // Desafio 4
 let pessoas = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+let primeiro = pessoas[0];
+let ultimo = pessoas[pessoas.length-1];
 function concatName(pessoas) {
-    for (let i = 0; i < concatName.length; i += 1) {
-    if (concatName[i] === concatName[concatName.length - 1]) {
-      let ultimoNome = concatName[i];
-    }
-    if (concatName[i] === concatName[0]) {
-      let primeiroNome = concatName[0];
-    }
-  return (console.log(ultimoNome, ',', primeiroNome));
-      } 
-  }
-  
+    return (ultimo + ','+primeiro);
+  } 
+console.log(ultimo + ','+primeiro)
   
 
 // Desafio 5
-let wins = 14;
-let ties = 8;
-let pointsWins = 0
+let pointsWins = 0;
 let pointsTies = 0;
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
+wins = 14;
+ties = 8;
 if (wins !== 0) {
   pointsWins = 3*wins;
  if (ties !==0) {
-  pointsTies = ties*1
+  pointsTies = ties*1;
  }
 let soma = pointsTies + pointsWins;
 }
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,3 +1,11 @@
-function compareTrue(jiboia,cobra) {
-  return jiboia && cobra;
-}
+let pessoas = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+let primeiro = pessoas[0];
+    let ultimo = pessoas[pessoas.length-1];
+function concatName(pessoas) {
+    
+    return (ultimo + ','+primeiro);
+    
+  
+      } 
+    
+  console.log(ultimo + ','+primeiro)
