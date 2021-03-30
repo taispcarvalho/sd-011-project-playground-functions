bd52afb148fe2765e56f5445d46e26ea5f55247d
Víctor Faria <vitilevi@hotmail.com>
2021-03-30 15:11:20 -0300

    Feito desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ')
 }
+
+console.log(splitSentence('foguete'))
 
 // Desafio 4
 function concatName() {
