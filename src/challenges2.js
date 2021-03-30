f0af0eb914b561006887596f9c0b7ccd3988aefe
Gustavo Lemes <gustavolemes997@gmail.com>
2021-03-30 17:36:43 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,22 +5,51 @@ function techList(arrTech, name) {
 
   if (arrTech.length === 0) {
     return 'Vazio!';
-  } else {
-    for (let index = 0; index < arrTech.length; index += 1) {
-      arrTech = arrTech.sort();
-      dados = {
-        tech: arrTech[index],
-        name: name,
-      };
-      lista.push(dados);
-    }
-    return lista;
   }
+  for (let index = 0; index < arrTech.length; index += 1) {
+    arrTech = arrTech.sort();
+    dados = {
+      tech: arrTech[index],
+      name: name,
+    };
+    lista.push(dados);
+  }
+  return lista;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrPhoneNumbers) {
+  if (checkArray(arrPhoneNumbers) || checkRepeatNumbers(arrPhoneNumbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return '(' + arrPhoneNumbers[0] + arrPhoneNumbers[1] + ')' + ' ' + arrPhoneNumbers[2] + arrPhoneNumbers[3] + arrPhoneNumbers[4] + arrPhoneNumbers[5] + arrPhoneNumbers[6] + '-' + arrPhoneNumbers[7] + arrPhoneNumbers[8] + arrPhoneNumbers[9] + arrPhoneNumbers[10];
+
+}
+
+function checkArray(arrayNumbers) {
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkRepeatNumbers(arrNumbers) {
+  let number;
+  for (let index1 = 0; index1 < arrNumbers.length; index1 += 1) {
+    let countNumber = 0;
+    number = arrNumbers[index1];
+    for (let index2 = 0; index2 < arrNumbers.length; index2 += 1) {
+      if (number === arrNumbers[index2]) {
+        countNumber += 1;
+      }
+    }
+    if (countNumber >= 3) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // Desafio 12
@@ -35,6 +64,8 @@ function hydrate() {
 
 module.exports = {
   generatePhoneNumber,
+  checkRepeatNumbers,
+  checkArray,
   techList,
   hydrate,
   triangleCheck,
