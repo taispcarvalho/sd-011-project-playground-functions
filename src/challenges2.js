ef4f2a8464a7947104d6b17930d179452ecc8e42
Tales Coelho <tales-coelho@live.com>
2021-03-30 16:48:53 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,8 +13,35 @@ function techList(nameTech, nome) {
   return arrTechs
 }
 
+
+function checkNumberPhone (phoneNumber) {
+  let repete = 0;
+  if (phoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index in phoneNumber) {
+    if (phoneNumber[index] < 0){
+      return 'não é possível gerar um número de telefone com esses valores, seja menor que 0';
+    }
+    if (phoneNumber[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores, maior que 9';
+    }
+    repete = 0;
+    for (let repeteIndex = 0; repeteIndex < phoneNumber.length; repeteIndex += 1) {
+      if (phoneNumber[index] === phoneNumber[repeteIndex]) {
+        repete += 1;
+      }
+      if (repete >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores, repete 3 vezes ou mais'
+      }
+    }
+  }
+}
 // Desafio 11
 function generatePhoneNumber(arrNumber) {
+  if (checkNumberPhone(arrNumber)) {
+    return checkNumberPhone(arrNumber);
+  }
   let numberPhone = '(';
   for (let index = 0; index < arrNumber.length; index += 1) {
     if(numberPhone.length === 3) {
@@ -27,13 +54,12 @@ function generatePhoneNumber(arrNumber) {
   }
   return numberPhone;
 }
-
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 2, 4, 5, 6, 7, 8, 9, 0, 1],));
 
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+  
 }
 
 // Desafio 13
