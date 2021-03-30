586262c30413705158b88fac898aa1875a1d2f52
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-03-30 16:21:35 -0300

    Exercicios 1 a 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(technologiesList, name) {
+  let techs = [];
+  technologiesList.sort();
+
+  if (technologiesList.length <= 0) {
+    return 'Vazio!'
+  }
+  for (let index = 0; index < technologiesList.length; index += 1) {
+    techs.push (
+      {
+        tech: technologies[index],
+        name: name
+      },
+    );
+  } return techs
+}
+ 
+// Desafio 11
+function generatePhoneNumber() {
+
 }
 
 // Desafio 11
