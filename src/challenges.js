02869f2b278330805da9ca58e83ee310f504b363
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-03-30 15:36:37 -0300

    Realiza requisito 2 e faz correções no requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,10 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let resultado = (base * height) / 2;
+
+  return resultado;
 }
 
 // Desafio 3
