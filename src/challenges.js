d30331d0621aab2cecd0df89a92c1fd4f9f22e3b
Filizzola <juliofilizzola@hotmail.com>
2021-03-30 18:59:40 -0300

    sexto desafio

--- src/challenges.js
+++ src/challenges.js
@@ -29,10 +29,27 @@ function footballPoints(wins, ties) {
 console.log(footballPoints(14, 8));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
 
+function highestCount(number) {
+  let contRepet = 0;
+  let contNumber = 0;
+  let indexNumberR = 0;
+  for(let index in number){
+    let verficaNumber = number[index];
+    for (let index2 in number){
+      if(verficaNumber === number[index2]){
+        contNumber += 1;
+      }
+    }
+    if(contNumber > contRepet){
+    contRepet = contNumber;
+    indexNumberR = index;
+  }
+  contNumber = 0;
+  }
+  return number[indexNumberR];
+}
+console.log(highestCount([6, 1, 2, 3, 2, 5, 7]))
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
