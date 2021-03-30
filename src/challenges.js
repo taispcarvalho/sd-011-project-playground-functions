fb41b2026353c80c29c19f2b1f478f2e4be27926
Rodolfo <rodolforrcont@gmail.com>
2021-03-30 17:30:32 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -94,9 +94,24 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
   // seu código aqui
-}
+  let arrayFizzBuzz = [];
+  for (let index = 0; index<array.length;index+=1){
+    if (array[index] % 3 === 0 && array[index] % 5 === 0){
+      arrayFizzBuzz.push('fizzBuzz');
+      }else if (array[index] % 5 === 0){
+       arrayFizzBuzz.push('buzz')
+      }else if (array[index] % 3 === 0){
+       arrayFizzBuzz.push('fizz')
+      }else {
+        arrayFizzBuzz.push('bug');
+      }
+  }
+  return arrayFizzBuzz
+  }
+  console.log(fizzBuzz([2, 15, 7, 9, 45]))
+
 
 // Desafio 9
 function encode() {
