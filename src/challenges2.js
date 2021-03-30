aacf5e36114c505f86aa08b6cc9f657eb27ac92d
Matheus Gaspar <msouzagaspar@gmail.com>
2021-03-30 19:19:01 -0300

    requisito 11: implementa função generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,8 +5,50 @@ function techList(techNames, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkInvalidDigits(numbers) {
+  for (let n of numbers) {
+    if (n > 9 || n < 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkTooManyRepeats(numbers) {
+  const count = {};
+  for (let n of numbers) {
+    if (count[n]) {
+      count[n] += 1;
+    } else {
+      count[n] = 1;
+    }
+  }
+  const maxCount = Object.values(count).reduce((a, b) => Math.max(a, b));
+  return maxCount >= 3;
+}
+
+function isValid(numbers) {
+  const hasInvalidDigits = checkInvalidDigits(numbers);
+  const hasTooManyRepeats = checkTooManyRepeats(numbers);
+  if (hasInvalidDigits || hasTooManyRepeats) {
+    return false;
+  }
+  return true;
+}
+
+function formatPhoneNumber(numbers) {
+  numbers.splice(0, 0, '(');
+  numbers.splice(3, 0, ') ');
+  numbers.splice(9, 0, '-');
+  return numbers.join('');
+}
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) return 'Array com tamanho incorreto.';
+  if (!isValid(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return formatPhoneNumber(numbers);
 }
 
 // Desafio 12
