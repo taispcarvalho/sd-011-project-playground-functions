30f4c8cb3e51b9110ce4584484826cdf08f4a9d8
Gustavo Lemes <gustavolemes997@gmail.com>
2021-03-30 18:45:34 -0300

    Corrigindo lint requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -15,7 +15,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(arrayString) {
-  return `${arrayString[arrayString.length - 1]} + ', ' + ${arrayString[0]}`;
+  return `${arrayString[arrayString.length - 1]}, ${arrayString[0]}`;
 }
 
 // Desafio 5
