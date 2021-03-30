deef7e43048c10f119725844a3357926be420062
Marco Galindo <marco1408@gmail.com>
2021-03-30 18:16:02 -0300

    exercício 7 finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -45,6 +45,12 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let distancia1 = cat1 - mouse;
   let distancia2 = cat2 - mouse;
+  if (distancia1 < 0) {
+    distancia1 = distancia1 * -1;
+  }
+  if (distancia2 < 0) {
+    distancia2 = distancia2 * -1;
+  }
   if (distancia1 < distancia2) {
     return 'cat1';
   } else if (distancia2 < distancia1) {
