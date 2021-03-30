00df330a3f5263958249d8ac7da153d8961cfd78
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-03-30 17:22:55 -0300

    Finaliza o desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -36,8 +36,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (3 * wins) + (ties);
+  return (points);
 }
 
 // Desafio 6
