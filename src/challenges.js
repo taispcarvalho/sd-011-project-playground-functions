dd2ac996466849ed65fe774dc43b3bf818ddc6e6
Reginara Gomes <refagomes@gmail.com>
2021-03-30 18:08:00 -0300

    Adiciona requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -10,9 +10,12 @@ function compareTrue(value1, value2) {
 console.log(compareTrue(true, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
