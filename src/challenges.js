88dacb53d934a4aec094db1993b89c06487693d2
Amandha W. Barok <amandha.barok@gmail.com>
2021-03-30 11:11:54 -0700

    Exercício 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,16 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 === true && value2 === true){
+  if (value1 === true && value2 === true) {
     return true;
-  } else{
+  } else {
     return false;
   }
 }
-console.log(compareTrue(true, false));
-
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = base * height / 2
+  return result;
 }
 
 // Desafio 3
