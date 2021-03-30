6c107d91cef6cebe3f08259afb2bcb6314307448
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-03-30 17:01:54 -0300

    Finaliza ex 03

--- src/challenges.js
+++ src/challenges.js
@@ -5,13 +5,14 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  
-  return base * height / 2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string, array) {
+  string = 'vini cius';
+  array = string.split(" ");
+  console.log(array);
 }
 
 // Desafio 4
