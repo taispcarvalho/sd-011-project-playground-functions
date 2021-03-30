ed8ce57ef32426bbc24a1b7615eac0ded9d5fb6f
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-03-30 15:39:04 -0300

    solved function highest Count

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,42 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if ( value1 === true && value2 === true ){
-    return true
-  }
-  else {
-    return false
+  if (value1 === true && value2 === true) {
+    return true;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ")
+  return string.split(" ");
 }
 
 // Desafio 4
 function concatName(array) {
-  return array.filter((e, i) => i ===0 || i === array.length - 1)
+  return array.filter((e, i) => i ===0 || i === array.length - 1);
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins * 3) + (ties * 1)
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let ordemArray = array.sort(function orderArray(a, b) { return a - b });
+
+  let sumRepeatLargestNumber = 0;
+
+  for( let i = 1; i <= ordemArray.length; i += 1) {
+    if(ordemArray[i] === ordemArray[i + 1]) {
+      sumRepeatLargestNumber += 1;
+    }
+  }
+  return sumRepeatLargestNumber;
 }
 
 // Desafio 7
