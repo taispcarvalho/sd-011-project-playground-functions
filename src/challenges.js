9793f4f293772fa5ef6e3794b7e725ea7cebb33c
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-03-30 15:39:46 -0300

    Finish requirement 8

--- src/challenges.js
+++ src/challenges.js
@@ -79,9 +79,27 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let answer = [];
+
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 3 === 0 && numbers[index] % 5 != 0) {
+      answer.push('fizz');
+    } else if (numbers[index] % 3 != 0 && numbers[index] % 5 === 0) {
+      answer.push('buzz');
+    } else if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+      answer.push('fizzBuzz');
+    } else {
+      answer.push('bug!');
+    }
+  }
+
+  return answer;
 }
+
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
+console.log(fizzBuzz([7, 9]));
+console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
 function encode() {
