3110151141e356263f6c477dfbb6b8002e1a229f
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-03-30 15:31:27 -0300

    conclusao do requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aquiiiii
+function compareTrue(a, b) {
+  if(a = true && b = true){
+    return true;
+  } else {
+    return false;
+  }
+  
 }
 
 // Desafio 2
