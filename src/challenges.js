5cc1b74dcc7a9599a8cc4fbb9ab993dd58b76ac5
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-03-30 15:11:02 -0300

    Adicionado exercicio 6

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,15 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arr) {
+  let highestNumberInArray = Math.max(...arr); //Math.max retorna o maior numero do array, e os 3 dots significa que pode vir mais de um argumento.
+  let timesRepeat = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === highestNumberInArray) {
+      timesRepeat += 1;
+    }
+  }
+  return timesRepeat;
 }
 
 // Desafio 7
