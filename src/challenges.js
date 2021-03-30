b9b59c3b19e28b53b51b945462f2545108b087c8
dutofanim <etofanim@gmail.com>
2021-03-30 19:27:10 -0300

    Lint fixes requisito 7.

--- src/challenges.js
+++ src/challenges.js
@@ -82,9 +82,8 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat2';
   } else if (distanceBetweenCat1 < distanceBetweenCat2) {
     return 'cat1';
-  } else {
-    return 'os gatos trombam e o rato foge';
   }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
