f10cb67796e2e21337c2979c51ead4cfe31408c1
israeltonholo <israeltonholo@hotmail.com>
2021-03-30 16:42:43 -0300

    adiciona exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -52,9 +52,16 @@ function highestCount(value) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  } else if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
+
 
 // Desafio 8
 function fizzBuzz() {
