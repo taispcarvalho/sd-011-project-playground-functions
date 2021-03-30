7461ef28318a36cd11172a88b1dd09cbc2eb8395
Gisele Costa <jikinha@gmail.com>
2021-03-30 18:23:25 -0300

    Exercicio 9

--- src/challenges.js
+++ src/challenges.js
@@ -63,25 +63,20 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(string) {
-  for (let index = 0; index < string.length; index += 1) {
-    if (string[index] === 'a') string[index] = '1';
-    else if (string[index] === 'e') string[index] = '2';
-    else if (string[index] === 'i') string[index] = '3';
-    else if (string[index] === 'o') string[index] = '4';
-    else if (string[index] === 'u') string[index] = '5';
-  }
-  }
+  string = string.replace(/a/g, "1");
+  string = string.replace(/e/g, "2");
+  string = string.replace(/i/g, "3");
+  string = string.replace(/o/g, "4");
+  string = string.replace(/u/g, "5");
   return string;
 }
 
 function decode(string) {
-  for (let index = 0; index < string.length; index += 1) {
-    if (string[index] === '1') string[index] = 'a';
-    else if (string[index] === '2') string[index] = 'e';
-    else if (string[index] === '3') string[index] = 'i';
-    else if (string[index] === '4') string[index] = 'o';
-    else if (string[index] === '5') string[index] = 'u';
-  }
+  string = string.replace(/1/g, "a");
+  string = string.replace(/2/g, "e");
+  string = string.replace(/3/g, "i");
+  string = string.replace(/4/g, "o");
+  string = string.replace(/5/g, "u");  
   return string;
 }
 
