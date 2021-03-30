3663e45f5b3183faca3f5bb2895fb8a27b1df76d
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-03-30 18:44:43 -0300

    Finish requirement 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,36 @@
+// Auxiliary function for requirement 10
+function bubbleSort(arr) {
+  let arrSize = arr.length;
+  let indexA, indexB, arrElement1, arrElement2;
+
+  for (indexA = 1; indexA < arrSize - 1; indexA += 1) {
+    for (indexB = 0; indexB < arrSize - indexA; indexB += 1) {
+      arrElement1 = arr[indexB];
+      arrElement2 = arr[indexB + 1];
+      if (arrElement1 > arrElement2) {
+        arr[indexB] = arrElement2;
+        arr[indexB + 1] = arrElement1;
+      }
+    }
+  }
+
+  return arr;
+}
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  let sortedTechs = bubbleSort(techs);
+  let finalList = [];
+  let finalListObject;
+
+  for (let tech of sortedTechs) {
+    finalListObject = {};
+    finalListObject['tech'] = tech;
+    finalListObject['name'] = name;
+    finalList.push(finalListObject);
+  }
+
+  return finalList;
 }
 
 // Desafio 11
