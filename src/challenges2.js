5e5f931de107d971383410c5047e150f3a75dc67
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-03-30 17:13:30 -0300

    Adicionado exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,23 @@
 // Desafio 10
-function techList() {
-  
+function techList(arr, name) {
+  let arrayOrdenada = arr.sort();
+  let obj = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    obj[i] = {
+      tech: arrayOrdenada[i],
+      name: name,
+    };
+  }
+  return obj;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber() {}
 
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
 }
-
-console.log(2);
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
