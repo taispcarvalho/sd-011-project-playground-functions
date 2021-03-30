af268b212b7b9e9e518a12e89adb810124a789cf
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-03-30 17:01:47 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -5,12 +5,13 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return ((base * height)/2);
+  return ((base * height)/ 2);
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(array) {
+  array = array.split(' ');
+  return array;
 }
 
 // Desafio 4
