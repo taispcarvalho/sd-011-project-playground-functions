f306683756f10ff10948155750e9dda94182074a
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-03-30 17:47:02 -0300

    Exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,12 @@
 // Desafio 10
 //  .sort() ordena os elementos, link sobre o metodo sort utilizado para este exercicio: https://www.w3schools.com/jsref/jsref_sort.asp
-function techList(arr, name) {
+function techList(arr, nome) {
   let arrayOrdenada = arr.sort();
   let obj = [];
   for (let i = 0; i < arr.length; i += 1) {
     obj[i] = {
       tech: arrayOrdenada[i],
-      name: name,
+      name: nome,
     };
   }
   return obj;
