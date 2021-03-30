a1481f38a337884ceab426602252cc495ebe890b
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-30 16:26:03 -0300

    Adiciona requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -27,22 +27,28 @@ console.log(splitSentence('go Trybe'));
 // Desafio 4
 function concatName(strArr) {
   let strGenerated = '';
-  for (let index = 0; index <= strArr.length; index += 1){
-    if (strArr[index] === 0) {
-      strGenerated = strGenerated + strArr[index] + ' , ';
-    } else if (strArr[index] === strArr.length) {
+  for (let index = 0; index < strArr.length; index += 1){
+    if (index === 0){
       strGenerated = strGenerated + strArr[index];
-    } return strGenerated;
-  } 
+      strGenerated = strGenerated + ', ';
+    } else if ( index === strArr.length - 1) {
+      strGenerated = strGenerated + strArr[index];
+    }
+  } return strGenerated;
   
 }
 
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let winPoints = 3;
+  let tiePoints = 1;
+  let totalPoints = (winPoints * (wins) ) + (tiePoints * (ties));
+  return totalPoints;
 }
+
+console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount() {
