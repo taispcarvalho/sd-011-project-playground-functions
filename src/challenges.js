1419ee3d46189fd75f136d114d2dcd87a081e3a2
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-03-30 16:58:04 -0300

    Verçao comentada do exercicio 6 (..Em desemvolvimento..) Salvando progresso.

--- src/challenges.js
+++ src/challenges.js
@@ -31,10 +31,32 @@ function footballPoints(wins, ties) {
 }
 console.log(footballPoints(0, 0));
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+/* Desafio 6
+ Retorne a quantidade de vezes que o maior deles se repete.
+O que será verificado:
+Retorne 2 quando o parâmetro passado na funcão highestCount seja [9, 1, 2, 3, 9, 5, 7]
+Retorne 1 quando o parâmetro passado na funcão highestCount seja [0, 4, 4, 4, 9, 2, 1]
+Retorne 3 quando o parâmetro passado na funcão highestCount seja [0, 0, 0] */
+function highestCount(array) {
+  /*Primeiro achar o maior numero
+  1 - Criar uma variável highestNumber = 0.
+  2 - Criar uma variável countHighest = 1. (Pelo menos uma vez o maior numero aparecerá, não é?)
+  3 -PERCORRER o array de entrada com o for.
+  4 - [Acha o maior numero.] Salvar na variável maiorNumero o maior entre o primeiro numero do array vs variável maior numero.
+  5 - No proximo loop comparar maior numero com proximo item do array.(se o segundo for maior que var maiorNumero salva o segundo. Se não, faz nada. Fazer isso para todo o arrei.) Fazer a dinamica disso com o index do for.
+  6 - Com o maior numero em mãos [conta quantas vezes ele aparece no array.]
+  Se forem iguais acrescenta um no contador de maior e passa.
+  7 - return countHighest
+  */
+  let highestNumber = 0;
+  let countHighest = 1;
+  for (index = 0; index <= array.length; index +=1){
+    if (array[index] > highestNumber) {highestNumber = array[index];}
+    if (array[index] === highestNumber){countHighest += 1;} 
+  }
+  return countHighest;
 }
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
 
 // Desafio 7
 function catAndMouse() {
