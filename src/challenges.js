2181127950a789f0f6d56566733fcf0f668d33bd
marlon307 <marlon-ramosb@hotmail.com>
2021-03-30 16:43:30 -0300

    Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,48 @@
 // Desafio 1
-function compareTrue( param1 , param2 ) {
+function compareTrue(param1, param2) {
   return param1 && param2;
 }
 
+console.log(compareTrue(true, true));
+
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, altura) {
+  return base * altura / 2;
 }
+
+console.log(calcArea(5, 2));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
+
+console.log(splitSentence('foguete marlon'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+let arrr = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+
+function concatName(arr) {
+  return arr[arr.length - 1].concat(', ', arr[0]);
 }
 
+console.log(concatName(arrr));
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
+
+console.log(footballPoints(7, 2));
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+
 }
+
+let numberr = [0, 4, 4, 4, 9, 2, 1];
+
+console.log(highestCount(numberr));
 
 // Desafio 7
 function catAndMouse() {
