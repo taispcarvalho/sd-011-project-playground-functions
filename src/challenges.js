ee00a9b70daa58d02a1a6dcc3c4bb450d0e02b39
Júlio Mota <juliomotacampos@gmail.com>
2021-03-30 14:50:00 -0300

    Exercício 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(value1, value2) {
+  return value1 && value2;
 }
 
 // Desafio 2
 function calcArea() {
-  
+  // seu código aqui
 }
 
 // Desafio 3
