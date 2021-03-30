799b5ece84b5794f9b1899c2665fb090437f804d
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-03-30 15:14:16 -0300

    Desafio 4 realizado.

--- src/challenges.js
+++ src/challenges.js
@@ -17,8 +17,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  return (`${array[0]}, ${array[array.length - 1]}`);
 }
 
 // Desafio 5
