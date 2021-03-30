a3e43a430128ba6b3aeae4bb51751f997ddf6a06
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-30 18:15:00 -0300

    adiciona requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,50 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function hasValidNumbers(numbers) {
+  for (let number of numbers) {
+    if (number < 0 || number > 9) return false;
+  }
+
+  return true;
+}
+
+function isValidPhoneNumber(numbers) {
+  let status = hasValidNumbers(numbers);
+  let count = 0;
+  let previous;
+  let sortedNumbers = numbers.slice(0).sort();
+
+  for (let i = 0; i < sortedNumbers.length; i += 1) {
+    count = previous && previous === sortedNumbers[i] ? count + 1 : 1;
+
+    if (count === 3) {
+      status = false;
+      break;
+    }
+
+    previous = sortedNumbers[i];
+  }
+
+  return status;
+}
+
+function applyPhoneNumberMask(numbers) {
+  let ddd = numbers.slice(0, 2).join('');
+  let firsPart = numbers.slice(2, 7).join('');
+  let secondPart = numbers.slice(7, 12).join('');
+
+  return `(${ddd})${firsPart}-${secondPart}`;
+}
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) return 'Array com tamanho incorreto.';
+
+  if (!isValidPhoneNumber(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  return applyPhoneNumberMask(numbers);
 }
 
 // Desafio 12
