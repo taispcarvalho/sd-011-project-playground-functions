5de64f32bcb08b73501ac98ee7c3932631d6c9d8
Nalu <aluziapantoja@gmail.com>
2021-03-30 16:22:32 -0300

    2 - Função da área do triângulo + correções lint

--- src/challenges.js
+++ src/challenges.js
@@ -3,14 +3,14 @@ function compareTrue(value1, value2) {
   // seu código aqui
   if (value1 === true && value2 === true) {
     return true;
-  } else {
-    return false;
   }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base,height) {
   // seu código aqui
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
