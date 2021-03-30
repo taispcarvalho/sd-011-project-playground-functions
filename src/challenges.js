2e936298dc96660782d36a224a317cded1f68c33
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-03-30 14:57:38 -0300

    Feito o requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,24 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if(value1 === true && value2 === true) { 
-    return true
-  }else {
-    return false
+  if (value1 === true && value2 === true) { 
+    return true;
   }
+  
+  return false;
+  
 }
 
-console.log(compareTrue(true, true));
+console.log(compareTrue(true, false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = 0;
+  area = (base*height)/2;
+  return area;
 }
+
+console.log(calcArea(10, 50));
+
 
 // Desafio 3
 function splitSentence() {
