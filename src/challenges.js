70d91c8fa0b54304bab1a382acdfe16a657fd237
dutofanim <etofanim@gmail.com>
2021-03-30 14:57:12 -0300

    Iniciando o projeto playgroun-functions.

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue() {
-
+function compareTrue(value1,value2) {
+  return ((value1 === true && value2 === true) ? true : false);
 }
 
 // Desafio 2
@@ -20,7 +20,7 @@ function concatName() {
 
 // Desafio 5
 function footballPoints() {
-
+  
 }
 
 // Desafio 6
@@ -30,20 +30,20 @@ function highestCount() {
 
 // Desafio 7
 function catAndMouse() {
-  
+
 }
 
 // Desafio 8
 function fizzBuzz() {
-  
+
 }
 
 // Desafio 9
 function encode() {
-  
+
 }
 function decode() {
-  
+
 }
 
 module.exports = {
