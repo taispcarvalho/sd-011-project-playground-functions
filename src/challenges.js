bf006e1f06230ee5742cd14fc022d9c705b0d936
Wilquer82 <wilquer.fig@gmail.com>
2021-03-30 19:12:18 -0300

    1 2 3 4 5 6 7 sera

--- src/challenges.js
+++ src/challenges.js
@@ -1,57 +1,39 @@
-// Desafio 1
-//let valor1=true;
-//let valor2=true;
+//  Desafio 1 OK
 function compareTrue(valor1,valor2) {
   return(valor1&&valor2);
-}
-//compareTrue(valor2,valor1);
 
-// Desafio 2
-//let base = 1;
-//let altura = 51;
-//let areaTriangulo = 0.1;
+// Desafio 2 OK
 function calcArea(base,altura) {
   areaTriangulo = base * altura / 2;
   return(areaTriangulo);
 }
-//calcArea(base,altura);
-//console.log(areaTriangulo);
 
-// Desafio 3
-//let frase="Wilquer Figueiredo Alves"
+// Desafio 3 OK
 function splitSentence(frase){
   let separado=frase.split(" ");
   return(separado);
 }
-//splitSentence(frase);
-  // seu código aqui
-
 
 // Desafio 4
 //let lista=["meu","nome","nao","e","jhonny"];
-//console.log(lista.length);
+
 function concatName(lista){
   let primPos=lista[0];
   let ultPos=lista[lista.length-1];
-  let resposta= ultPos.concat(" ",primPos);
+  let resposta= ultPos.concat(", ",primPos);
   console.log(resposta);
   }
 //concatName(lista);
 
-// Desafio 5
-//let wins=0;
-//let ties=0;
+// Desafio 5 OK
 function footballPoints(wins,ties) {
   let totalpoints=wins*3+ties*1;
   return(totalpoints);
 }
-//footballPoints(wins,ties);
 
-// Desafio 6
-//let valores=[0,0,0];
+// Desafio 6 OK
 function highestCount(valores){ 
   let arrumado=valores.sort();
-  //console.log(arrumado);
   let maior=arrumado[arrumado.length-1];
   let quanVezes=0;
     for(let index=0; index<=valores.length-1;index++){
@@ -61,27 +43,37 @@ function highestCount(valores){
     }
  return(quanVezes);
 }
-//highestCount(valores);
 
 // Desafio 7
-//let mouse=6;
-//let cat1=13;
-//let cat2=12 ;
 
 function catAndMouse(mouse,cat1,cat2) {
-  if(cat1-mouse==cat2-mouse){
-    return("Os gatos trombam e o rato foge");
+  if(cat1-mouse>cat2-mouse){
+    return("cat2");
   }else if(cat1-mouse<cat2-mouse){
     return("cat1");
   }else{
-    return("cat2");
+    return("Os gatos trombam e o rato foge");
   }
 }
-//catAndMouse(mouse,cat1,cat2);
+
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+
+function fizzBuzz(arrayteste){
+  let arrayresposta=[0];
+  arrayresposta.pop();
+  for(let cont=0;cont<=arrayteste.length-1;){
+    if(arrayteste[cont]%3==0&arrayteste[cont]%5==0){
+      arrayresposta.push("fizzbuzz");
+    }else if(arrayteste[cont]%3==0&arrayteste[cont]%5!==0){
+      arrayresposta.push("fizz");
+    }else if(arrayteste[cont]%3!==0&arrayteste[cont]%5==0){
+      arrayresposta.push("buzz");
+    }else{
+      arrayresposta.push("bug");
+    }
+  }
+  return(arrayresposta);
 }
 
 // Desafio 9
