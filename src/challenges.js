7f830c21ec4683b58535463fc2b60cbb5d6beed3
murilodospacos <murilopacos@gmail.com>
2021-03-30 15:57:00 -0300

    Primeira resolucao exercicio 3

--- src/challenges.js
+++ src/challenges.js
@@ -12,9 +12,10 @@ function calcArea(base, heigth) {
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  return frase.split(' ');
 }
+
 
 // Desafio 4
 function concatName() {
