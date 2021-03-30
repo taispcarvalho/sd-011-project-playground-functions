6a41b8c8fe0a8bc174a5fccc1396f940fb6b869c
Guinho Anjos <iago.anjos@hotmail.com>
2021-03-30 17:25:58 -0300

    desafio 6 highestCount feito

--- src/challenges.js
+++ src/challenges.js
@@ -1,40 +1,57 @@
-// Desafio 1
+// Desafio 1 - FEITO
 function compareTrue(boo1, boo2) {
   return boo1 && boo2;
 }
 console.log(compareTrue(false,true));
 
 
-// Desafio 2
+// Desafio 2 -FEITO
 function calcArea(base, height) {
   return (base * height)/2
 }
 console.log(calcArea(51,1));
 
-// Desafio 3
+// Desafio 3 -FEITO
 function splitSentence(phrase) {
   let arrayPhrase = phrase.split(' ');
   return arrayPhrase;
 }
 console.log(splitSentence('go trybe'))
 
-// Desafio 4
+// Desafio 4 -FEITO
 function concatName(...arrayName) {
   let name = arrayName[arrayName.length -1].concat(', ' + arrayName[0]);
  return name;
 }
 console.log(concatName('captain', 'my', 'captain'));
-// Desafio 5
+
+// Desafio 5 -FEITO
 function footballPoints(wins, ties) {
   let points=(wins * 3)+ ties;
   return points;
 }
 console.log(footballPoints(0,0));
 
-// Desafio 6
-function highestCount() {
-  
+// Desafio 6 -FEITO
+function highestCount(...numberArray) {
+  let highestNumber= 0;
+  let highestNumberCount=0;
+for(let index = 0; index<numberArray.length; index+=1){
+    if(highestNumber<numberArray[index]){
+        highestNumber=numberArray[index]
+    }
 }
+for(let index2 = 0; index2<numberArray.length; index2+=1){
+  if(highestNumber===numberArray[index2]){
+     highestNumberCount=highestNumberCount + 1;
+  }else{
+      highestNumberCount+=0;
+  }
+}
+
+return highestNumberCount;
+}
+console.log(highestCount(0, 0, 0));
 
 // Desafio 7
 function catAndMouse() {
