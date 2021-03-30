25583b7439e0f6c6d54b7aa29c3ee5d03a160b22
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-03-30 16:29:31 -0300

    Exercicios 1 a 10 fix

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue(calculo1 , calculo2) {
+function compareTrue(calculo1,calculo2) {
   if (calculo1 === true && calculo2 === true) {
     return true;
 } else {
@@ -9,7 +9,7 @@ function compareTrue(calculo1 , calculo2) {
 }
 
 // Desafio 2
-function calcArea(base , height) {
+function calcArea(base,height) {
   return base * height / 2
 }
 calcArea (10 , 50)
@@ -25,31 +25,75 @@ function concatName(nomes) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins , ties) {
+  let pontos
+  return pontos = wins * 3 + ties
+
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maiorNumeroAteAgora = array[0];
+  let count = 0;
+  for (let index = 0; index < array.length; index += 1){
+    if (maiorNumeroAteAgora < array[index]){
+      maiorNumeroAteAgora = array [index];
+    }
+  }
+  for (let index = 0; index < array.length; index += 1){
+    if (array[index] === maiorNumeroAteAgora){
+      count += 1;
+    }
+  }
+  return count
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Position = Math.abs(cat1 - mouse)
+  let cat2Position = Math.abs(cat2 - mouse) 
+  if (cat1Position > cat2Position) {
+    return 'cat2'
+  }
+  else if (cat2Position > cat1Position) {
+    return 'cat1'
+  
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
+
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(Numbers) {
+  let Return = [];
+  for (let i = 0; i < Numbers.length; i += 1) {
+    if (Numbers[i] % 3 === 0 && Numbers[i] % 5 === 0) {
+      Return.push('fizzBuzz');
+    } else if (Numbers[i] % 3 === 0) {
+      Return.push('fizz');
+    } else if (Numbers[i] % 5 === 0) {
+      Return.push('buzz');
+    } else {
+      Return.push('bug!');
+    }
+  }
+  return Return;
+
+ 
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(palavras) {
+  palavras = palavras.replaceAll('a' , '1').replaceAll('e' , '2').replaceAll('i' , '3').replaceAll ('o' , '4').replace ('u' , '5');
+  return palavras;
+
 }
-function decode() {
-  // seu código aqui
+
+function decode(codigos) {
+  codigos = codigos.replaceAll('1' , 'a').replaceAll('2' , 'e').replaceAll('3' , 'i').replaceAll('4' , 'o').replaceAll ('5' , 'u');
+  return codigos
+
 }
 
 module.exports = {
