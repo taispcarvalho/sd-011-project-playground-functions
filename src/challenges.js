067f26c7c85d16838020bd2942a678669b0a5e69
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-03-30 17:15:52 -0300

    Desafios 3 e 4 concluidos

--- src/challenges.js
+++ src/challenges.js
@@ -4,18 +4,20 @@ function compareTrue(bool1,bool2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  return (base*height)/2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  return sentence.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStrings) {
+  let posicao1 = arrayStrings[0];
+  let posicao2 = arrayStrings[arrayStrings.length-1]
+  return posicao1 + ', ' + posicao2;
 }
 
 // Desafio 5
