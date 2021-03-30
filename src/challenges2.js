08583b06a504a23fc0b8d163e851135e45f4ad11
Gustavo Lemes <gustavolemes997@gmail.com>
2021-03-30 17:06:44 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrTech, name) {
+  let lista = [];
+  let dados = {};
+
+  if (arrTech.length === 0) {
+    return 'Vazio!';
+  } else {
+    for (let index = 0; index < arrTech.length; index += 1) {
+      arrTech = arrTech.sort();
+      dados = {
+        tech: arrTech[index],
+        name: name,
+      };
+      lista.push(dados);
+    }
+    return lista;
+  }
 }
 
 // Desafio 11
