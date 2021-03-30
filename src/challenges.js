f0740793649206d96fecd17c23c4b02d5ade6c46
IsleneGomes <islenegomes@gmail.com>
2021-03-30 18:23:44 -0300

    Analisando erros-4

--- src/challenges.js
+++ src/challenges.js
@@ -4,7 +4,7 @@ function compareTrue(par, impar) {
 }
 
 // Desafio 2
-function calcArea(a , b) {
+function calcArea(a, b) {
   let area = (a * b) / 2;
   return (area);
 }
@@ -15,18 +15,17 @@ function splitSentence() {
 }
 
 // Desafio 4
-let pessoas = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
+let pessoas = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
 function concatName(pessoas) {
-for (let i = 0; i < concatName.length; i+=1) {
+    for (let i = 0; i < concatName.length; i += 1) {
     if (concatName[i] === concatName[concatName.length - 1]) {
       let ultimoNome = concatName[i];
     }
     if (concatName[i] === concatName[0]) {
       let primeiroNome = concatName[0];
     }
-  
-  return (console.log(ultimoNome, ', ',primeiroNome));
-    } 
+  return (console.log(ultimoNome, ',', primeiroNome));
+      } 
   }
   
   
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,25 @@
+
+ 
+let pessoa = [];
+function concatName(pessoa) {
+}
+
+let wins = 14;
+let ties = 8;
+let pointsWins = 0
+let pointsTies = 0;
+function footballPoints(wins,ties) {
+if (wins !== 0) {
+  pointsWins = 3*wins;
+ if (ties !==0) {
+  pointsTies = ties*1;
+ }
+let soma = pointsTies + pointsWins;
+}console.log(soma)
+  return (soma);
+}
+
+
 let pessoas = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
 function concatName(pessoas) {
 for (let i = 0; i < concatName.length; i+=1) {
@@ -7,6 +29,6 @@ for (let i = 0; i < concatName.length; i+=1) {
     if (concatName[i] === concatName[0]) {
       let primeiroNome = concatName[0];
     }
-  return (console.log(ultimoNome, ', ',primeiroNome));
+  return (ultimoNome, ', ',primeiroNome);
     } 
   }
