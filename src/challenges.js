412b0516a52013a57492ba578a0197dd27743359
Marcelo Leite <marsleite@gmail.com>
2021-03-30 15:26:53 -0300

    Resolução do requisito 3.

--- src/challenges.js
+++ src/challenges.js
@@ -10,9 +10,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  return frase.split(' ');
 }
+console.log(splitSentence('go trybe'));
 
 // Desafio 4
 function concatName() {
