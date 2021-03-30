c1169cfe3cbde685506f9037bb4d151c34553bee
LeoFuna <leo.funa@gmail.com>
2021-03-30 16:47:44 -0300

    Desafio 5 realizado

--- src/challenges.js
+++ src/challenges.js
@@ -35,8 +35,8 @@ function concatName(input) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
