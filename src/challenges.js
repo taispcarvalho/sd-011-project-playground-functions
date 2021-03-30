3d436589ede659e3bf71ccff530468eb146e2ff9
gab-boff <gmeninboff@gmail.com>
2021-03-30 19:13:38 -0300

    Adicionando Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -44,8 +44,16 @@ function highestCount(arrayNumbers) {
 // A arrow function para realizar a ordenação dos números foi retirada de "https://www.digitalocean.com/community/tutorials/js-array-sort-numbers"
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let result;
+  if (mouse - cat1 > mouse - cat2) {
+    result = 'cat1';
+  } else if (mouse - cat1 < mouse - cat2) {
+    result = 'cat2';
+  } else {
+    result = 'os gatos se trombam e o rato foge';
+  }
+  return result;
 }
 
 // Desafio 8
