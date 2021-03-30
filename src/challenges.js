c7e11cf08b77e80199d82cfc1fd9bb3235469a6e
Oryange <oryangestrifezze@gmail.com>
2021-03-30 19:35:15 -0300

    Ajusta virgula

--- src/challenges.js
+++ src/challenges.js
@@ -49,17 +49,18 @@ function highestCount(numbers) {
 return numbersOfNumbers;
 }
 
-// Desafio 7
+// Desafio 7 
 function catAndMouse(mouse, cat1, cat2) {
 
   let distanceCat1 = cat1 - mouse;
   let distanceCat2 = cat2 - mouse;
 
-  if ( distanceCat1 > distanceCat2) {
+  if (distanceCat1 > distanceCat2) {
     return 'cat2';
   } else if (distanceCat2 > distanceCat1) {
     return 'cat1';
-  } else { return 'os gatos trombam e o rato foge'
+  } else { 
+    return 'os gatos trombam e o rato foge';
   } 
 }; 
 
