8f31f9ababf40cce0b1d0e7299607079a3d6e133
Edson <edson.exe@outlook.com>
2021-03-30 20:57:23 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+
+function techList(tech, name) {
+  let output = []; 
+
+  for (let index = 0; index < tech.length; index += 1){  
+    let object = {
+      tech: String,
+      name: String
+    }   
+    if (tech.length != null) {
+      let objeto = Object.create(object);
+      objeto.tech = tech[index];
+      objeto.name = nome;
+      output.push(objeto);
+    } else {
+      output = 'Vazio!'
+    } 
+  } return output;
 }
 
 // Desafio 11
