c99298ca7127ca1e2e0f14834a7b14fd2d51fefa
Rubia Emilly <rubiaemilly29@gmail.com>
2021-03-30 19:25:42 -0300

    Atualizado desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -5,27 +5,27 @@ function compareTrue(value1, value2) {
   }  else{
     return false
   }                                                                                                                                                                           
-} console.log( compareTrue(true,true))
+} //console.log( compareTrue(true,true))
 
 // Desafio 2
 function calcArea(base, height) {
  return (base * height) / 2.
-} console.log(calcArea(10, 50))
+} //console.log(calcArea(10, 50))
 
 // Desafio 3
 function splitSentence(frase) {
   return frase.split(' ')
-} console.log(splitSentence("go Trybe"))
+} //console.log(splitSentence("go Trybe"))
 
 // Desafio 4
 function concatName(array) {
   return ` ${array[array.length-1]} , ${array[0]}`
-} console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+} //console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3 ) + ties
-} console.log(footballPoints(14,8))
+} //console.log(footballPoints(14,8))
 
 // Desafio 6
 function highestCount(arra) {
@@ -39,7 +39,7 @@ function highestCount(arra) {
   }
   return cont
   
-}console.log( highestCount([ 9, 1, 2, 3, 9, 5, 7 ]))
+} //console.log( highestCount([ 9, 1, 2, 3, 9, 5, 7 ]))
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
