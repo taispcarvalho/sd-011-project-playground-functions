ba3cd4d35c9ec7d9fb24e1a5a6e2d8f486958ecc
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-03-30 16:21:55 -0300

    solved function fizz buzz

--- src/challenges.js
+++ src/challenges.js
@@ -43,8 +43,23 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      result.push('fizzBuzz');
+    }
+    else if (array[i] % 3 === 0) {
+      result.push('fizz');
+    }
+    else if (array[i] % 5 === 0) {
+      result.push('buzz');
+    }
+    else {
+      result.push('bug!');
+    }
+  }
+  return result;
 }
 
 // Desafio 9
