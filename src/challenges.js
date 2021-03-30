a1cdfec618690b84f82d6eb1ff6c5f0f10a2908c
Juliana Happatsch <jhappatsch@gmail.com>
2021-03-30 15:59:39 -0300

    Corrigindo exercício 4

--- src/challenges.js
+++ src/challenges.js
@@ -15,7 +15,8 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  return array[0] + ', ' + array[array.length - 1];
+  let toString = array[0] + ', ' + array[array.length - 1];
+  return toString;
 }
 
 // Desafio 5
