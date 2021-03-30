f521400aa551bf4f86475b27235514ae8612feee
Júlio Mota <juliomotacampos@gmail.com>
2021-03-30 19:14:35 -0300

    Exercício 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function createObject(techs, name) {
+  let arrayObject = [];
+  
+  for (let index of techs) {
+    let objectTech = {};
+  
+    objectTech['tech'] = index;
+    objectTech['name'] = name;
+  
+    arrayObject.push(objectTech);
+  }
+
+  return arrayObject;
+}
+
+function techList(techs, name) {
+  let arrayObject = [];
+  
+  if (arrayObject === []) {
+    console.log('Vazio!');
+  } else {
+    arrayObject = createObject(techs, name);
+  }
+
+  return arrayObject;
 }
 
 // Desafio 11
