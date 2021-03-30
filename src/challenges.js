ea78b01bd2cb57c62f919bf212b818c2bcdb49a0
tulio sploradori <tuliospl@gmail.com>
2021-03-30 17:45:09 -0300

    conclusao do item 7

--- src/challenges.js
+++ src/challenges.js
@@ -23,7 +23,6 @@ function footballPoints(wins, ties) {
   return ((wins * 3) + (ties * 1));
 }
 
-
 // Desafio 6
 function highestCount(numbers) {
   let maior = 0;
@@ -37,18 +36,25 @@ function highestCount(numbers) {
     if (numbers[i] === maior) {
       contador += 1;
     }
-  }  
-  return(contador);
+  }
+  return (contador);
 }
-
-
-
-
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distaciaCat1 = cat1 - mouse;
+  let distaciaCat2 = cat2 - mouse;
+  if (distaciaCat1 < distaciaCat2) {
+    return 'cat1';
+  }
+  else if (distaciaCat2 < distaciaCat1) {
+    return 'cat2';
+  }
+  else if (distaciaCat1 === distaciaCat2) {
+    return 'Os gatos trombam e o rato foge';
+  }
 }
+
 
 // Desafio 8
 function fizzBuzz() {
