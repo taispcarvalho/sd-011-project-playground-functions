d9775613f7e24cf0c4723ebb7bc503bc2a4d012d
fernando lasmar <f3rnandolasmar@gmail.com>
2021-03-30 17:19:35 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -2,13 +2,14 @@
 function compareTrue(value1, value2) {
   return value1 && value2;
 }
-
-console.log(compareTrue(true,true));
+console.log('teste 1')
+console.log(compareTrue(true, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return ((base * height) / 2);
 }
+console.log(calcArea(35, 30));
 
 // Desafio 3
 function splitSentence() {
