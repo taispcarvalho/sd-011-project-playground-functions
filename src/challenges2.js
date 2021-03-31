07b44dc2fbff2bed0876849e3d212ece0edd4752
Diego Ribeiro <dihrm2001@gmail.com>
2021-03-30 21:11:09 -0300

    Exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function createTechObjects(list, name) {
+  let array = [];
+  let objects = {};
+  for(let technology of list) {
+    // Source: https://stackoverflow.com/questions/2383484/how-to-create-a-dynamic-object-in-a-loop
+    objects = {tech: technology, name: name};
+    array.push(objects);
+  }
+  return array;
 }
+
+function techList(listDevTech, name) {
+  let sortedListDevTech = listDevTech.sort();
+  return createTechObjects(sortedListDevTech, name);
+}
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
