15d53820dfcfa5d757e7e984703a5d8632c1601b
renan <renan_menezes07@hotmail.com>
2021-03-31 19:12:59 -0300

    Desafio 13: removi linha de teste 'console.log'.

--- src/challenges2.js
+++ src/challenges2.js
@@ -65,7 +65,6 @@ function hydrate(string) {
   }
   return (soma > 1) ? `${soma} copos de água` : `${soma} copo de água'`;
 }
-console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
 
 module.exports = {
   generatePhoneNumber,
