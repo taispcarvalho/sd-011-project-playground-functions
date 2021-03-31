8b3073732b2472c1acb715cc47c48b53ba74c04b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-03-31 16:46:46 -0300

    Finalizaçao ex 07 TENTATIVA 03

--- src/challenges.js
+++ src/challenges.js
@@ -49,8 +49,8 @@ function highestCount(numbers) {
 
 // Desafio 7
 function catAndMouse(cat1, cat2, mouse) {
-  let distance1 = mouse - cat1;
-  let distance2 = mouse - cat2;
+  let distance1 = Math.abs(mouse - cat1);
+  let distance2 = Math.abs(mouse - cat2);
 
   if (distance1 < distance2) {
     return 'cat1';
