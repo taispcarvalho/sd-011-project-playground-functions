3e6b854a1f6c8b1c4589ed4ba0eabb17bb3dad0b
Filizzola <juliofilizzola@hotmail.com>
2021-03-31 19:39:35 -0300

    desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -64,15 +64,13 @@ function fizzBuzz(array) {
     if (array[index] % 3 === 0 && array[index] % 5 === 0) {
       retorno.push('fizzBuzz');
     } else {
-      if (array[index] % 3 === 0 ) {
+      if (array[index] % 3 === 0) {
         retorno.push('fizz');
-      } else {
-        if (array[index] % 5 === 0 ){
+      } else if (array[index] % 5 === 0) {
           retorno.push('buzz');
         } else {
           retorno.push('bug!');
         }
-      }
     }
   }
   return retorno;
@@ -81,10 +79,26 @@ function fizzBuzz(array) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    array = array.replace('a', '1');
+    array = array.replace('e', '2');
+    array = array.replace('i', '3');
+    array = array.replace('o', '4');
+    array = array.replace('u', '5');
+  }
+  return array;
 }
-function decode() {
+
+function decode(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    array = array.replace('1', 'a');
+    array = array.replace('2', 'e');
+    array = array.replace('3', 'i');
+    array = array.replace('4', 'o');
+    array = array.replace('5', 'u');
+  }
+  return array;
   // seu código aqui
 }
 
