e79e262fc2820d8f81e530f7d11d6dd12ab5a03e
LeoFuna <leo.funa@gmail.com>
2021-03-31 08:12:14 -0300

    Desafio 10 realizado linter ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,13 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techToLearn, myName) {
+  let listOfTechToLearn = [];
+  let techToLearnSorted = techToLearn.sort();
+  for (let index = 0; index < techToLearn.length; index += 1) {
+    let newObject = { tech: techToLearnSorted[index], name: myName };
+    listOfTechToLearn.push(newObject);
+  }
+  return listOfTechToLearn;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
