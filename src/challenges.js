11e6f8856e449a085c3911b7ff7590265717d179
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-31 08:27:53 -0300

    Add code

--- src/challenges.js
+++ src/challenges.js
@@ -55,11 +55,16 @@ function highestCount(arrayCountBiggest) {
   return countHighestValue;
 }
 
-console.log(highestCount([9, 1, 2, 9, 9, 5, 7]));
-
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distMouseCat1 = Math.abs(mouse - cat1);
+  let distMouseCat2 = Math.abs(mouse - cat2);
+  if (distMouseCat1 < distMouseCat2) {
+    return 'cat1';
+  } else if (distMouseCat2 < distMouseCat1) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e os ratos fogem';
 }
 
 // Desafio 8
