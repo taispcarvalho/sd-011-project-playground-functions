19e46d2ab459f5cecc10a9e10ad6001561deadb8
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-03-31 15:28:23 -0300

    NataliaSRiber Resoluçao Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue (param1, param2) {
+function compareTrue(param1, param2) {
   if( param1 === true && param2 === true){
   return true;
   } 
@@ -9,29 +9,35 @@ function compareTrue (param1, param2) {
 
 // Desafio 2
 let area = 0;
-function calcArea (base, height){
+function calcArea(base, height){
   area = (base * height) / 2;
   return area;
 }
 //console.log(calcArea(5, 2));
 
 // Desafio 3
-function splitSentence (stringPhrase) {
+function splitSentence(stringPhrase) {
   let splitPhrase = stringPhrase.split(' ');
   return splitPhrase;
 }
 //console.log(splitSentence('Go Trybe'));
 
 // Desafio 4 
-function concatName (namesList) {
+function concatName(namesList) {
   return (namesList[namesList.length - 1]+',' + ' ' + namesList[0]);
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+//console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+let points = 0;
+let winPoints = 3;
+let tiesPoints = 1;
+function footballPoints(wins, ties) {
+  points = (wins * winPoints)+ (ties*tiesPoints);
+  return points;
 }
+//console.log(footballPoints(14, 8));
+
 // Desafio 6
 function highestCount() {
   // seu código aqui
