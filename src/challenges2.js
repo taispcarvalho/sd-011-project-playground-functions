f3c7d4aa6b526ab5d21b49198e61c76d17aa2517
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-03-31 17:28:08 -0300

    Adiciona verificacao de array vazio na funcao tech

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,26 @@
 // Desafio 10
 function techList(array, name) {
   let newArray = [];
-  for (let index = 0; index < array.length; index += 1) {
-    let objeto = {
-      tech: array[index],
-      name: `${name}`,
-    };
-    newArray.push(objeto);
+  if (array.length > 0) {
+    for (let index = 0; index < array.length; index += 1) {
+      let objeto = {
+        tech: array[index],
+        name: `${name}`,
+      };
+      newArray.push(objeto);
+    }
+    return newArray.sort((a, b) => (a.tech > b.tech ? 1 : -1));
   }
-  return newArray.sort((a, b) => (a.tech > b.tech ? 1 : -1));
+  return 'Vazio!';
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
-
+// let teste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+// function generatePhoneNumber(array) {
+//   let phoneNumber = '(' + array[0] + array[1] + ')' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10]
+//   return phoneNumber
+// }
+// console.log(generatePhoneNumber(teste))
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
