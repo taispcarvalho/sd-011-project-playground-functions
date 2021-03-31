885953995987fe54cbc142225dbe64802ae34f3e
igordmf <igordanielmf@gmail.com>
2021-03-30 23:23:01 -0300

    Corrige erros linter desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -88,18 +88,18 @@ function fizzBuzz(arrayOfNumbers) {
 // Desafio 9
 function encode(string) {
   return string.replaceAll('a', 1)
-               .replaceAll('e', 2)
-               .replaceAll('i', 3)
-               .replaceAll('o', 4)
-               .replaceAll('u', 5);
+    .replaceAll('e', 2)
+    .replaceAll('i', 3)
+    .replaceAll('o', 4)
+    .replaceAll('u', 5);
 }
 
 function decode(string) {
   return string.replaceAll('1', 'a')
-               .replaceAll('2', 'e')
-               .replaceAll('3', 'i')
-               .replaceAll('4', 'o')
-               .replaceAll('5', 'u');
+    .replaceAll('2', 'e')
+    .replaceAll('3', 'i')
+    .replaceAll('4', 'o')
+    .replaceAll('5', 'u');
 }
 
 module.exports = {
