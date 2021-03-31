408d3c91c7aaa714c65c1f0c221415fbd8971f6b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-31 18:18:51 -0300

    Change code of generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -62,9 +62,16 @@ function checkConditionals(arrayNumbers) {
 
 // Desafio 11
 function generatePhoneNumber(arrayPhone) {
-  checkConditionals(arrayPhone);
-  return `(${arrayPhone[0]}${arrayPhone[1]}) ${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}
-  ${arrayPhone[5]}-${arrayPhone[6]}${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
+  let negOrBiggNine = checkNegativeNineNumber(arrayPhone);
+  let repeatedNumber = checkRepeatedNumber(arrayPhone);
+  let sizePhone = checkSizePhoneNumber(arrayPhone);
+  if (negOrBiggNine || repeatedNumber ) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (sizePhone) {
+    return 'Array com tamanho incorreto.';
+  }
+  return `(${arrayPhone[0]}${arrayPhone[1]}) ${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}-${arrayPhone[6]}${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
 }
 
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
