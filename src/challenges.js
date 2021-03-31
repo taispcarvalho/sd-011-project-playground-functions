c7dc9250e768382eeca4781aac1dc6c76e232d6d
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-03-31 20:39:00 -0300

    Desafio 7 corrigindo novas falhas

--- src/challenges.js
+++ src/challenges.js
@@ -58,15 +58,16 @@ function catAndMouse(mouse, cat1, cat2) {
   if (cat1 - mouse < cat2 - mouse) {
     return 'cat1';
   }
-  else if (cat1 - mouse > cat2 - mouse) {
+
+  if (cat1 - mouse > cat2 - mouse) {
     return 'cat2';
   }
-  
-  else if (cat1 - mouse === cat2 - mouse) {
+
+  if (cat1 - mouse === cat2 - mouse) {
     return 'os gatos trombam e o rato foge';
-    
   }
 }
+console.log(catAndMouse(2, 8, 8));
 
 // Desafio 8
 function fizzBuzz() {
