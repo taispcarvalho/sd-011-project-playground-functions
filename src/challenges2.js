e4f7ee781c5a85ba82c02bdb4abd437b41999a93
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-03-31 18:10:38 -0300

    exercicio 11 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,21 +25,23 @@ function checkValidity(array) {
       return false;
     }
     counter[array[index]] += 1;
-    if (counter[array[index]] > 3) {
+    if (counter[array[index]] >= 3) {
       return false;
     }
   }
   return true;
 }
-function generatePhoneNumber() {
-  if (!checkSize) {
+function generatePhoneNumber(tel) {
+  if (!checkSize(tel)) {
     return 'Array com tamanho incorreto.';
   }
-  if (!checkValidity) {
+  if (!checkValidity(tel)) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
+  return `(${tel[0]}${tel[1]}) ${tel[2]}${tel[3]}${tel[4]}${tel[5]}${tel[6]}-${tel[7]}${tel[8]}${tel[9]}${tel[10]}`;
 }
-
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+// console.log(`(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`);
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
