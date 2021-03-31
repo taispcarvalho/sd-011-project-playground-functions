383b8f6af979479032c3519cf75d2fe07e33c731
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-31 11:50:58 -0300

    Change code

--- src/challenges.js
+++ src/challenges.js
@@ -90,8 +90,8 @@ function encode(inputString) {
   let numbers = [1, 2, 3, 4, 5];
   let outputString = '';
   let storeNumber = 0;
-  for(let index = 0; index < inputString.length; index += 1) {
-    for (let index2 = 0; index2 < vowels.length; index2++) {
+  for (let index = 0; index < inputString.length; index += 1) {
+    for (let index2 = 0; index2 < vowels.length; index2 += 1) {
       if (inputString[index] === vowels[index2]) {
         storeNumber = numbers[index2];
       }
@@ -126,6 +126,8 @@ function decode(inputString) {
   }
   return outputString;
 }
+
+console.log(decode('Th3s 3s 1n 2nc4d3ng t2st.'));
 
 module.exports = {
   calcArea,
