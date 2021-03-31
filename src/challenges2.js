54a7bfbc85f57fc73389945ae3e1dd6bd86f05fb
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-03-31 18:03:47 -0300

    Mudanca techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,12 +14,13 @@ function techList(array, name) {
   return 'Vazio!';
 }
 
+console.log(techList(techs, 'Henrique'));
 // Desafio 11
 // let teste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
-// function generatePhoneNumber(array) {
-//   let phoneNumber = '(' + array[0] + array[1] + ')' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10]
-//   return phoneNumber
-// }
+function generatePhoneNumber(array) {
+  // let phoneNumber = '(' + array[0] + array[1] + ')' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10]
+  // return phoneNumber
+}
 // console.log(generatePhoneNumber(teste))
 // Desafio 12
 function triangleCheck() {
