07d4f2885f211edd7b17d4e5e825a01f1cb2ced4
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-03-31 14:23:19 -0300

    Conclusão Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,53 @@ function techList(arrayTec, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let telefone = [];
+  let cont = 0;
+	if (typeof validationArray(array) === 'string'){
+		return validationArray(array);
+	}
+  telefone[0] = '(';
+  telefone[3] = ')';
+  telefone[4] = ' ';
+  telefone[10] = '-';
+  for (let index = 0; index < array.length; index += 1){
+    if(telefone[cont] !== undefined){
+      index -= 1;
+    } else {
+      telefone[cont] = array[index].toString();
+    }
+    cont += 1;
+  }
+  return telefone.join('');
+}
+
+function repetArray(array){
+	for (let firstIndex = 0; firstIndex < array.length; firstIndex += 1) {
+    let cont = 0;
+		for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+			if (array[firstIndex] === array[secondIndex]) {
+				cont += 1;
+			}
+			if (cont >= 3) {
+				return cont;
+			}
+		}
+	}
+  return 0;
+}
+
+function validationArray(array){
+  let repeticao = repetArray(array);
+	if (array.length !== 11){
+		return "Array com tamanho incorreto.";
+	}	
+	for (let index = 0; index < array.length; index += 1){
+		if(array[index] < 0 || array[index] > 9 || repeticao >= 3){
+			return "não é possível gerar um número de telefone com esses valores";
+		}
+	}
+  return array;
 }
 
 // Desafio 12
