88410545371e133289bb3499835a1b1d0e663749
heyynat <natalifplima@gmail.com>
2021-03-31 19:47:16 -0400

    ajuste final no Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,15 +1,16 @@
 // Desafio 10
 function techList(skills, name) {
   let packageSkills = [];
+
+  if (skills.length <= 0) {
+    return 'Vazio!';
+  }
   skills.sort();
   for (let index = 0; index < skills.length; index += 1) {
     packageSkills.push({
       tech: skills[index],
       name: name
     });
-    if (skills[index] == null) {
-      return 'Vazio!';
-    }
   }
   return packageSkills;
 }
