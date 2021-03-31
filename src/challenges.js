83d2e594eb62dabc737afe41e5cadd9aa3405de1
Savio Moraes <smmsavio@hotmail.com>
2021-03-30 21:45:25 -0300

    Inicia resolucao do requisito 05

--- src/challenges.js
+++ src/challenges.js
@@ -27,9 +27,12 @@ console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
