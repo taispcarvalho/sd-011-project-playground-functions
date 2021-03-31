b24b91ed22515644d7e32848170dae8718801bc5
Felipe Buso <felipe_buso@hotmail.com>
2021-03-31 18:37:35 -0300

    Iniciado exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,9 +19,73 @@ function techList(techs, name) {
 // console.log(techList([], 'Felipe'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function sizeArray(list) {
+  let size = false;
+  
+  if (list.length === 11) {
+    size = true;
+  }
+  return size;
 }
+// console.log(sizeArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+
+function limitNumbers(list) {
+  let limit = true;
+
+  for (let index of list) {
+    if (index < 0 || index > 9) {
+      limit = false;
+    }
+  }
+  return limit;
+}
+// console.log(limitNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+// console.log(limitNumbers([1, 2, 3, 4, 11, 6, 7, 8, 9, 0, 1]));
+// console.log(limitNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1]));
+
+function repeatedNumber(list) {
+  let repeated = false;
+
+  for (let index of list) {
+    let count = 0;
+    for (let number of list) {
+      if (count < 3) {
+        if (list[index] === number) {
+          count += 1;
+        }
+      } else {
+        repeated = true;
+      }
+    }
+  }
+  return repeated;
+}
+// console.log(repeatedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+// console.log(repeatedNumber([1, 2, 3, 4, 5, 5, 7, 8, 9, 0, 1]));
+// console.log(repeatedNumber([1, 2, 3, 4, 4, 4, 7, 8, 9, 0, 1]));
+
+function generatePhoneNumber(array) {
+  let sizeNumbers = sizeArray(array);
+  let positiveNumberUnit = limitNumbers(array);
+  let frequentNumbers = repeatedNumber(array);
+  let phoneNumber = 'não é possível gerar um número de telefone com esses valores';
+
+  if (sizeNumbers === false) {
+    return phoneNumber;
+  } else if ( positiveNumberUnit === false) {
+    return phoneNumber;
+  } else if (frequentNumbers === true) {
+    return phoneNumber;
+  } else {
+    phoneNumber = "teste";
+  }
+  return phoneNumber;
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 5, 5, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 19, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, -5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1]));
 
 // Desafio 12
 function triangleCheck() {
