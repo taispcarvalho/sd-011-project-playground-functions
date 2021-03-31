32a663b4b1fe65b67abf638dc338cd2f73c47a8d
PH Assis <pieit@live.com>
2021-03-31 14:42:51 -0300

    challenge 11 ready

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,4 @@
+/* eslint-disable guard-for-in */
 // Desafio 10
 
 function techList(ability, name) {
@@ -14,11 +15,57 @@ function techList(ability, name) {
   return abilityList;
 }
 
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function errorCheck(array) {
+  let check = 'ok';
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0 || array[i] > 9) {
+      check = 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (array.length !== 11) {
+    check = 'Array com tamanho incorreto.';
+  }
+  return check;
+}
+
+function arrayToObject(array) {
+  let emptyObject = {};
+  for (let i = 0; i < array.length; i += 1) {
+    let y = array[i];
+    if (emptyObject[y] == null) {
+      emptyObject[y] = 1;
+    } else emptyObject[y] += 1;
+  }
+  return emptyObject;
+}
+
+function tooManyNumbers(pN) {
+  let x = arrayToObject(pN);
+  let indexZero = '';
+  for (let i in x) {
+    if (x[i] >= 3) {
+      indexZero = 'full';
+      break;
+    }
+  }
+  return indexZero;
+}
+
+function generatePhoneNumber(array) {
+  let phoneArray = '(xx) xxxxx-xxxx';
+
+  for (let i = 0; i < array.length; i += 1) {
+    phoneArray = phoneArray.replace('x', array[i]);
+  }
+  if (tooManyNumbers(array) === 'full') {
+    phoneArray = 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (errorCheck(array) !== 'ok') {
+    phoneArray = errorCheck(array);
+  }
+
+  return phoneArray;
 }
 
 // Desafio 12
