b1c608fe44c0185c23c3996c411d6ff7791a969c
VictorS <czpask@gmail.com>
2021-03-31 19:45:45 -0300

    Exercicio 12 corrigido lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,19 +14,25 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  let triCondi = false;
-  let checkA = lineA < lineB + lineC;
-  let difLineA = Math.abs(lineA - (lineB + lineC));
-
+  let triCondi = true;
+  let checkA = lineA < (lineB + lineC);
+  let difLineA = lineA > Math.abs(lineB - lineC);
+  let checkB = lineB < (lineA + lineC);
+  let difLineB = lineB > Math.abs(lineA - lineC);
+  let checkC = lineC < (lineA + lineB);
+  let difLineC = lineC > Math.abs(lineA - lineB);
   if (checkA && difLineA) {
     triCondi = true;
-  } 
-  else {
+  } else if (checkB && difLineB) {
+    triCondi = true;
+  } else if (checkC && difLineC) {
+    triCondi = true;
+  } else {
     triCondi = false;
   }
   return triCondi;
 }
-console.log(triangleCheck(10, 14, 8));
+console.log(triangleCheck(5, 10, 3));
 
 // Desafio 13
 function hydrate() {
