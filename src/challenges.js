c27519a71d025881f84e4ef70e622c89310c9e51
Gabriel <gabriel.omercador@hotmail.com>
2021-03-31 14:00:42 -0300

    Exercicios 4 e 5

--- src/challenges.js
+++ src/challenges.js
@@ -16,13 +16,14 @@ function splitSentence(splitPhrase) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayName) {
+  let formatedName = arrayName[arrayName.length - 1] + ', ' + arrayName[0];
+  return formatedName; 
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
