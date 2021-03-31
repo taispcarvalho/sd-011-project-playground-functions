632220b568b8a780343f8232caf906354525f886
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-03-31 16:51:18 -0300

    correções finais

--- src/challenges.js
+++ src/challenges.js
@@ -7,7 +7,7 @@ function compareTrue(valor1, valor2) {
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  return base * height / 2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
@@ -15,16 +15,16 @@ function splitSentence(frase) {
   // seu código aqui
   let ajuda = '';
   let retorno = [];
-  for (let index in frase){
-    if (frase[index]!==' '){
-      ajuda+=frase[index];
-    }else{
+  for ( let index in frase){
+    if (frase[index] !== ' '){
+      ajuda += frase[index];
+    } else {
       retorno.push(ajuda);
-      ajuda=''
+      ajuda = '';
     }
-    if (index == frase.length-1){
+    if ( index === frase.length - 1){
       retorno.push(ajuda);
-      ajuda='';
+      ajuda = '';
     }
   }
   return retorno;
@@ -33,31 +33,30 @@ function splitSentence(frase) {
 // Desafio 4
 function concatName(frases) {
   // seu código aqui
-  let primeiro='';
-  let segundo='';
-  let retorno='';
-  primeiro=frases[frases.length-1];
-  segundo=frases[0];
-  retorno = primeiro + ", " + segundo;
+  let primeiro = '';
+  let segundo = '';
+  let retorno = '';
+  primeiro = frases[frases.length-1];
+  segundo = frases[0];
+  retorno = primeiro + ', ' + segundo;
   return retorno;
 }
-
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  return (wins*3)+(ties*1);
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
 function highestCount(numeros) {
   // seu código aqui
   let ordem = numeros.sort();
-  let maior = ordem[numeros.length-1];
-  let retorno=0;
-  for (let index in numeros ){
-    if (numeros[index]===maior){
-      retorno+=1;
+  let maior = ordem[numeros.length - 1];
+  let retorno = 0;
+  for ( let index in numeros){
+    if ( numeros[index] === maior){
+      retorno += 1;
     }
   }
   return retorno;
@@ -66,14 +65,14 @@ function highestCount(numeros) {
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
-  let distancia1 = Math.abs(mouse - cat1);
-  let distancia2 = Math.abs(mouse - cat2);
-  if (distancia1 < distancia2){
+  let distancia1 = Math.abs(mouse - cat1); 
+  let distancia2 = Math.abs(mouse - cat2); 
+  if ( distancia1 < distancia2){
     return 'cat1';
-  }else{
+  } else {
     if (distancia2 < distancia1){
       return 'cat2';
-    }else{
+    } else {
       return 'os gatos trombam e o rato foge';
     }
   }
@@ -83,9 +82,7 @@ function catAndMouse(mouse,cat1,cat2) {
 function fizzBuzz(numeros) {
   // seu código aqui
   let retorno = [];
-
   for (let index in numeros){
-
     if ((numeros[index] % 3 == 0 ) && (numeros[index] % 5 == 0)){
       retorno.push('fizzBuzz');
     }else{
