413b385ec3bba7906228037614dc59ab3f9a97fb
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-30 23:51:06 -0300

    deixa os codigos mais limpos

--- src/challenges.js
+++ src/challenges.js
@@ -1,60 +1,68 @@
 // Desafio 1
 function compareTrue(param1, param2) {
-  return param1 && param2 === true;
+  return param1 && param2;
 }
-console.log(compareTrue(true, true));
+console.log(compareTrue(true, false));
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height) / 2;
-  return area;
+  return (base * height) / 2;
 }
 console.log(calcArea(10, 50));
 // Desafio 3
 function splitSentence(frase) {
-  let space = ' ';
-  let fraseParaSeparar = frase.split(space);
-  return fraseParaSeparar;
+  return frase.split(' ');
 }
 console.log(splitSentence('go Trybe'));
 // Desafio 4
 function concatName(array) {
-  let fraseConcatenada = array[array.length - 1] + ', ' + array[0];
-  return fraseConcatenada;
+  return array[array.length - 1] + ', ' + array[0];
 }
 console.log(concatName(['captain', 'my', 'captain']));
 // Desafio 5
 function footballPoints(wins, ties) {
-  let winPoints = (3 * wins) + ties;
-  return winPoints;
+  return 3 * wins + ties;
 }
 console.log(footballPoints(2, 1));
 // Desafio 6
-function highestCount(arr) {
-  let maiorNumero = '';
-  for (let index = 0; index < arr.length; index += 1) {
-    if (arr[index] > arr[index + 1]) {
-      maiorNumero = arr[index]
+function getHighest(numbers) {
+  let highestNumber = numbers[0];
+  for (let number of numbers) {
+    if (number > highestNumber) {
+      highestNumber = number;
     }
   }
+  return highestNumber;
 }
 
-let listaDeNumeros = [9, 1, 2, 3, 9, 5, 7]
+function countElement(numbers, element) {
+  let count = 0;
+  for (let number of numbers) {
+    if (number === element) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function highestCount(arr) {
+  let highest = getHighest(arr);
+  return countElement(arr, highest);
+}
+
+let listaDeNumeros = [9, 1, 2, 3, 9, 5, 7];
 console.log(highestCount(listaDeNumeros));
 // Desafio 7
 function catAndMouse(mousePosition, catOnePosition, catTwoPosition) {
   let distanciaCat1 = Math.abs(catOnePosition - mousePosition);
   let distanciaCat2 = Math.abs(catTwoPosition - mousePosition);
-  console.log(distanciaCat1, distanciaCat2);
   if (distanciaCat1 > distanciaCat2) {
     return 'cat2';
-  } else if (distanciaCat2 > distanciaCat1) {
+  }
+  if (distanciaCat2 > distanciaCat1) {
     return 'cat1';
-  } 
+  }
   return 'os gatos trombam e o rato foge';
 }
-console.log(catAndMouse(0, 2, 4));
-console.log(catAndMouse(0, 6, 4));
-console.log(catAndMouse(0, 4, 4));
 console.log(catAndMouse(10, 4, 22));
 // Desafio 8
 function fizzBuzz(arr) {
@@ -76,7 +84,7 @@ let lista = [2, 15, 7, 9, 45];
 console.log(fizzBuzz(lista));
 // Desafio 9
 function encode() {
-  // seu código aqui
+
 }
 function decode() {
   // seu código aqui
