cb7275f92f9001512fdd787460625e62c2433e2a
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-03-31 16:28:42 -0300

    Adiciona funcao techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+let lala = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+let lalala = 'Henrique';
+function techList(array, name) {
+  let newArray = [];
+  for (let index = 0; index < array.length; index += 1) {
+    let objeto = {
+      tech: array[index],
+      name: `${name}`,
+    };
+    newArray.push(objeto);
+  }
+  return newArray.sort((a, b) => (a.tech > b.tech ? 1 : -1));
 }
 
+console.log(techList(lala, lalala));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
