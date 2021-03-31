3f97d5893ea0827db4d22815c98a87c41fed4af6
Vitor <vitorguima@gmail.com>
2021-03-31 12:35:07 -0300

    exercicio 9 finalizado e ajustes CR Oliva

--- src/challenges.js
+++ src/challenges.js
@@ -5,7 +5,7 @@ function compareTrue(param1, param2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  let totalArea = ((base * height) / 2);
+  let totalArea = (base * height) / 2;
   return totalArea;
 }
 
@@ -17,8 +17,8 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  let nameReturn = array[array.length - 1] + ', ' + array[0];
-  return nameReturn;
+  let formattedName = array[array.length - 1] + ', ' + array[0];
+  return formattedName;
 }
 
 // Desafio 5
@@ -26,8 +26,6 @@ function footballPoints(wins, ties) {
   let result = (3 * wins) + ties;
   return result;
 }
-
-let highestArray = [0, 0, 0];
 
 // Desafio 6
 
@@ -37,7 +35,7 @@ function highestCount(array) {
   let higherNumber = 0;
   let repeatNumber = 0;
   for (let highIndex = 0; highIndex < array.length; highIndex += 1) {
-    let possibleHigherNumber = higherNumber
+    let possibleHigherNumber = higherNumber;
     higherNumber = array[highIndex];
     if (higherNumber < possibleHigherNumber) {
       higherNumber = possibleHigherNumber;
@@ -68,15 +66,15 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(array) {
   let newArray = [];
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] % 3 === 0 && array[i] % 5 !== 0) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
       newArray.push('fizz');
-    } else if (array[i] % 5 === 0 && array[i] % 3 !== 0) {
+    } else if (array[index] % 5 === 0 && array[index] % 3 !== 0) {
       newArray.push('buzz');
-    } else if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+    } else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
       newArray.push('fizzBuzz');
     } else {
-      newArray.push('bug!')
+      newArray.push('bug!');
     }
   }
   return newArray;
