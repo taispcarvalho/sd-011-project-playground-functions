cf3191da76f5de80905611644c9270308a8a26bf
Renan <renan.vamo@gmail.com>
2021-03-30 22:08:55 -0300

    Resolve o ex 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,10 +13,51 @@ function techList(anyArray, anyName) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  
+  function checkIfFollowTheRules() {
+    if (array.length !== 11) {
+      return false;
+    } else {
+      for (let index in array) {
+      let count = 0
+        for (let number of array) {
+          if (array[index] == number) {
+            count += 1;
+            if(count > 2 || array[index] > 9 || array[index] < 0) {
+              return false;
+            }
+          } 
+        }
+      } 
+      return true
+    }
+  }
+if (checkIfFollowTheRules()) {
+  let formatedPhoneNumber = ''
+  for (let index = 0; index < array.length; index += 1) {
+    if (index == 0) {
+      formatedPhoneNumber += '('
+      formatedPhoneNumber += array[index]
+    } else if (index == 2) {
+      formatedPhoneNumber += ')'
+      formatedPhoneNumber += array[index]
+    } else if (index == 6) {
+      formatedPhoneNumber += '-'
+      formatedPhoneNumber += array[index]
+    } else {
+      formatedPhoneNumber += array[index]
+    }    
+  }
+  return formatedPhoneNumber
+} else {
+  return "não é possível gerar um número de telefone com esses valores"
 }
+}  
 
+
+let myArray = [0, 2, 3, 4, 5, 7, 7, 8, 9, 0, 7]
+console.log(generatePhoneNumber(myArray));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
