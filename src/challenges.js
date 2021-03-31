1c921530cf2a71c0ed10754556d45595e8e74d3e
LeoLacana <wilson17414@gmail.com>
2021-03-31 04:52:51 -0300

    resol exer 5

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,9 @@ function concatName(concatArray) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = wins*3 + ties;
+  return points;
 }
 
 // Desafio 6
