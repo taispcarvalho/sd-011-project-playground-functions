55e83bf4eed660654de1c49fd55402ea86dea48b
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-03-30 22:14:22 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -62,8 +62,21 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let resultado = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (array[index] % 3 === 0) {
+      resultado.push('fizz');
+    } else if (array[index] % 5 === 0) {
+      resultado.push('buzz');
+    }
+    if (array[index] % 3 !== 0 && array[index] % 5 !== 0) {
+      resultado.push('bug!');
+    }
+  }
+  return resultado;
 }
 
 // Desafio 9
