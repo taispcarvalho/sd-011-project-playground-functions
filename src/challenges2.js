e614b142419570aaeb112985da8013cbdec3cdf8
Juliana Happatsch <jhappatsch@gmail.com>
2021-03-30 22:14:59 -0300

    Exercício 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,31 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  for (let i in array) {
+    array[i] = {
+      tech: array[i],
+      name: name
+    }
+  }
+
+  array.sort(function(a, b) {
+    let techA = a.tech.toLowerCase();
+    let techB = b.tech.toLowerCase();
+
+    if (techA < techB) {
+      return -1;
+    }
+    if (techA > techB) {
+      return 1;
+    }
+
+    return 0;
+    });
+    // Inspirado no código do site http://www.javascriptkit.com/javatutors/arraysort2.shtml
+
+  return array;
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
