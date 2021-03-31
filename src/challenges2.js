b429f5c5eb8eeec6d0026cc452b594b5bb70950d
josanjohnata <josanjohnata@gmail.com>
2021-03-31 12:41:22 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,47 @@ function techList(tech, name2) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  
+function generatePhoneNumber(numbers) {
+  for (let number of numbers) {
+    if (number < 0 || number > 9) return false;
+  }
+
+  return true;
+}
+
+function validPhoneNumber(numbers) {
+  let status = validNumbers(numbers);
+  let count = 0;
+  let previous = 0;
+  let randomNumbers = numbers.slice(0).sort();
+
+  randomNumbers.forEach(number) => {
+    count = previous && previous === number ? count + 1 : 1;
+
+    if (count === 3) status = false;
+
+    previous = number;
+  };
+
+  return status;
+}
+
+function phoneNumber(numbers) {
+  let firstPart= numbers.slice(0, 2).join('');
+  let secondPart = numbers.slice(2, 7).join('');
+  let thirddPart = numbers.slice(7, 12).join('');
+
+  return (${firstPart}) ${secondPart}-${thirddPart};
+}
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) return 'Array com tamanho incorreto.';
+
+  if (!validPhoneNumber(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  return phoneNumber(numbers);
 }
 
 // Desafio 12
