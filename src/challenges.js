505823afc715e89fe09f80d0c1a9c5859d2d7851
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-03-31 11:10:46 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -31,11 +31,11 @@ function concatName(array) {
   lastFirst = `${last}, ${first}`; // O mesmo que dizer last + ',' + first;
   return lastFirst;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = wins * 3 + ties;
+  return points;
 }
 
 // Desafio 6
