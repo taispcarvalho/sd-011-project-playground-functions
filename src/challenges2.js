ae0d1e4c7442ac779903d3549302800fa5c1fcc0
lucasSCsantos <luc.cristovam10@gmail.com>
2021-03-31 19:38:13 -0300

    lint correction6

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,23 +12,46 @@ function techList(techs, nome) {
   return lista;
 }
 
+function checkRepeated(arrayNumbers){
+  let count = 0;
+  let index = 0;
+  for(let num of arrayNumbers){
+    if(num === arrayNumbers[index]){
+      count += 1
+    }
+    if(count >= 3){
+      return true
+    }
+    index += 1
+  }
+}
+
+
+
+function checkInvalid(arrayNumbers){
+  for(let numb of arrayNumbers){
+    if(numb > 9 || numb < 0){
+      return true
+    }
+  }
+}
+
+
+
 // Desafio 11
 function generatePhoneNumber(numbers) {
   let phonePad = '(**) *****-****';
-
+      
+  if(checkInvalid(numbers) === true || checkRepeated(numbers) === true){
+    return 'não é possível gerar um número de telefone com esses valores';
+  } else if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
   for(let i = 0; i < numbers.length; i += 1){
-
-    if(numbers[i] > 9 || numbers[i] < 0){
-      return 'não é possível gerar um número de telefone com esses valores';
-    } else if (numbers.length !== 11){
-      return 'Array com tamanho incorreto.'
-    }
     phonePad = phonePad.replace('*', numbers[i]);
-    
   }
 
   return phonePad;
-  
 }
 
 console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]));
