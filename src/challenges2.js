dc56fc26f7417b9fb4e499fdd37e3021e89d4ec8
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-31 11:38:43 -0300

    requisito 10 finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function sortObject(element, name) {
+  return {tech: element, name: name};
+}
+function techList(array, name) {
+  let newObject = [];
+  if (array.length === 0 ) return 'vazio';
+  for (element of array.sort()) {
+    newObject.push(sortObject(element, name));
+  }
+  return newObject;
 }
 
+let lista = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+console.log(techList(lista, 'Lucas'));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
