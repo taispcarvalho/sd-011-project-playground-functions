e6c2f72a0f2f9f8718aa887fefcba90c08c483c0
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-31 16:55:03 -0300

    Adiciona Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,43 @@ function techList(techArr, name) {
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
+function repeatedNumberFinder(arrNumbers2) {  
+  let count = 0;
+  for (let index = 0; index < arrNumbers2.length; index += 1) {
+    count = 0;
+    for (let index2 = 1; index2 < arrNumbers2.length; index2 += 1) {
+      if (arrNumbers2[index] === arrNumbers2[index2]) {
+        count += 1;
+        if (count >= 3) {
+          return true;
+        }
+      }
+    }
+  }
 }
+
+function generatePhoneNumber(arrNumbers) {
+  let newStr = '';
+  let repeatedNumber = repeatedNumberFinder(arrNumbers);
+  for (let index = 0; index < arrNumbers.length; index += 1) {
+    if (arrNumbers[index] < 0 || arrNumbers[index] > 9 || repeatedNumber === true) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } if (index === 0) {
+      newStr = `${newStr}(${arrNumbers[0]}`;
+    } else if (index === 1) {
+      newStr = `${newStr + arrNumbers[1]}) `;
+    } else if (index < 6) {
+      newStr += arrNumbers[index];
+    } else if (index === 6) {
+      newStr = `${newStr + arrNumbers[index]}-`;
+    } else if (index < arrNumbers.length) {
+      newStr += arrNumbers[index];
+    }
+  }
+  return newStr;
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
