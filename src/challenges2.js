06352aa594da5780d62cc4977eab21b5966964ff
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-31 16:44:48 -0300

    requisito 12 terminado

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,35 +10,36 @@ function techList(array, name) {
   }
   return newObject;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 // Desafio 11
-function countElement(numbers, element) {
-  let count = 0;
-  for (let number of numbers) {
-    if (number === element) {
-      count += 1;
-    }
-  }
-  if (count >= 3) return 'não é possível gerar um número de telefone com esses valores';
-}
+// function countElement(numbers, element) {
+//   let count = 0;
+//   for (let number of numbers) {
+//     if (number === element) {
+//       count += 1;
+//     }
+//   }
+//   if (count >= 3) return 'não é possível gerar um número de telefone com esses valores';
+// }
 
 // function checkNumbers(number) {
 //   if (number < 0 || number > 9) return 'não é possível gerar um número de telefone com esses valores';
 // }
-console.log(generatePhoneNumber([10, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 10]));
+// console.log(generatePhoneNumber([10, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 10]));
 
-function generatePhoneNumber(array) {
-  let phoneNumber = '()-';
-  for (let number of array) {
-  }
-  return phoneNumber;
+function generatePhoneNumber() {
+//   let phoneNumber = '()-';
+//   for (let number of array) {
+//   }
+//   return phoneNumber;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (c < a + b && c > Math.abs(b - a)) return true;
+  if (a < c + b && a > Math.abs(b - c)) return true;
+  if (b < a + c && b > Math.abs(c - a)) return true;
+  return false
 }
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
