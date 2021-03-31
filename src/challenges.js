013916cda599d13ea2b1b55c0f59ae1195b6f8eb
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-31 11:44:02 -0300

    Add code that change numbers and letters

--- src/challenges.js
+++ src/challenges.js
@@ -105,10 +105,26 @@ function encode(inputString) {
   }
   return outputString;
 }
-console.log(encode('Hai thereou!'));
 
-function decode() {
-  // seu código aqui
+function decode(inputString) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let numbers = [1, 2, 3, 4, 5];
+  let outputString = '';
+  let storeVowel = null;
+  for(let index = 0; index < inputString.length; index += 1) {
+    for (let index2 = 0; index2 < numbers.length; index2++) {
+      if (inputString[index] == numbers[index2]) {
+        storeVowel = vowels[index2];
+      }
+    }
+    if (storeVowel !== null) {
+      outputString += storeVowel;
+      storeVowel = null;
+    } else {
+      outputString += inputString[index];
+    }
+  }
+  return outputString;
 }
 
 module.exports = {
