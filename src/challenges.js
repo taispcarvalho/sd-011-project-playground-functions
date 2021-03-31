107a51caa3b5cfa3d22c7561b4ca77b664c24228
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-31 11:26:38 -0300

    Add code that change letters by numbers

--- src/challenges.js
+++ src/challenges.js
@@ -84,12 +84,29 @@ function fizzBuzz(inputArray) {
   return emptyArray;
 }
 
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
-
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(inputString) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let numbers = [1, 2, 3, 4, 5];
+  let outputString = '';
+  let storeNumber = 0;
+  for(let index = 0; index < inputString.length; index += 1) {
+    for (let index2 = 0; index2 < vowels.length; index2++) {
+      if (inputString[index] === vowels[index2]) {
+        storeNumber = numbers[index2];
+      }
+    }
+    if (storeNumber !== 0) {
+      outputString += storeNumber;
+      storeNumber = 0;
+    } else {
+      outputString += inputString[index];
+    }
+  }
+  return outputString;
 }
+console.log(encode('Hai thereou!'));
+
 function decode() {
   // seu código aqui
 }
