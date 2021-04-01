a7852688821830849d6ee40d019477e42d1ea070
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-01 20:31:55 -0300

    Refatora requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,26 +18,6 @@ function techList(techArr, name) {
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function repeatedNumberCounter(arrNumbers2) {
-  let count = 0;
-  for (let index = 0; index < arrNumbers2.length; index += 1) {
-    count = 0;
-    for (let index2 = 1; index2 < arrNumbers2.length; index2 += 1) {
-      if (arrNumbers2[index] === arrNumbers2[index2]) {
-        count += 1;
-      }
-    }
-  }
-  return count;
-}
-
-function repeatedNumberFinder(arrNumbers2) {
-  let numberDetected = repeatedNumberCounter(arrNumbers2);
-  if (numberDetected >= 3) {
-    return true;
-  }
-}
-
 function telephoneGenerator1(arrTel) {
   let newString = '';
   newString = `(${arrTel[0]}${arrTel[1]}) `;
@@ -59,6 +39,31 @@ function telephoneGenerator2(arrTel) {
   return newString2;
 }
 
+function repeatedNumberCounter(arrNumbers2) {
+  let count = 0;
+  let currentCount = 0;
+  for (let index = 0; index < arrNumbers2.length; index += 1) {
+    count = 0;
+    for (let index2 = 1; index2 < arrNumbers2.length; index2 += 1) {
+      if (arrNumbers2[index] === arrNumbers2[index2]) {
+        count += 1;
+      }
+    }
+    if (currentCount < count) {
+      currentCount = count;
+    }
+  }
+  return currentCount;
+}
+
+function repeatedNumberFinder(arrNumbers2) {
+  let numberDetected = repeatedNumberCounter(arrNumbers2);
+  if (numberDetected >= 3) {
+    return true;
+  }
+}
+console.log(repeatedNumberFinder([5, 2, 2, 2, 5, 3, 7, 2, 8, 9, 0]));
+
 function isValidTelephone(arrTelephone) {
   for (let index = 0; index < arrTelephone.length; index += 1) {
     if (arrTelephone[index] < 0 || arrTelephone[index] > 9) {
@@ -73,7 +78,7 @@ function generatePhoneNumber(arrNumbers) {
   if (arrNumbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  if (validNumber || repeatedNumber) {
+  if (repeatedNumber || validNumber) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
   let telephoneGenerated = telephoneGenerator1(arrNumbers) + telephoneGenerator2(arrNumbers);
@@ -81,7 +86,7 @@ function generatePhoneNumber(arrNumbers) {
   return telephoneGenerated;
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 2, 2, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function checkA(lineA, lineB, lineC) {
