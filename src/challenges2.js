82ec48954797476acbdd588290378a563246cfc6
Leandro Lopes <leandro_carlos@uol.com.br>
2021-04-01 15:26:33 -0300

    exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,24 +1,48 @@
 // Desafio 10
 function techList(techs, name) {
   if (techs.length <= 0) return 'Vazio!';
-  let results = []
+  let results = [];
   techs = techs.sort();
   for (let tech of techs) {
     results.push({
-     tech,
-     name, 
+      tech,
+      name,
     });
   }
-  return results;  
+  return results;
 }
-results = []
-name = 'Leandro'
-console.log(techList(results,name))
+//  results = []
+//  results = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript']
+//  name = 'Leandro'
+//  console.log(techList(results,name))
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function verificarDuplicados(array) {
+  let count = {};
+  array.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+      return count;
 }
+function generatePhoneNumber(array) {
+  let format = '(xx)xxxxx-xxxx';
+  if (array.length != 11) {
+      return "Array com tamanho incorreto.";
+  }
+  let obj = verificarDuplicados(array);
+  for (const property in obj) {
+    if (obj[property] >= 3) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }  
+  } 
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0 || array[i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"; 
+    }
+    format = format.replace ('x', array[i])
+      
+  }
+      return format
+}
+//  console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
