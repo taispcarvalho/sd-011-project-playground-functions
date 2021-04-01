c06669d48241265271b62dc2f7e1b043fbd6cb8a
IsleneGomes <islenegomes@gmail.com>
2021-04-01 16:53:43 -0300

    Tentando os Bonus-4

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,9 +22,9 @@ function generatePhoneNumber(array) {
   let numeroRepetido = [];
   if (array.length !== 11) {
     resultado = ('Array com tamanho incorreto.');
-  } else if (numero < 0 || numero > 9)
+  } else if (numero < 0 || numero > 9){
     resultado = ('não é possível gerar um número de telefone com esses valores');
-  for (let index of array) {
+  }/*for (let index of array) {
     for (let indexNumeroRepetido of array) {
       if (indexNumeroRepetido === index) {
         numeroRepetido[index] = (numeroRepetido[index] || 0) + 1;0
@@ -32,7 +32,7 @@ function generatePhoneNumber(array) {
       resultado = ('não é possível gerar um número de telefone com esses valores');
       }
     }
-  }
+  }*/
   return generatePhoneNumber(resultado);
 }
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
