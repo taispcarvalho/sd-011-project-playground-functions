6b8c69d1f1f45d7ff12f0e2d41b024b6072b98fe
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-01 18:03:50 -0300

    exercicio 9

--- src/challenges.js
+++ src/challenges.js
@@ -75,11 +75,45 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let leters = phrase.split('');
+  let output = [];
+  for (index = 0; index < leters.length; index += 1) {
+    if (leters[index] == 'a') {
+        output.push(1);
+    } else if (leters[index] == 'e') {
+        output.push(2);
+    }else if (leters[index] == 'i') {
+        output.push(3);
+    }else if (leters[index] == 'o') {
+        output.push(4);
+    }else if (leters[index] == 'u') {
+        output.push(5);
+    } else {
+        output.push(leters[index]);
+    }
+  }
+  return output.join('');
 }
-function decode() {
-  // seu código aqui
+function decode(phrase) {
+  let leters = phrase.split('');
+  let output = [];
+  for (index = 0; index < leters.length; index += 1) {
+    if (leters[index] == 1) {
+        output.push('a');
+    } else if (leters[index] == 2) {
+        output.push('e');
+    }else if (leters[index] == 3) {
+        output.push('i');
+    }else if (leters[index] == 4) {
+        output.push('o');
+    }else if (leters[index] == 5) {
+        output.push('u');
+    } else {
+        output.push(leters[index]);
+    }
+  }
+  return output.join('');
 }
 
 module.exports = {
