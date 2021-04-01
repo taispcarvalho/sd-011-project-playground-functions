3389da8d8765c665d9c765ab0c91438b06dad3ec
Wilquer82 <wilquer.fig@gmail.com>
2021-04-01 18:46:06 -0300

    0104-18:46

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,51 @@
+// Desafio 1 ok
+//let valor1=true;
+//let valor2=true;
 function compareTrue(valor1,valor2) {
   return(valor1&&valor2);
 }
+//compareTrue(valor2,valor1);
+// Desafio 2 ok
+//let base = 1;
+//let altura = 51;
+//let areaTriangulo = 0.1;
 function calcArea(base,altura) {
   areaTriangulo = base * altura / 2;
   return(areaTriangulo);
 }
+//calcArea(base,altura);
+//console.log(areaTriangulo);
+// Desafio 3
+//let frase="Wilquer Figueiredo Alves"
 function splitSentence(frase){
   let separado=frase.split(" ");
   return(separado);
 }
+//splitSentence(frase);
+  // seu código aqui
+// Desafio 4
+//let lista=["meu","nome","nao","e","jhonny"];
+//console.log(lista.length);
 function concatName(lista){
   let primPos=lista[0];
   let ultPos=lista[lista.length-1];
   let resposta= ultPos.concat(", ",primPos);
   return(resposta);
   }
+//concatName(lista);
+// Desafio 5
+//let wins=0;
+//let ties=0;
 function footballPoints(wins,ties) {
   let totalpoints=wins*3+ties;
   return(totalpoints);
 }
+//footballPoints(wins,ties);
+// Desafio 6
+//let valores=[0,0,0];
 function highestCount(valores){ 
   let arrumado=valores.sort();
+  //console.log(arrumado);
   let maior=arrumado[arrumado.length-1];
   let quanVezes=0;
     for(let index=0; index<=valores.length-1;index++){
@@ -30,42 +55,55 @@ function highestCount(valores){
     }
  return(quanVezes);
 }
+//highestCount(valores);
 // Desafio 7
 //let mouse=5;
 //let cat1=10;
 //let cat2=0; 
+
   function catAndMouse(mouse,cat1,cat2){
-    cat1=Math.abs(diff(mouse,cat1));
-    cat2=Math.abs(diff(mouse,cat2));
-      if(cat1===cat2){
+    cat1=Math.abs(cat1,mouse);
+    cat2=Math.abs(cat2,mouse);
+    if(cat1===cat2){
       return("Os gatos trombam e o rato foge");
       //console.log("Os gatos trombam e o rato foge");
-    }else if(<cat2){
+    }else if(cat1<cat2){
       return("cat1");
-      //console.log("cat1")
+      //console.log("cat1");
     }else{
       return("cat2");
       //console.log("cat2");
     }
+      
+      
+   
   }
 //catAndMouse(mouse,cat1,cat2);
 //     return("cat1");
 //        }else
         // if (cat2>cat1)
- //       {
+        {
 //          return("cat2");
-   //     }
+        }
         //else{
         //  return("Os gatos trombam e o rato foge");
+
+    
+          
+            
+          
+    
+    
+  
         //}
 //    }else if(cat1>mouse&&cat2>mouse){
 //      if(cat1<cat2){
 //        return("cat1");
 //        }else
          //if (cat1>cat2)
-     //    {
+         {
 //          return("cat2");
-     //   }
+        }
         //else{
         //  return("Os gatos trombam e o rato foge");
         //}
@@ -85,9 +123,9 @@ function highestCount(valores){
 //                return("cat2");
 //              }else
                //if(mouse-cat1<cat2-mouse)
-       //        {
+               {
 //                return("cat1");
-         //     }
+              }
               //else{
               //  return("Os ga tos trombam e o rato foge");
               //}
@@ -99,7 +137,7 @@ function highestCount(valores){
 //    }
 //  }
 //catAndMouse(mouse,cat1,cat2);
-
+// Desafio 8
 function fizzBuzz(numeros){
     let arrayresposta=[0];
   arrayresposta.pop();
@@ -116,6 +154,8 @@ function fizzBuzz(numeros){
   }
   return(arrayresposta);
 }
+//fizzBuzz(numeros);
+// Desafio 9 ok
 function encode(frase){
     frase=frase.replaceAll("a",1);
     frase=frase.replaceAll("e",2);
@@ -132,7 +172,6 @@ function decode(frase) {
     frase=frase.replaceAll("5","u");
     return(frase);
 } 
-
 module.exports = {
   calcArea,
   catAndMouse,
