7cf11ab7bbf9ff0b70d8effa7cc97694c552c92a
Rafael Schmitz <schimitaum@gmail.com>
2021-04-01 16:01:40 -0300

    exercicios 5 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -5,27 +5,39 @@ function compareTrue(bool1, bool2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length-1] + ", " + array[0];
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return 3 * wins + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maior = 0;
+  for (let i in array){
+    if (array[i] > maior) {
+      maior = array[i];
+    }
+  }
+  let counter = 0;
+  for (let n in array){
+    if (array[n] === maior) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 // Desafio 7
