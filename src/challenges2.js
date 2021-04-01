7430a1d35f60282124a59a4956c72028687c8d10
IsleneGomes <islenegomes@gmail.com>
2021-04-01 19:40:16 -0300

    Desafios 10, testando codigo

--- src/challenges2.js
+++ src/challenges2.js
@@ -54,9 +54,6 @@ function generatePhoneNumber(array) {
   return generatePhoneNumber(array);
 }
 
-let arraydeteste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-console.log(generatePhoneNumber(arraydeteste));
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let triaA = lineA < lineB + lineC && lineA > Math.abs(lineB - lineC);
