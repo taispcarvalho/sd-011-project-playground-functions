a207510c670077215a3e370e0f39398e6010a4a4
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-01 12:41:37 -0300

    Desafio 13 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -83,10 +83,22 @@ function triangleCheck(lineA, lineB, lineC) {
 
 }
 
-// Desafio 13
-function hydrate() {
+function hydrate(string) {
   // seu código aqui
+  let regex = /\d+/g; // \d+ = Pega os digitos com 1 caractere ou mais | /g = faz a "pesquisa" na string toda.
+  let stringDigits = string.match(regex);
+  let countTotal = 0;
+  for (n of stringDigits) {
+    countTotal += eval(n);
+  }
+  if (countTotal === 1) {
+    return '1 copo de água';
+  } else {
+    return `${countTotal} copos de água`;
+  }
+
 }
+
 
 module.exports = {
   generatePhoneNumber,
