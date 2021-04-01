c7a14550b0c34a590b3636cf1398a1ef210f24ef
IsleneGomes <islenegomes@gmail.com>
2021-04-01 13:48:37 -0300

    Arrumando erros.5

--- src/challenges2.js
+++ src/challenges2.js
@@ -72,7 +72,7 @@ function triangleCheck(lineA, lineB, lineC) {
   let triaB = lineB < lineA + lineC && lineB > Math.abs(lineA - lineC);
   let triaC = lineC < lineB + lineA && lineC > Math.abs(lineB - lineA);
   if (lineA !== 0 && lineB !== 0 && lineC !== 0) {
-    return triaA || triaB || triaC;
+    return (triaA || triaB || triaC);
   }
 }
 console.log(triangleCheck(10, 6, 2));
