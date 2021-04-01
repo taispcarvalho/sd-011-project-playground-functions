378d0c45915a0232204de3c25c07d4f2960aee31
IsleneGomes <islenegomes@gmail.com>
2021-04-01 10:20:00 -0300

    Arrumando erros.2

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,7 +1,8 @@
- string = '1 cachaça, 5 cervejas e 1 copo de vinho'
-  let numeroCopos = string.match(/\d+/g).map(Number); //me gera um array com os números
+function hydrate(string) {
+  let numeroCopos = string.match(/\d+/g).map(Number);
   let somaCopos = 0;
-  for (let index = 0; index < numeroCopos.length; index += 1){
+  for (let index = 0; index < numeroCopos.length; index += 1) {
     somaCopos += numeroCopos[index];
   }
-  console.log(somaCopos, 'copos de água')
+  return (somaCopos + 'copos de água');
+}
