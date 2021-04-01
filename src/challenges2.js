be26d9d423a08dd90641979675c26998e8b83bfe
marlon307 <marlon-ramosb@hotmail.com>
2021-04-01 13:52:06 -0300

    Desafio 11 update 3

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,10 +11,10 @@ function techList(arr, nm) {
 }
 
 // Desafio 11 Regex Retirado https://gist.github.com/knoonrx/a180f0b7e3039e42c621
-function Repeat(n) {
-  let repeat = 1;
-  for (let index = 0; index < n.length; index += 1) {
-    if (n[index] === n[index + 1]) {
+function Repeat(nIndx, n) {
+  let repeat = 0;
+  for (let index = 0; index < nIndx.length; index += 1) {
+    if (nIndx[index] === n) {
       repeat += 1;
     }
   }
@@ -23,11 +23,12 @@ function Repeat(n) {
 
 function checkNum(n) {
   for (let index of n) {
-    if (index < 0 || index > 9 || Repeat(index) >= 3) {
-      return false;
+    if (index < 0 || index > 9 || Repeat(n, index) > 2) {
+      return true;
     }
   }
 }
+
 // Regex retiradohttps://gist.github.com/knoonrx/a180f0b7e3039e42c621
 function generatePhoneNumber(nPhone) {
   let response = '';
@@ -36,7 +37,7 @@ function generatePhoneNumber(nPhone) {
 
   if (nPhone.length !== 11) {
     response = 'Array com tamanho incorreto.';
-  } else if (checkNum(nPhone) === false) {
+  } else if (checkNum(nPhone) === true) {
     response = 'não é possível gerar um número de telefone com esses valores';
   } else {
     for (let index of nPhone) {
@@ -47,7 +48,7 @@ function generatePhoneNumber(nPhone) {
   return response;
 }
 
-console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
+console.log(generatePhoneNumber([0, 2, 3, 4, 5, 2, 7, 2, 1, 3, 4]));
 
 // Desafio 12
 function triangleCheck() {
