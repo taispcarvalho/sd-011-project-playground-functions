e459eb4e400b4055853294e914f7bee225aff797
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-04-01 14:04:54 -0300

    Exercicio 10. Feito validaçao para array vzo.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,36 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+/* 10 - Crie uma função de Lista de tecnologias
+Crie uma função que recebe um array de nomes de tecnologias que você quer aprender. Essa função deve receber também um segundo parâmetro chamado name com um nome. 
+Para cada tecnologia no array, crie um objeto com a seguinte estrutura:
+{
+  tech: "NomeTech",
+  name: name
+} 
+Caso o array venha vazio sua função deve retornar 'Vazio!'*/
+function techList(techArray, name) {
+  let objectArray = [];
+  if (techArray.length < 0){
+    return 'Vazio!';
+  }
+  for ( let index = 0; index < techArray.length; index += 1){
+    let object = {
+      tech: techArray[index],
+      name
+    }
+    objectArray.push(object);
+  }
+  return (techArray.length === 0) ? 'Vazio!' : objectArray;
 }
+console.log(techList([],"Lucas"));
+/* O que será verificado:
+Retorne uma lista de objetos ordenados quando é passada uma lista com 5 tecnologias deve
+Retorne a mensagem de erro 'Vazio!' quando a lista tiver com 0 tecnologias
+Teste: 
+ */
 
-// Desafio 11
+
+
+// Desafio 11--------------------------------------------------------------
 function generatePhoneNumber() {
   // seu código aqui
 }
