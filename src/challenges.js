d83b6ce73f087876f3aede68ba117d071beaba19
Hamander <1hamander@gmail.com>
2021-04-01 17:45:34 -0300

    Desafio_9 Lint_Fixes. Closing curly braces.

--- src/challenges.js
+++ src/challenges.js
@@ -86,24 +86,20 @@ function encode(string)
   let stringArray = string.toLowerCase().split('');
 
   for (let index = 0; index < stringArray.length; index += 1) {
-    if (stringArray[index] === 'a') 
-    {
+    
+    if (stringArray[index] === 'a') {
       stringArray[index] = '1';
-    } 
-    else if (stringArray[index] === 'e') 
-    {
+
+    } else if (stringArray[index] === 'e') {
       stringArray[index] = '2';
-    }
-    else if (stringArray[index] === 'i') 
-    {
+
+    } else if (stringArray[index] === 'i') {
       stringArray[index] = '3';
-    }
-    else if (stringArray[index] === 'o') 
-    {
+
+    } else if (stringArray[index] === 'o') {
       stringArray[index] = '4';
-    }
-    else if (stringArray[index] === 'u') 
-    {
+
+    } else if (stringArray[index] === 'u') {
       stringArray[index] = '5';
     }
   }
@@ -116,25 +112,22 @@ function decode(string)
   let stringArray = string.toLowerCase().split('');
 
   for (let index = 0; index < stringArray.length; index += 1) {
-    if (stringArray[index] === '1') 
-    {
+    
+    if (stringArray[index] === '1') {
       stringArray[index] = 'a';
-    } 
-    else if (stringArray[index] === '2') 
-    {
+
+    } else if (stringArray[index] === '2') {
       stringArray[index] = 'e';
-    }
-    else if (stringArray[index] === '3') 
-    {
+
+    } else if (stringArray[index] === '3') {
       stringArray[index] = 'i';
-    }
-    else if (stringArray[index] === '4') 
-    {
+
+    } else if (stringArray[index] === '4') {
       stringArray[index] = 'o';
-    }
-    else if (stringArray[index] === '5') 
-    {
+
+    } else if (stringArray[index] === '5') {
       stringArray[index] = 'u';
+
     }
   }
   let stringfinal = stringArray.join('');
