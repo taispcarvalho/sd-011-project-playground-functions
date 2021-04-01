7dd40711905b81bac60df357059daeefab7ef85b
Alberto Candido <albertocandido98@gmail.com>
2021-04-01 17:39:42 -0300

    Segunda tentativa para arrumar requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,6 @@ function techList(techs, name) {
   return newList;
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
-
 // Desafio 11
 function checkRepeated(param1) {
   let countrepeated = 0;
@@ -34,7 +32,7 @@ function checkRepeated(param1) {
     }
     countrepeated = 0;
   }
-  return result > 3;
+  return result >= 3;
 }
 
 function generatePhoneNumber(numbers) {
@@ -51,8 +49,6 @@ function generatePhoneNumber(numbers) {
   }
   return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
 }
-
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
