a6487d9fa3c203c51dfce0afcfba773fc414da45
IsleneGomes <islenegomes@gmail.com>
2021-04-01 19:37:09 -0300

    Desafios 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,15 +42,20 @@ function generatePhoneNumber(array) {
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
+  if (array.length === 11) {
+    let primeiroNUmero = ('(' + array[0] + array[1]);
+    let numeroMeio = (') ' + array[2] + array[3] + array[4] + array[5] + array[6]);
+    let ultimoNumero = (' - ' + array[7] + array[8] + array[9] + array[10]);
+    return (primeiroNUmero + numeroMeio + ultimoNumero);
+  }
   if (!numeroZeroNove(array) || repeticaoNumero(array)) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
-  let primeiroNUmero = ('(' + array[0] + array[1] + ') ');
-  let numeroMeio = (array[2] + array[3] + array[4] + array[5] + array[6]);
-  let ultimoNumero = (' - ' + array[7] + array[8] + array[9] + array[10]);
-
-  return (primeiroNUmero + numeroMeio + ultimoNumero);
+  return generatePhoneNumber(array);
 }
+
+let arraydeteste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+console.log(generatePhoneNumber(arraydeteste));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,14 +1,57 @@
-function hydrate(string) {
-  let numeroCopos = string.match(/\d+/g).map(Number);
-  let somaCopos = 0;
-  for (let index = 0; index < numeroCopos.length; index += 1) {
-    somaCopos += numeroCopos[index];
+function techList(array, name) {
+  let test = [];
+  if (array.length <= 0) {
+    return 'Vazio!';
   }
-  if (somaCopos > 1) {
-    return (somaCopos + ' copos de água');
-  } if (somaCopos >= 0 || somaCopos < 1) {
-    return (somaCopos + ' copo de água');
+  for (let index = 0; index < array.length; index += 1) {
+    test.push = ({
+      tech: array[index],
+      pessoa: name,
+    }
+    );
   }
-  return hydrate;
+  return test;
 }
-console.log(hydrate('cachaça'));
+
+// Desafio 11
+function numeroZeroNove(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return false;
+    }
+  }
+  return true;
+}
+function repeticaoNumero(array) {
+  let contador = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    for (let indexCont = 0; indexCont < array.length; indexCont += 1) {
+      if (array[indexCont] === array[index]) {
+        contador += 1;
+      }
+    }
+    if (contador >= 3) {
+      return false;
+    }contador = 0;
+  }
+  return true;
+}
+
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (array.length === 11) {
+    let primeiroNUmero = ('(' + array[0] + array[1]);
+    let numeroMeio = (') ' + array[2] + array[3] + array[4] + array[5] + array[6]);
+    let ultimoNumero = (' - ' + array[7] + array[8] + array[9] + array[10]);
+    return (primeiroNUmero + numeroMeio + ultimoNumero);
+  }
+  if (!numeroZeroNove(array) || repeticaoNumero(array)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return generatePhoneNumber(array);
+}
+
+let arraydeteste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+console.log(generatePhoneNumber(arraydeteste));
