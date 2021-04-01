404165fda721e1294bc94cbd012bdf3d0f28527d
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-01 13:48:58 -0300

    Desafio 11 Finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -35,11 +35,7 @@ function generatePhoneNumber(arrayOfNumber) {
   }
   return 'Array com tamanho incorreto.';
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 5])); // -> 'Array com tamanho incorreto.'
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1])); // -> 'não é possível gerar um número de telefone com esses valores'
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10])); // -> 'não é possível gerar um número de telefone com esses valores'
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0])); // -> 'não é possível gerar um número de telefone com esses valores'
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])); // -> (12) 34567-8901
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
