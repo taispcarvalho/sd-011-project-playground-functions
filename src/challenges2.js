21d0e99679cf4298a2316c511a89fe0a7c5042bc
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-01 18:08:22 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,9 +5,9 @@ function techList(arrayTechs, name) {
   arrayTechs = arrayTechs.sort();
 
   for (let tech of arrayTechs) {
-    returnTechs.push({tech, name});
+    returnTechs.push({ tech, name });
   }
-return returnTechs;
+  return returnTechs;
 }
 
 let array10 = ['React', 'Node JS', 'Phyton', 'Rest', 'CSS'];
@@ -15,9 +15,45 @@ let name10 = 'Ivaldo';
 console.log(techList(array10, name10));
 
 // Desafio 11
-function generatePhoneNumber() {
-  
+
+function zeroNove(number) {
+  return number < 0 || number > 9;
 }
+
+function maiorIgualTres(arr, number) {
+  let repeat = 0;
+  for (let index = 0; index < phone.length; index += 1) {
+    if (number === arr[index]) {
+      repeat += 1;
+    }
+    if (repeat >= 3) return true;
+  }
+  return false; 
+}
+
+function generatePhoneNumber(phone) {
+  if (phone.length !== 11) return "Array com tamanho incorreto.";
+
+  for (let index = 0; index < phone.length; index += 1) {
+    if (zeroNove(phone[index]) || maiorIgualTres(phone, phone[index])) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let numPhone = '(';
+  for (let index = 0; index < phone.length; index += 1) {
+    if (numPhone.length === 3) {
+      numPhone += ') ';
+    } else if (numPhone.length === 10) {
+      numPhone += '-';
+    }
+    numPhone += phone[index];
+  }
+  return numPhone;
+}
+
+let phone = [3, 3, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+console.log(generatePhoneNumber(phone));
 
 // Desafio 12
 function triangleCheck() {
