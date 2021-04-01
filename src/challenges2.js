b70c9c2402c2ab9df99f28390dc94cb8b659dbd6
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-01 20:19:50 -0300

    Feito o requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,9 +19,47 @@ let person = 'Lucas';
 console.log(techList(arrayTechs, person));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function countRepeaterNumber(array) {
+  let count;
+  let saveMaxCount = 1;
+  for (let index = 0; index < array.length; index += 1) {
+    let save = array[index];
+    count = 1;
+    for (let indexB = index + 1; indexB < array.length; indexB += 1) {
+      if (save === array[indexB]) {
+        count += 1;
+      }
+    }
+    if (count > saveMaxCount) {
+      saveMaxCount = count;
+    }
+  }
+  return saveMaxCount;
 }
+
+function generatePhoneNumber(array) {
+  let phone = '';
+  let countStorage = countRepeaterNumber(array);
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9 || countStorage >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } if (index === 0) {
+      phone += `(${array[index]}`;
+    } else if (index === 1) {
+      phone += `${array[index]}) `;
+    } else if (index === 6) {
+      phone += `${array[index]}-`;
+    } else {
+      phone += array[index];
+    }
+  } return phone;
+}
+
+let numberArray = [5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0];
+console.log(generatePhoneNumber(numberArray));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
