47c64b3eee21f25a4830f05f270bde16c7987bdd
Marcelo Leite <marsleite@gmail.com>
2021-04-01 20:53:35 -0300

    refatorando o codigo para atender o requisito 10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,11 +2,17 @@
 function techList(techno, name) {
   let techOrdeal = techno.sort();
   let resulTech = [];
+  if (techOrdeal.length === 0) {
+    return 'Vazio!';
+  }
   for (let index = 0; index < techOrdeal.length; index += 1) {
     resulTech.push({ tech: techOrdeal[index], name: name });
   }
   return resulTech;
 }
+let tec = ['JS', 'Angular', 'React', 'HTML', 'CSS'];
+let nome = 'Marcelo';
+console.log(techList(tec, nome));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
