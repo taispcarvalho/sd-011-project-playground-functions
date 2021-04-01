efaa7f30da70250bca2d65b87811026fda769704
Alberto Candido <albertocandido98@gmail.com>
2021-04-01 16:19:06 -0300

    Requisito 11 - recebe 11 numeros e verifica condiçoes para formar numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,35 +1,70 @@
 // Desafio 10
-function techList(name) {
+function techList() {
   // seu código aqui
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkrepeated(param1) {
+  let countrepeated = 0;
+  let result = 0;
+  for (let index = 0; index < param1.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < param1.length; secondIndex += 1) {
+      if (param1[index] === param1[secondIndex]) {
+        countrepeated += 1;
+      }
+    }
+    if (result < countrepeated) {
+      result = countrepeated;
+    }
+    countrepeated = 0;
+  }
+  return result > 3;
+}
+// console.log(checkrepeated([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 9, 9, 9]));
+
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length > 11) {
+  return 'Array com tamanho incorreto.'
+  }
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9 || checkrepeated(numbers)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } else if (checkrepeated(numbers)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${numbers[0]}${numbers[1]})${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
 }
 
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 1, 1, 9, 8]));
+
 // Desafio 12
-function triangleCheck(lineA,lineB,lineC) {
+function triangleCheck(lineA, lineB, lineC) {
+  let result;
   if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
-    return true;
+    result = true;
   } else {
-    return false;
+    result = false;
   }
+  return result;
 }
 
 // Desafio 13
 function hydrate(param1) {
   let reg = /\d+/g;
   let cupsOfWater = param1.match(reg);
-  let result = 0;
-  for (index = 0; index < cupsOfWater.length; index += 1) {
-    result += parseInt(cupsOfWater[index]);
+  let sumResult = 0;
+  let result;
+  for (let index = 0; index < cupsOfWater.length; index += 1) {
+    sumResult += parseInt(cupsOfWater[index]);
   }
-  if (result > 1) {
-    return `${result} copos de água`;
+  if (sumResult > 1) {
+    result = `${sumResult} copos de água`;
   } else {
-    return `${result} copo de água`;
+    result = `${sumResult} copo de água`;
   }
+  return result;
 }
 
 module.exports = {
