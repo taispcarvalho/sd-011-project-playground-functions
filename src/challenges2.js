8e2df95035f187d85dc17d7294903326f6c7e2ff
IsleneGomes <islenegomes@gmail.com>
2021-03-31 21:35:19 -0300

    Arrumando o codigo - Desafio 10node /home/islenegomes/Documentos/Trybe/Projetos/sd-011-project-playground-functions/src/tempCodeRunnerFile.js

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,29 +1,38 @@
 // Desafio 10
 function techList(array, name) {
   let test = [];
-  if (array.length <= 0){
+  if (array.length <= 0) {
     return 'Vazio!';
   }
-  for(let index = 0; index < array.length; index += 0){
-    test.push = ({
-    tech: '',
-    name: name,
-    }
-  );
+  for (let index = 0; index < array.length; index += 0) {
+      test.push = ({
+      tech: array[index],
+      name: name,
+      }
+    );
   }
    return test;
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  
-}
-
-// Desafio 12
-function triangleCheck() {
   // seu código aqui
 }
 
+// Desafio 12
+function triangleCheck(lineA, lineB, lineC) {
+let triaA = lineA < lineB + lineC && lineA > Math.abs(lineB - lineC);
+let triaB = lineB < lineA + lineC && lineB > Math.abs(lineA - lineC);
+let triaC = lineC < lineB + lineA && lineC > Math.abs(lineB - lineA);
+
+  if(triaA|| triaB || triaC) {
+    let resultado = true;
+  } else {
+    let resultado = false;
+  }
+  return resultado;
+}
+console.log(triangleCheck(10, 14, 8))
 // Desafio 13
 function hydrate() {
   // seu código aqui
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,14 +1,13 @@
-function techList(array, name) {
-  let test = [];
-  if (array.length <= 0){
-    return 'Vazio!';
+function triangleCheck(lineA, lineB, lineC) {
+let triaA = lineA < lineB + lineC && lineA > Math.abs(lineB - lineC);
+let triaB = lineB < lineA + lineC && lineB > Math.abs(lineA - lineC);
+let triaC = lineC < lineB + lineA && lineC > Math.abs(lineB - lineA);
+
+  if(triaA|| triaB || triaC) {
+    let resultado = true;
+  } else {
+    let resultado = false;
   }
-  for(let index = 0; index < array.length; index += 0){
-    test.push = ({
-    tech: '',
-    name: name,
-    }
-  );
-  }
-   return test;
+  return resultado;
 }
+console.log(triangleCheck(10, 14, 8))
