d7dd231a8b0faf336f9595f451dddfb8c7c71521
Filizzola <juliofilizzola@hotmail.com>
2021-04-01 10:25:03 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,21 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
+  let retorno = [];
+  let retorno1 = array.sort();
+  if (retorno1.length !== 0) {
+    for (let index in retorno1) {
+      retorno[index] = {
+      tech: array[index],
+      name: name,
+      } 
+    }
+    return retorno;
+  } else {
+    return 'Vazio!';
+  }
   // seu código aqui
 }
-
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Julio'));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
