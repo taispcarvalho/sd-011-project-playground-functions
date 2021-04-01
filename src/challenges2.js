11c57a3a06a84fe1627c32849fc869279b17ddf9
IsleneGomes <islenegomes@gmail.com>
2021-04-01 16:57:42 -0300

    Tentando os Bonus-5

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,27 +16,8 @@ function techList(array, name) {
 
 // Desafio 11
 function generatePhoneNumber(array) {
-  let resultado = '';
-  let index = 0;
-  let numero = array[index];
-  let numeroRepetido = [];
-  if (array.length !== 11) {
-    resultado = ('Array com tamanho incorreto.');
-  } else if (numero < 0 || numero > 9){
-    resultado = ('não é possível gerar um número de telefone com esses valores');
-  }/*for (let index of array) {
-    for (let indexNumeroRepetido of array) {
-      if (indexNumeroRepetido === index) {
-        numeroRepetido[index] = (numeroRepetido[index] || 0) + 1;0
-      } if (numeroRepetido[index] >= 3) {
-      resultado = ('não é possível gerar um número de telefone com esses valores');
-      }
-    }
-  }*/
-  return generatePhoneNumber(resultado);
+  
 }
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
-console.log(generatePhoneNumber(array));
 
 function numeroTelefone(array) {
   let numeroDeTelefone = generatePhoneNumber(array);
