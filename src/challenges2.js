fbde8b7120360734802c780e90eaf4908eaee979
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-01 17:05:36 -0300

    Refatora complexidade requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,32 +33,50 @@ function repeatedNumberFinder(arrNumbers2) {
   }
 }
 
+function isValidTelephone(arrTelephone) {
+  for (let index = 0; index < arrTelephone.length; index += 1) {
+    if (arrTelephone[index] < 0 || arrTelephone[index] > 9) {
+      return true;
+    }
+  }
+}
+
+function telephoneGenerator1(arrTel) {
+  let newString = '';
+  newString = `(${arrTel[0]}${arrTel[1]}) `;
+  for (let index = 1; index < 7; index += 1) {
+    if (index > 1 && index < 7) {
+      newString += arrTel[index];
+    }
+  }
+  return newString;
+}
+
+function telephoneGenerator2(arrTel) {
+  let newString2 = '-';
+  for (let index = 7; index < 11; index += 1) {
+    if (index > 6) {
+      newString2 += arrTel[index];
+    }
+  }
+  return newString2;
+}
+
 function generatePhoneNumber(arrNumbers) {
-  let newStr = '';
   let repeatedNumber = repeatedNumberFinder(arrNumbers);
+  let validNumber = isValidTelephone(arrNumbers);
   if (arrNumbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  for (let index = 0; index < arrNumbers.length; index += 1) {
-    if (arrNumbers[index] < 0 || arrNumbers[index] > 9 || repeatedNumber === true) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-    if (index === 0) {
-      newStr = `${newStr}(${arrNumbers[0]}`;
-    } else if (index === 1) {
-      newStr = `${newStr + arrNumbers[1]}) `;
-    } else if (index < 6) {
-      newStr += arrNumbers[index];
-    } else if (index === 6) {
-      newStr = `${newStr + arrNumbers[index]}-`;
-    } else if (index < arrNumbers.length) {
-      newStr += arrNumbers[index];
-    }
+  if (validNumber || repeatedNumber) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
-  return newStr;
+  let telephoneGenerated = telephoneGenerator1(arrNumbers) + telephoneGenerator2(arrNumbers);
+
+  return telephoneGenerated;
 }
 
-console.log(generatePhoneNumber([]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function checkA(lineA, lineB, lineC) {
