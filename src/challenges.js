620998560d647b684128c282c609a0ec5985820a
Rafael Schmitz <schimitaum@gmail.com>
2021-04-01 14:39:55 -0300

    exercicios 3 e 4

--- src/challenges.js
+++ src/challenges.js
@@ -10,40 +10,12 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  let positions = [];
-  let output = [];
-  for(let i in string){
-    if(string[i] === " "){
-      positions.push(i);    //guarda posição dos separadores
-    }
-  }
-  for(let n = 0; n <= positions.length; n+=1){
-    //define intervalos envolvendo posicao dos separadores
-    let start;
-    let end;
-    //inicio do intervalo
-    if(n === 0){
-      start = 0;
-    }
-    else{
-      start = positions[n-1];
-    }
-    //fim do intervalo
-    if(n === positions.length){
-      end = string.length;
-    }
-    else{
-      end = positions[n];
-    }
-    output.push(string.slice(start,end));
-  }
-  return output;
+  return string.split(" ");
 }
-console.log(splitSentence("vamo que vamo"));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length-1] + ", " + array[0];
 }
 
 // Desafio 5
