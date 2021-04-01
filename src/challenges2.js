908fd18a0e1117f2e9b8d2d20d4767b9ccad573e
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-01 02:10:11 -0300

    Finish bonus requirement 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,6 +9,7 @@ function maySwap(arr, index) {
   }
 }
 
+// Auxiliary function for requirement 10
 function bubbleSort(arr) {
   let arrSize = arr.length;
   let indexA;
@@ -44,15 +45,52 @@ function techList(techs, name) {
 }
 
 // Auxiliary function for Desafio 11
-function exceedNumberRepetition(number) {
-  maxRepetition = 2;
+function getBiggestNumber(numbers) {
+  let index;
+  let biggestNumber;
+
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  for (index = 0; !numbers[index]; index += 1) ;
+  biggestNumber = numbers[index];
+
+  for (let number of numbers) {
+    if (number > biggestNumber) {
+      biggestNumber = number;
+    }
+  }
+
+  return biggestNumber;
 }
 
 // Auxiliary function for Desafio 11
-function numberInValidRange(number) {
-  let lowerBound = 0;
-  let upperBound = 9;
+function getBiggestRepetition(numbers) {
+  let numberFrequencies = [];
 
+  for (let number of numbers) {
+    if (!numberFrequencies[number]) {
+      numberFrequencies[number] = 1;
+    } else {
+      numberFrequencies[number] += 1;
+    }
+  }
+
+  return getBiggestNumber(numberFrequencies);
+}
+
+// Auxiliary function for Desafio 11
+function exceedMaxNumberRepetition(numbers, maxNumberRepetition) {
+  if (getBiggestRepetition(numbers) > maxNumberRepetition) {
+    return true;
+  }
+
+  return false;
+}
+
+// Auxiliary function for Desafio 11
+function numberInValidRange(number, lowerBound, upperBound) {
   if (number >= lowerBound && number <= upperBound) {
     return true;
   }
@@ -61,27 +99,49 @@ function numberInValidRange(number) {
 }
 
 // Auxiliary function for Desafio 11
-function allNumbersInValidRange(numbers) {
-  let foundInvalidNumber = false;
+function allNumbersInValidRange(numbers, lowerBound, upperBound) {
+  let answer = true;
 
-  for (let index = 0; index < numbers.length && !foundInvalidNumber; index += 1) {
-    foundInvalidNumber = !numberInValidRange(numbers[index]);
+  for (let index = 0; index < numbers.length && answer; index += 1) {
+    answer = numberInValidRange(numbers[index], lowerBound, upperBound);
   }
 
-  return foundInvalidNumber;
+  return answer;
+}
+
+// Auxiliary function for Desafio 11
+function formatPhoneNumber(numbers, index) {
+  if (index === 1) {
+    return `${numbers[index]}) `;
+  }
+
+  if (index === 6) {
+    return `${numbers[index]}-`;
+  }
+
+  return numbers[index];
 }
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
+  const lowerBound = 0;
+  const upperBound = 9;
+  const maxNumberRepetition = 2;
+  let phoneNumber = '(';
+
   if (numbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-
-  if (!allNumbersInValidRange(numbers) || exceedNumberRepetition(numbers)) {
-    return "não é possível gerar um número de telefone com esses valores";
+  let allInValidRange = allNumbersInValidRange(numbers, lowerBound, upperBound);
+  if (!allInValidRange || exceedMaxNumberRepetition(numbers, maxNumberRepetition)) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
 
+  for (let index = 0; index < numbers.length; index += 1) {
+    phoneNumber += formatPhoneNumber(numbers, index);
+  }
 
+  return phoneNumber;
 }
 
 // Desafio 12
