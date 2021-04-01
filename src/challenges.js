e5c97304e5359068e1fba916e86d4a7dd9c8313c
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-04-01 13:01:27 -0300

    STRING QUE TAVA FALTANDO DISGRAÇA

--- src/challenges.js
+++ src/challenges.js
@@ -45,9 +45,9 @@ function catAndMouse(mouse, cat1, cat2) {
  let distanciaCat2 = Math.abs (mouse - cat2);
 
  if (distanciaCat1 < distanciaCat2) {
-   return cat1;
+   return 'cat1';
  } else if (distanciaCat2 < distanciaCat1) {
-   return cat2;
+   return 'cat2';
  } else {
    return 'os gatos trombam e o rato foge';
  }
