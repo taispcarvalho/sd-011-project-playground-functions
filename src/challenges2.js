846411383b8d74578a0ae67db49ea564e615f8e0
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-01 19:07:58 -0300

    Resolve problema de identacao do desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,15 +1,15 @@
 // Desafio 10
 function techList(array, myName) {
   let techName = [];
-// Nessa parte, para resolver o problema do lint ('Guarding for-in (guard-for-in)') utilizei este material: https://eslint.org/docs/2.0.0/rules/guard-for-in
-for (let index in array.sort()) {
-  if ({}.hasOwnProperty.call(array, index)) {
-    techName[index] = {
-      tech: array[index],
-      name: myName,
-    };
+  // Nessa parte, para resolver o problema do lint ('Guarding for-in (guard-for-in)') utilizei este material: https://eslint.org/docs/2.0.0/rules/guard-for-in
+  for (let index in array.sort()) {
+    if ({}.hasOwnProperty.call(array, index)) {
+      techName[index] = {
+        tech: array[index],
+        name: myName,
+      };
+    }
   }
-} 
   if (techName.length === 0) {
     return 'Vazio!';
   }
@@ -19,11 +19,9 @@ for (let index in array.sort()) {
 console.log(techList([], 'Vitor'));
 
 // Desafio 11
-function generatePhoneNumber(array) {
+function generatePhoneNumber() {
   // seu código aqui
 }
-
-console.log
 
 // Desafio 12
 function triangleCheck() {
