efaed42e5d2a2a03daa0bdc015435d60f94f52eb
Vinicius <viniciusfod@gmail.com>
2021-04-01 01:30:31 -0300

    adiciona desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -28,10 +28,15 @@ function concatName(concat) {
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties * 1;
 }
+console.log(footballPoints(14, 8))
+console.log(footballPoints(1, 2))
+console.log(footballPoints(0, 0))
+
 
 // Desafio 6
 function highestCount() {
