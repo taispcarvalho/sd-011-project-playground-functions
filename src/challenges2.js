b1e0b558c314a56d810a38505f1d841a628ee7cc
igordmf <igordanielmf@gmail.com>
2021-04-01 14:28:22 -0300

    Refazendo desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -82,7 +82,7 @@ function generatePhoneNumber(arrayOfNumbers) {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if ((lineA < lineB + lineC) && (lineB < lineA + lineC) && (lineC < lineA + lineB)) {
+  if ((lineA < lineB + lineC) && (lineB < lineA + lineC) && (lineC < lineA + lineB) && (lineA > Math.abs(lineB - lineC)) && (lineB > Math.abs(lineA - lineC)) && (lineC > Math.abs(lineA - lineB))) {
     return true;
   }
   return false;
