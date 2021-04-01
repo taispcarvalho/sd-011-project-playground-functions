80e41ccd358fa25d9222c65c6f5975e1e9e55470
IsleneGomes <islenegomes@gmail.com>
2021-03-31 21:55:30 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,10 +25,11 @@ let triaA = lineA < lineB + lineC && lineA > Math.abs(lineB - lineC);
 let triaB = lineB < lineA + lineC && lineB > Math.abs(lineA - lineC);
 let triaC = lineC < lineB + lineA && lineC > Math.abs(lineB - lineA);
 
-return triaA
+return triaA || triaB || triaC;
+
 }
   
-console.log(triangleCheck(10, 14, 8))
+console.log(triangleCheck(10, 6, 2))
 
 // Desafio 13
 function hydrate() {
