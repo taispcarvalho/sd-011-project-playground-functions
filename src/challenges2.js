b885a951dc0de81c3db9a8156be28bc7758a9679
IsleneGomes <islenegomes@gmail.com>
2021-04-01 09:34:14 -0300

    Arrumando erros

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,7 +7,7 @@ function techList(array, name) {
   for (let index = 0; index < array.length; index += 0) {
     test.push = ({
       tech: array[index],
-      name: nm,
+      name: name,
     }
     );
   }
@@ -32,12 +32,13 @@ console.log(triangleCheck(10, 6, 2))
 
 // Desafio 13
 function hydrate(string) {
+  string = '1 cachaça, 5 cervejas e 1 copo de vinho'
   let numeroCopos = string.match(/\d+/g).map(Number); //me gera um array com os números
   let somaCopos = 0;
   for (let index = 0; index < numeroCopos.length; index += 1){
     somaCopos += numeroCopos[index];
-  }
-  return (somaCopos, 'copos de água');
+    }
+  return (somaCopos + 'copos de água');
   }
 
 module.exports = {
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,9 +1,7 @@
-function hydrate(string) {
-  string = '1 cachaça, 5 cervejas e 1 copo de vinho'
+ string = '1 cachaça, 5 cervejas e 1 copo de vinho'
   let numeroCopos = string.match(/\d+/g).map(Number); //me gera um array com os números
   let somaCopos = 0;
   for (let index = 0; index < numeroCopos.length; index += 1){
     somaCopos += numeroCopos[index];
   }
-  return (somaCopos, 'copos de água');
-  }
+  console.log(somaCopos, 'copos de água')
