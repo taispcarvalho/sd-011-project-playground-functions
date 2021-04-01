de8df1ee44e6ff1090f9fea0d6aeb07553586382
Savio Moraes <smmsavio@hotmail.com>
2021-04-01 15:51:08 -0300

    Inicia resolucao do requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, name) {
+  if (arrayTech.length <= 0) {
+    return 'Vazio!'
+  }
+  let newArrayTech = [];
+  arrayTech = arrayTech.sort();
+  for (let tech = 0; tech < arrayTech.length; tech += 1) {
+    newArrayTech.push({tech: arrayTech[tech], name: name,});
+  }
+  return newArrayTech;
 }
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+console.log(techList([], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
