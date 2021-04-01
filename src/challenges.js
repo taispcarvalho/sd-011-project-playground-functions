ea41301c53c6227f97b99cc8f38053fd0a7f9ac3
Renan <renan.vamo@gmail.com>
2021-04-01 15:29:16 -0300

    Ajusta lint do ex4

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,9 @@ function splitSentence(str) {
 
 // Desafio 4
 function concatName(array) {
-  let firstName = array[array.length-1]
-  let lastName = array[0]
-  return `${firstName}, ${lastName}`
+  let firstName = array[array.length - 1];
+  let lastName = array[0];
+  return `${firstName}, ${lastName}`;
 }
 
 // Desafio 5
