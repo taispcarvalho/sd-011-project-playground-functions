1366b0715854bbb7de6f8558ef8fe1b8fedad1bc
Guinho Anjos <iago.anjos@hotmail.com>
2021-04-01 12:40:48 -0300

    desafio 11 generate phone feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,50 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, nameTech) {
+  for(let index in arrayTech){
+  let objectTech  = {name:nameTech}
+  objectTech = {tech:arrayTech[index]}
+  }
+return objectTech
 }
-
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"]
+"Lucas"))
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numberPhone) {
+  let numberOrganized = "("
+  let result = 0;
+  let count = 0;
+
+  for(index = 0; index<numberPhone.length; index+=1){
+    for(index2 = 1; index<numberPhone.length; index+=1){
+      if(numberPhone[index] === numberPhone[index2]){
+      count += 1;
+      }
+    }
+    if(count >= 3){
+      result = "não é possível gerar um número de telefone com esses valores";
+      break;
+    } 
+    if(numberPhone.length !== 11){
+      result = "Array com tamanho incorreto.";
+      break;
+    }else{
+      result=numberOrganized
+    }
+    if(numberPhone[index]<0 || numberPhone[index]>9){
+      result ="não é possível gerar um número de telefone com esses valores";
+      break;
+    }else{
+      result = numberOrganized
+    }
+    if(index === 2){
+    numberOrganized += ')'
+   }
+   if(index === 6){
+     numberOrganized +='-'
+   }
+   numberOrganized +=numberPhone[index];
+  }
+return result
 }
 
 // Desafio 12
@@ -14,8 +53,9 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let waterCup += string.replace(/\D/g, "");
+  return console.log(waterCup + ' copos de água')
 }
 
 module.exports = {
