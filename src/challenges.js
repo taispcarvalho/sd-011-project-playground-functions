e4aea6963df0a25d8d8ef960ae6246b432629581
Hamander <1hamander@gmail.com>
2021-04-01 17:48:13 -0300

    Desafio_9 Lint_Fixes. Closing curly braces.

--- src/challenges.js
+++ src/challenges.js
@@ -89,16 +89,12 @@ function encode(string)
     
     if (stringArray[index] === 'a') {
       stringArray[index] = '1';
-
     } else if (stringArray[index] === 'e') {
       stringArray[index] = '2';
-
     } else if (stringArray[index] === 'i') {
       stringArray[index] = '3';
-
     } else if (stringArray[index] === 'o') {
       stringArray[index] = '4';
-
     } else if (stringArray[index] === 'u') {
       stringArray[index] = '5';
     }
@@ -115,19 +111,14 @@ function decode(string)
     
     if (stringArray[index] === '1') {
       stringArray[index] = 'a';
-
     } else if (stringArray[index] === '2') {
       stringArray[index] = 'e';
-
     } else if (stringArray[index] === '3') {
       stringArray[index] = 'i';
-
     } else if (stringArray[index] === '4') {
       stringArray[index] = 'o';
-
     } else if (stringArray[index] === '5') {
       stringArray[index] = 'u';
-
     }
   }
   let stringfinal = stringArray.join('');
