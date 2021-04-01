56f2cf0a624b74e8cba1e89ef1cafbf3b6078048
Jessica Arruda <sicarruda@gmail.com>
2021-04-01 18:17:45 -0300

    Correçao desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -49,18 +49,15 @@ function highestCount(array) {
 // Desafio 7
 
 function catAndMouse(mouse, cat1, cat2) {
-  let firthCat, secondCat;
+  let firthCat;
+  let secondCat;
   if (cat1 < mouse && cat2 > mouse) {
-    firthCat = mouse - cat1;
-    secondCat = cat2 - mouse;
+    (firthCat = mouse - cat1), (secondCat = cat2 - mouse);
   } else if (cat2 < mouse && cat1 > mouse) {
-    firthCat = cat1 - mouse;
-    secondCat = mouse - cat2;
+    (firthCat = cat1 - mouse), (secondCat = mouse - cat2);
   } else {
-    firthCat = cat1 - mouse;
-    secondCat = cat2 - mouse;
+    (firthCat = cat1 - mouse), (secondCat = cat2 - mouse);
   }
-
   function mouseCatch(firth, second) {
     if (firth === second) {
       return 'os gatos trombam e o rato foge';
@@ -69,7 +66,6 @@ function catAndMouse(mouse, cat1, cat2) {
     }
     return 'cat2';
   }
-
   return mouseCatch(firthCat, secondCat);
 }
 
@@ -93,20 +89,20 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(string) {
-  let stringEncode = string.replace('a', 1);
-  stringEncode = stringEncode.replace('e', 2);
-  stringEncode = stringEncode.replace('i', 3);
-  stringEncode = stringEncode.replace('o', 4);
-  stringEncode = stringEncode.replace('u', 5);
+  let stringEncode = string.replace(/a/g, 1);
+  stringEncode = stringEncode.replace(/e/g, 2);
+  stringEncode = stringEncode.replace(/i/g, 3);
+  stringEncode = stringEncode.replace(/o/g, 4);
+  stringEncode = stringEncode.replace(/u/g, 5);
   return stringEncode;
 }
 
 function decode(string) {
-  let stringDecode = string.replace(1, 'a');
-  stringDecode = stringDecode.replace(2, 'e');
-  stringDecode = stringDecode.replace(3, 'i');
-  stringDecode = stringDecode.replace(4, 'o');
-  stringDecode = stringDecode.replace(5, 'u');
+  let stringDecode = string.replace(/1/g, "a");
+  stringDecode = stringDecode.replace(/2/g, "e");
+  stringDecode = stringDecode.replace(/3/g, "i");
+  stringDecode = stringDecode.replace(/4/g, "o");
+  stringDecode = stringDecode.replace(/5/g, "u");
   return stringDecode;
 }
 
