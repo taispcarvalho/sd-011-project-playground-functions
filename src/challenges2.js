748536a0bb42079d629e6682ed690695e78f0c52
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-01 18:54:14 -0300

    Ajusta o desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,26 +2,28 @@
 function techList(array, myName) {
   let techName = [];
 // Nessa parte, para resolver o problema do lint ('Guarding for-in (guard-for-in)') utilizei este material: https://eslint.org/docs/2.0.0/rules/guard-for-in
-  for (let index in array.sort()) {
-    if ({}.hasOwnProperty.call(array, index)) {
-      techName[index] = {
-        tech: array[index],
-        name: myName,
-      };
-    }
+for (let index in array.sort()) {
+  if ({}.hasOwnProperty.call(array, index)) {
+    techName[index] = {
+      tech: array[index],
+      name: myName,
+    };
   }
-  if (array.lenght === 0) {
+} 
+  if (techName.length === 0) {
     return 'Vazio!';
   }
   return techName;
 }
 
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Vitor'));
+console.log(techList([], 'Vitor'));
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
 }
+
+console.log
 
 // Desafio 12
 function triangleCheck() {
