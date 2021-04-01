148eea26b58dde34940ad1a6664364adfb67a701
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-01 11:03:19 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,15 +14,24 @@ function techList(list, name) {
   }
   return result;
 }
-console.log(techList(['A', 'B', 'C'], 'José'));
 
 // Desafio 11
 function generatePhoneNumber(array) {
   if (array.length !== 11) {
     return 'Array com tamanho incorreto';
   }
+  const sorted = [...array].sort();
+  for (let index = 0; index < 11; index += 1) {
+    if (sorted[index] < 0 || sorted[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if (sorted[index] === sorted[index + 1] && (sorted[index] === sorted[index + 2])) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return '(' + array[0] + array[1] + ')' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
 }
-console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9]));
+console.log((generatePhoneNumber([3, 5, 5, 3, 2, 2, 1, 1, 9, 7, 8])));
 
 // Desafio 12
 function triangleCheck() {
