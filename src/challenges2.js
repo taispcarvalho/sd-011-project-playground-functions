32fcf58e87151c029360900495dcb007d28e17d8
IsleneGomes <islenegomes@gmail.com>
2021-04-02 12:00:57 -0300

    Correçoes Finais - Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -46,7 +46,7 @@ function generatePhoneNumber(array) {
   }
   if (array.length === 11) {
     let primeiraParte = `(${array[0]}${array[1]}) ${array[2]}${array[3]}`;
-    let segundaParte = `${array[4]}${array[5]}${array[6]} - ${array[7]}`;
+    let segundaParte = `${array[4]}${array[5]}${array[6]}-${array[7]}`;
     let ultimaParte = `${array[8]}${array[9]}${array[10]}`;
     return (primeiraParte + segundaParte + ultimaParte);
   }
