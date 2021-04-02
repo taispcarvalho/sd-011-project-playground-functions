c67421a745f34c3dfbddcec96caf25b8bc122a0d
Matheus Costa <mathws.costag@gmail.com>
2021-04-01 22:56:06 -0300

    corrige desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -35,7 +35,9 @@ function generatePhoneNumber(numbers) {
       return 'não é possível gerar um número de telefone com esses valores'
     }
   }
-  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
+  return `(${numbers[0]}${numbers[1]})
+  ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-
+  ${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
 }
 
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 9]));
