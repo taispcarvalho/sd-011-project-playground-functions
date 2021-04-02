a8fed5cf0ee2626a74c40e9a3037250ca6e4c575
Marco Galindo <marco1408@gmail.com>
2021-04-02 15:29:32 -0300

    alterações após o lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,7 +28,7 @@ function generatePhoneNumber(array) {
   function maisDeTres(array) {
     let contador = 0;
     for (let index = 0; index < array.length; index += 1) {
-      contador =0;
+      contador = 0;
       for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
         if (array[index] === array[secondIndex]) {
           contador += 1;
@@ -41,56 +41,52 @@ function generatePhoneNumber(array) {
   }
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
-  } else if (valorInvalido(array) === true || maisDeTres(array) === true) {
-    return 'não é possível gerar um número de telefone com esses valores'
-  } else {
-    for (let index = 0; index < array.length; index += 1) {
-      telefone += array[index];
-      if (index === 1) {
-        telefone += ') ';
-      } else if (index === 6) {
-        telefone += '-';
-      }
-    }
-    return telefone;
+  } if (valorInvalido(array) === true || maisDeTres(array) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
+  for (let index = 0; index < array.length; index += 1) {
+    telefone += array[index];
+    if (index === 1) {
+      telefone += ') ';
+    } else if (index === 6) {
+      telefone += '-';
+    }
+  }
+  return telefone;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  function menorQueSoma (lineA, lineB, lineC) {
+  function menorQueSoma(lineA, lineB, lineC) {
     if (lineA < lineB + lineC && lineB < lineC + lineA && lineC < lineB + lineA) {
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
-  function maiorQueAbsoluto (lineA, lineB, lineC) {
+  function maiorQueAbsoluto(lineA, lineB, lineC) {
     if (lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineB - lineA)) {
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
-  return menorQueSoma(lineA, lineB, lineC) && maiorQueAbsoluto (lineA, lineB, lineC)
+  return menorQueSoma(lineA, lineB, lineC) && maiorQueAbsoluto(lineA, lineB, lineC);
 }
 
 // Desafio 13
 function hydrate(string) {
-  let quantidade = string.replace(/\D/g, "");
+  let quantidade = string.replace(/\D/g, '');
   let copos = 0;
   if (quantidade.length > 1) {
     for (let index = 0; index < quantidade.length; index += 1) {
-      copos = copos + parseInt(quantidade[index], 10);
+      copos += parseInt(quantidade[index], 10);
     }
   } else {
     copos = parseInt(quantidade, 10);
   }
   if (copos === 1) {
-    return copos + ' copo de água';
-  } else {
-    return copos + ' copos de água';
+    return `${copos} copo de água`;
   }
+  return `${copos} copos de água`;
 }
 
 module.exports = {
