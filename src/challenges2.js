650e0283f83921409ee801fd8ad6668ca64cdaea
murilodospacos <murilopacos@gmail.com>
2021-04-02 20:43:14 -0300

    Exercicio 11 - 1 Resolucao

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,31 +10,42 @@ function techList(learningTechnology, name) {
   return outExit;
 } 
 
-// Desafio 11
-let numbers1 = [1, 9, 3, 4, 6, 6, 7, 5, 9, 0, 1];
-let response;
+// Desafio 11 Obs: Vitor Cardoso, Julio Felizzolla, Maicon me ajudaram.
+
+function numberCont(numbers1, response) {
 let result = 0;
-let error;
 for (let index = 0; index < numbers1.length; index += 1){
 if (numbers1[index] === response){
   result += 1;
   }
 }
-  for (let i of numbers) { 
-  if (numbers1[i] < 0 || numbers1[i] > 9 || result > 2){
-  error = 'não é possivel gerar um número de telefone com esses valores';
-} else {
-  
+  return result;
 }
-console.log(error);
+
+function possibilities(arrayNumber) {
+  for (let index = 0; index < arrayNumber.length; index += 1) { 
+  if (arrayNumber[index] < 0 || arrayNumber[index] > 9 || numberCont(arrayNumber, arrayNumber[index]) > 2){
+  return 'não é possivel gerar um número de telefone com esses valores';
+  }
+}
+}
 
 function generatePhoneNumber(number1) {
-  let number1 = 
+  let posible = possibilities(number1)
+  let regex = /(\d{2})(\d{5})(\d{4})/;
+  let myNumbers ='';
   if (number1.length !== 11) { 
     return 'Array com tamanho incorreto'
+  } else if (posible){
+    return posible;
   }
-  return `(${number1[0]}${number1[1]})${number1[2]}${number1[3]}${number1[4]}${number1[5]}${number1[6]}-${number1[7]}${number1[8]}${number1[9]}${number1[10]}`
- }
+  for (let index of number1) {
+    myNumbers += number1[index];
+    myNumbers = myNumbers.replace(regex, '($1)$2-$3');
+  }
+  return myNumbers;
+  }
+console.log(generatePhoneNumber([1, 9, 3, 4, 6, 6, 7, 5, 9, 0, 1]));
 
 // Desafio 12
 
@@ -44,8 +55,6 @@ function triangleCheck(lineA, lineB, lineC) {
   let response = false;
   if (lineC < sizeLineA && lineC > sizeLineB) {
     response = true;
-  } else {
-    response = false;
   }
   return response;
 }
