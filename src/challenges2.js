e016c1c66e32eaa3f7ce7abb6e429f8211bfee4b
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-02 16:58:50 -0300

    Desafio 10 - versao 1

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,21 @@
+// Desafio 10
+function techList(list, name) {
+  if (list.length === 0) {
+    return 'Vazio!';
+  }
+
+  let result = [];
+  list = list.sort();
+  for (let technology of list) {
+    result.push({
+      tech: technology,
+      name,
+    });
+  }
+  return result;
+}
+
+
 module.exports = {
   generatePhoneNumber,
   techList,
