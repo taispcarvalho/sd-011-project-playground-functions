d855001d0d3a235f4d5b25df7bb605939d2e6d4a
Rafael-oliveira <rafa_batista02@hotmail.com>
2021-04-02 17:11:56 -0300

    3,4,5

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(number, number2) {
-  if ( number && number2 === true){
+  if (number && number2 === true) {
     return true;
   } else {
     return false;
@@ -11,38 +11,54 @@ console.log(resu);
 
 // Desafio 2
 function calcArea(base, height) {
-  resulte = (base*height)/2;
+  let resulte = (base * height) /2;
   console.log(resulte);   
 }
-resulte = calcArea(10, 50);
-resulte = calcArea(5, 2);
-resulte = calcArea(51, 1);
+calcArea(10, 50);
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
 
+function splitSentence() {
+  frase = ['go', 'trybe'];
+  console.log(frase);
+}
+splitSentence(); 
+ 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+  let nome = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+  console.log(nome[3]+ ', ' +  nome[0]);    
 }
+concatName()
+
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  total = wins*3 + ties*1;
+  console.log(total);
 }
+footballPoints(0,0);
+  
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  let numeros = Math.max(...[5,4,4,2,10,15,15,12,6,15]);
+  
+} 
+highestCount();
+
+var teste = ["oi", "tudo", "bem", "oi"];
+var qtd = 0;
+for (var i = 0; i < teste.length; i++) {
+    if (teste[i] === "oi") qtd++;
 }
+console.log(qtd);
 
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
 }
-
+9,5,10,4,5,8,5,5
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
