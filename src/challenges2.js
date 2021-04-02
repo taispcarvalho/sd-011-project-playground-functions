d83486df42fd7363e90816324260261b4772fef9
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-02 14:50:52 -0300

    Exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -24,9 +24,38 @@ console.log(techList(['Python'], 'Thyago'));
 console.log(techList([], 'Thyago'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+//Crie uma função chamada generatePhoneNumber que receba uma array com 11 números e retorne um número de telefone, respeitando parênteses, traços e espaços.
+
+// Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], generatePhoneNumber deverá retornar (12) 34567-8901.
+
+// Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho incorreto.".
+
+// Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses valores".
+function generatePhoneNumber(array) {
+  let code = [];
+  let first = [];
+  let second = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array.length != 11) {
+      return 'Array com tamanho incorreto.';
+    } else if (array[i] < 0 || array[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } else if (i >= 0 && i < 2) {
+      code += array[i];
+    } else if (i >= 2 && i < 6) {
+      first += array[i];
+    } else if (i >= 6 && i <= 9) {
+      second += array[i];
+    }
+  }
+  let phone = '(' + code + ')' + first + '-' + second;
+  return phone;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 0]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11]));
 
 // Desafio 12
 function triangleCheck() {
