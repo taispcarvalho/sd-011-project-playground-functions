871432aff2f42bb457c5a6414f93a5431d89a952
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-04-02 07:19:50 -0300

    task 11 updates

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,7 +6,7 @@ function techList(tech, name) {
   let result = [];
   let list = tech.sort(); // 'list' recieve 'tech' sorted
 
-  if (list.length !== 0) { // if list.length diffent from zero, it's ok to continue
+  if (list.length !== 0) { // if list.length is different from zero, it's ok to continue
     for (let i = 0; i < list.length; i += 1) {
       result.push({ tech: list[i], name }); // just print the object in a for loop
     }
@@ -15,31 +15,60 @@ function techList(tech, name) {
   return result;
 }
 
+// D11 Auxiliary Functions
+// References:
+// How to count repeated numbers in arrays without using Two For Loops cause Complexibility:
+// https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+function lengthValidation(phone) {
+  let invalid = false;
+  if (phone.length !== 11) {
+    invalid = true;
+  }
+  return invalid;
+}
+
+function digitValidation(phone) {
+  let invalid = false;
+  for (let i = 0; i < phone.length; i += 1) {
+    if (phone[i] > 9 || phone[i] < 0) {
+      invalid = true;
+    }
+  }
+  return invalid;
+}
+
+function counterValidation(phone) {
+  let invalid = false;
+  let count = 0;
+  for (let i = 0; i < phone.length; i += 1) {
+    for (let j = 0; j < phone.length; j += 1) {
+      if (phone[i] === phone[j]) count += 1;
+    }
+  }
+  if (count > 2) invalid = true;
+  return invalid;
+}
+
 // Desafio 11
 // References:
 // Template Literals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 // Break Lines in TL: https://stackoverflow.com/questions/37321047/wrap-long-template-literal-line-to-multiline-without-creating-a-new-line-in-the
 function generatePhoneNumber(phone) {
   let result = '';
-  if (phone.length > 11 || phone.length < 11) {
-    result = 'Array com tamanho incorreto.';
+  if (lengthValidation(phone)) {
+    result = 'Array com tamanho incorreto';
+  } else if (digitValidation(phone)) {
+    result = 'não é possível gerar um número de telefone com esses valores1';
+  } else if (counterValidation(phone)) {
+    result = 'não é possível gerar um número de telefone com esses valores2';
   } else {
     result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}`
     + `${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
-    
-    for (let i = 0; i < phone.length; i += 1) {
-      let count = 0;
-      if (phone[i] > 9 || phone[i] < 0) {
-        result = 'não é possível gerar um número de telefone com esses valores';
-      }
-      for (let j = 0; j < phone.length; j += 1) {
-        if (phone[i] === phone[j]) count += 1;
-        if (count > 2) result = 'não é possível gerar um número de telefone com esses valores';
-      }
-    }
   }
   return result;
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 // References:
@@ -66,9 +95,7 @@ function triangleCheck(lineA, lineB, lineC) {
 // RegExp g Modifier (/g): https://www.w3schools.com/jsref/jsref_regexp_g.asp
 // Template Literals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 // Removing unnecessary zeros: https://stackoverflow.com/questions/3612744/remove-insignificant-trailing-zeros-from-a-number
-// parseInt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 // Number(): https://www.w3schools.com/jsref/jsref_number.asp
-// toString(): https://www.w3schools.com/jsref/jsref_tostring_number.asp
 // Missing Radix Parameter: https://stackoverflow.com/questions/7818903/jslint-says-missing-radix-parameter
 function hydrate(words) {
   let numbers = words.replace(/[^0-9]/g, ''); // A technique used to find the numbers inside a string with the addition of /g to search they all
