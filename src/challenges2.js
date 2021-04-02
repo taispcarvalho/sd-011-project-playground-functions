87060835db87167ae1ea68c33ca578c9caa1e3b5
Hugo Mouto <hmoutol@gmail.com>
2021-04-02 20:44:11 -0300

    Correção Lint Challange 2 - Update 1

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,41 +18,18 @@ function techList(arrayD10, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber(arrayD11) {
-  let ddd = [];
-  let numPart1 = [];
-  let numPart2 = [];
-
-  // Check for wrong conditions
-  switch (true) {
-  case arrayD11.length !== 11:
-    return 'Array com tamanho incorreto.';
-  case checkNumber(arrayD11):
-    return 'não é possível gerar um número de telefone com esses valores';
-  default:
-    for (let i = 0; i <= 1; i += 1) {
-      ddd.push(arrayD11[i]);
-    }
-    for (let i = 2; i <= 6; i += 1) {
-      numPart1.push(arrayD11[i]);
-    }
-    for (let i = 7; i <= 10; i += 1) {
-      numPart2.push(arrayD11[i]);
-    }
-    return `(${ddd.join('')}) ${numPart1.join('')}-${numPart2.join('')}`;
-  }
-}
-
-function checkNumber(arrayD11) {
+function checkNumber(array11) {
   let wrongNumber = false;
-  for (let i = 0; i < arrayD11.length; i += 1) {
-    if (arrayD11[i] > 9 || arrayD11[i] < 0) {
-      wrongNumber = true;
-      return wrongNumber;
-    }
+  let biggest = Math.max(...array11);
+  let lowest = Math.min(...array11);
+  if (biggest > 9 || lowest < 0) {
+    wrongNumber = true;
+    return wrongNumber;
+  }
+  for (let i = 0; i < array11.length; i += 1) {
     let sameNumberLoop = 0;
-    for (let i2 = i; i2 < arrayD11.length; i2 += 1) {
-      if (arrayD11[i] === arrayD11[i2]) {
+    for (let i2 = i; i2 < array11.length; i2 += 1) {
+      if (array11[i] === array11[i2]) {
         sameNumberLoop += 1;
         if (sameNumberLoop >= 3) {
           wrongNumber = true;
@@ -61,6 +38,36 @@ function checkNumber(arrayD11) {
       }
     }
   }
+}
+
+function generatePhoneNumber(array11) {
+  switch (true) {
+  case array11.length !== 11:
+    return 'Array com tamanho incorreto.';
+  case checkNumber(array11):
+    return 'não é possível gerar um número de telefone com esses valores';
+  default:
+    return pushNumbers(array11);
+  }
+}
+function pushNumbers(array11) {
+  let ddd = [];
+  let numPart1 = [];
+  let numPart2 = [];
+  for (let i = 0; i <= 10; i += 1) {
+    switch (true) {
+    case i >= 0 && i < 2:
+      ddd.push(array11[i]);
+      break;
+    case i >= 2 && i <= 6:
+      numPart1.push(array11[i]);
+      break;
+    case i > 6 && i <= 10:
+      numPart2.push(array11[i]);
+      break;
+    }
+  }
+  return `(${ddd.join('')}) ${numPart1.join('')}-${numPart2.join('')}`;
 }
 
 // Desafio 12
