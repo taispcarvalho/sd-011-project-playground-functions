44636eb5e4419bc5db963378dd03d8b5422c69c9
dutofanim <etofanim@gmail.com>
2021-04-02 16:59:21 -0300

    Implementa requisito 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,11 +21,46 @@ function techList(techArray, name) {
   return 'Vazio!';
 }
 
-let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+// part of Desafio 11
+function joinNumber(numberArray) {
+  let phoneTempArray = [];
+
+  phoneTempArray.push((numberArray.slice(0, 2)).join(''));
+  phoneTempArray.push((numberArray.slice(2, 7)).join(''));
+  phoneTempArray.push((numberArray.slice(7, 11)).join(''));
+
+ return `(${phoneTempArray[0]})${phoneTempArray[1]}-${phoneTempArray[2]}`;
+}
+
+// part of Desafio 11
+function verifyRepeatedNumber(numberArray) {
+  for (let index = 0; index < numberArray.length; index += 1) {
+    let tempNumber = numberArray[index];
+    let appearedTimes = 0;
+
+    for (let number of numberArray) {
+      if (number === tempNumber) {
+        appearedTimes += 1;
+      }
+    }
+    return appearedTimes;
+  }
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  let phoneNumber = [];
-  
+function generatePhoneNumber(numberArray) {
+
+  if (numberArray.length !== 11) return 'Array com tamanho incorreto.'
+
+  if (verifyRepeatedNumber(numberArray) >= 3) return 'não é possível gerar um número de telefone com esses valores'
+
+  for (let number of numberArray) {
+    if (number < 0 || number > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+
+  return joinNumber(numberArray);
 }
 
 // Desafio 12
