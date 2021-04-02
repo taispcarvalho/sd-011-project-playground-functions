826d22196004ada1819552aae565ac4141d0c7a9
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-04-02 11:16:37 -0300

    Desafio 10 concluido - Lista de tecnologias ordenadas alfabeticamente

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function sortArray(tech) {
+  let sorted = tech.sort(function (accumulator, currentNumber) {
+    if (accumulator < currentNumber) {
+      return -1;
+    }
+    if (accumulator > currentNumber) {
+      return 1;
+    }
+    return 0;
+  });
+  return sorted;
+}
+
+function techList(techArray, name) {
+  let sortedArray = sortArray(techArray);
+  let techListed = [];
+
+  for (let index = 0; index < sortedArray.length; index += 1) {
+    let currentTech = sortedArray[index];
+    techListed.push({
+      tech: currentTech,
+      name,
+    });
+  }
+  return techListed;
 }
 
 // Desafio 11
