563d950755be49ab89ae03a48da51ca2f759f1df
Alexandre <naitwa.alexandre@gmail.com>
2021-04-02 17:03:20 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,12 +17,88 @@ console.log(techList(['React', 'Javascrit', 'CSS', 'HTML', 'C#'], 'Alexandre'))
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+  function differentSize(sizePhone) {
+    if ((sizePhone !== 11)) {
+      return 'Array com tamanho incorreto.';
+    }
+    return '';
+  }
+  
+  function differentNumber(number) {
+    if ((number < 0) || (number > 9)) {
+      return true;
+    }
+    return false;
+  }
+  
+  function mostRepeated(array, number) {
+    let count = 0;
+    for (let indexCount = 0; indexCount < array.length; indexCount += 1) {
+      if (number === array[indexCount]) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+  
+  function errorNumberAndCount(number, array) {
+    let error = differentSize(array.length);
+    let phrase = '';
+    if (error !== '') {
+      phrase = error;
+    }
+    if (differentNumber(number) || (mostRepeated(array, number) >= 3)) {
+      phrase = 'não é possível gerar um número de telefone com esses valores';
+    }
+    return phrase;
+  }
+  
+  function includeChar(number, index) {
+    let phone = '';
+    if (index === 0) {
+      phone += '(';
+    }
+    if (index === 2) {
+      phone += ') ';
+    }
+    if (index === 7) {
+      phone += '-';
+    }
+    phone += number;
+    return phone;
+  }
+  
+  function generatePhoneNumber(arrayPhone) {
+    let phrase = '';
+    let error = '';
+    error = errorNumberAndCount('', arrayPhone);
+    if ((arrayPhone.length === 0) || (error !== '')) {
+      phrase = error;
+    }
+    for (let index = 0; index < arrayPhone.length; index += 1) {
+      phrase += includeChar(arrayPhone[index], index);
+      error = errorNumberAndCount(arrayPhone[index], arrayPhone);
+      if (error !== '') {
+        phrase = error;
+        break;
+      }
+    }
+    return phrase;
+  }  
 }
 
 // Desafio 12
+function verifyTriangle(N1, N2, N3) {
+  let verify = false;
+  if ((N1 < Math.abs(N2 + N3)) && (N1 > Math.abs(N2 - N3))) {
+    verify = true;
+  }
+  return verify;
+}
+
 function triangleCheck(lineA, lineB, lineC) {
   let result = false;
+
   let verifyA = verifyTriangle(lineA, lineB, lineC);
   let verifyB = verifyTriangle(lineB, lineC, lineA);
   let verifyC = verifyTriangle(lineC, lineA, lineB);
@@ -31,6 +107,8 @@ function triangleCheck(lineA, lineB, lineC) {
   }
   return result;
 }
+
+console.log(triangleCheck(10, 10, 10));
 
 // Desafio 13
 function hydrate(drinks) {
