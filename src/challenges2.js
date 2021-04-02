92fcfb562407c2dc6f6ebf778c5efdbe88d5dec4
IsleneGomes <islenegomes@gmail.com>
2021-04-02 11:56:29 -0300

    Correçoes - Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,4 @@
-// Desafio 10
+// Desafio 10 - ok
 function techList(array, name) {
   let test = [];
   array.sort();
@@ -17,7 +17,7 @@ function techList(array, name) {
 // Desafio 11
 function numeroZeroNove(array) {
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] < 0 || array[index] >= 10) {
+    if (array[index] < 0 || array[index] > 9) {
       return false;
     }
   }
@@ -25,20 +25,24 @@ function numeroZeroNove(array) {
 }
 function repeticaoNumero(array) {
   let contador = 0;
-  let index = 0;
-  for (let indexCont = 0; indexCont < array.length; indexCont += 1) {
-    if (array[indexCont] === array[index]) {
-      contador += 1;
+  for (let index = 0; index < array.length; index += 1) {
+    for (let indexCont = 0; indexCont < array.length; indexCont += 1) {
+      if (array[indexCont] === array[index]) {
+        contador += 1;
+      }
     }
+    if (contador >= 3) {
+      return false;
+    }contador = 0;
   }
-  if (contador >= 3) {
-    return true;
-  }contador = 0;
-  return false;
+  return true;
 }
 function generatePhoneNumber(array) {
-  if (array.length > 11) {
+  if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
+  }
+  if (!numeroZeroNove(array) || !repeticaoNumero(array)) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
   if (array.length === 11) {
     let primeiraParte = `(${array[0]}${array[1]}) ${array[2]}${array[3]}`;
@@ -46,13 +50,8 @@ function generatePhoneNumber(array) {
     let ultimaParte = `${array[8]}${array[9]}${array[10]}`;
     return (primeiraParte + segundaParte + ultimaParte);
   }
-  if (numeroZeroNove(array) || repeticaoNumero(array)) {
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
   return generatePhoneNumber(array);
 }
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
-console.log(generatePhoneNumber(arr));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -62,7 +61,7 @@ function triangleCheck(lineA, lineB, lineC) {
   return (triaA || triaB);
 }
 
-// Desafio 13
+// Desafio 13 - ok
 function hydrate(string) {
   let numeroCopos = string.match(/\d+/g).map(Number);
   let somaCopos = 0;
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,6 +1,6 @@
 function numeroZeroNove(array) {
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] < 0 || array[index] >= 10) {
+    if (array[index] < 0 || array[index] > 9) {
       return false;
     }
   }
@@ -8,20 +8,24 @@ function numeroZeroNove(array) {
 }
 function repeticaoNumero(array) {
   let contador = 0;
-  let index = 0;
-  for (let indexCont = 0; indexCont < array.length; indexCont += 1) {
-    if (array[indexCont] === array[index]) {
-      contador += 1;
+  for (let index = 0; index < array.length; index += 1) {
+    for (let indexCont = 0; indexCont < array.length; indexCont += 1) {
+      if (array[indexCont] === array[index]) {
+        contador += 1;
+      }
     }
+    if (contador >= 3) {
+      return false;
+    }contador = 0;
   }
-  if (contador >= 3) {
-    return true;
-  }contador = 0;
-  return false;
+  return true;
 }
 function generatePhoneNumber(array) {
-  if (array.length > 11) {
+  if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
+  }
+  if (!numeroZeroNove(array) || !repeticaoNumero(array)) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
   if (array.length === 11) {
     let primeiraParte = `(${array[0]}${array[1]}) ${array[2]}${array[3]}`;
@@ -29,10 +33,10 @@ function generatePhoneNumber(array) {
     let ultimaParte = `${array[8]}${array[9]}${array[10]}`;
     return (primeiraParte + segundaParte + ultimaParte);
   }
-  if (numeroZeroNove(array) || repeticaoNumero(array)) {
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
   return generatePhoneNumber(array);
 }
-let arr = [1, 2, 3, 4, 15, 6, 7, 8, 9, 1, 2];
+//Se tamanho do array 0 ou maior que 11 funciona
+//Arrumar contador de repetição e se o numero dentro do array é maior que 9
+
+let arr = [1, 2, 3, 1, 5, 12, 7, 8, 9, 2, 3];
 console.log(generatePhoneNumber(arr));
