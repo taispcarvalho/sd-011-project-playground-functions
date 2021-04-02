932436757c3c55b447ead04b756c0dce98464ded
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-02 13:13:29 -0300

    Corrigindo exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,16 +23,17 @@ function generatePhoneNumber(arr) {
   if (arr.filter(num0A9).length !== 11 || repeatNum(arr)) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
+  console.log(arr2);
   return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([2, 1, 2, 4, 5, 6, 7, 8, 9, 0, 1]));
 
-function repeatNum(arr) {
+function repeatNum(array) {
   //  arrOrdenada ordena todos os números de arr de forma crescente.
-  let arrOrdenada = arr.sort();
+  let arrOrdenada = array.sort();
   //  loop para verificar se o indice 0 é igual ao indice 1 e 2, caso for retornará falso, já que o array está ordenado de forma crescente, e se o indice 0 for igual ao indice 1 e 2 significa que o numero apareceu 3 vezes.
-  for (let i = 0; i < arr.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     if (
       arrOrdenada[i] === arrOrdenada[i + 1] &&
       arrOrdenada[i] === arrOrdenada[i + 2]
