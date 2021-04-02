bfd0878e581d7508a4628496f4aa24ad911b05a8
Bernardo <bernardocezario21@gmail.com>
2021-04-02 03:42:39 -0300

    Complete exercise 7

--- src/challenges.js
+++ src/challenges.js
@@ -37,8 +37,17 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Distance = Math.abs(mouse - cat1);
+  let cat2Distance = Math.abs(mouse - cat2);
+
+  if (cat1Distance < cat2Distance) {
+    return 'cat1';
+  } else if (cat2Distance < cat1Distance) {
+    return 'cat2'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
