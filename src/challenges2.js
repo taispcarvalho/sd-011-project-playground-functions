5c221b43257e9cb80731473de2ed78bea2d12f1f
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-02 14:59:19 -0300

    Exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -24,14 +24,6 @@ console.log(techList(['Python'], 'Thyago'));
 console.log(techList([], 'Thyago'));
 
 // Desafio 11
-
-//Crie uma função chamada generatePhoneNumber que receba uma array com 11 números e retorne um número de telefone, respeitando parênteses, traços e espaços.
-
-// Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], generatePhoneNumber deverá retornar (12) 34567-8901.
-
-// Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho incorreto.".
-
-// Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses valores".
 function generatePhoneNumber(array) {
   let code = [];
   let first = [];
@@ -53,7 +45,7 @@ function generatePhoneNumber(array) {
       second += array[i];
     }
   }
-  let phone = '(' + code + ')' + first + '-' + second;
+  let phone = `(${code}) ${first}-${second}`;
   return phone;
 }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
