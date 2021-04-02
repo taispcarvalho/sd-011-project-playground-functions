075f4a0342d6e90c9c216abd670ca562d570c3fc
dutofanim <etofanim@gmail.com>
2021-04-02 17:24:59 -0300

    Revisa requisito 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,6 +22,16 @@ function techList(techArray, name) {
 }
 
 // part of Desafio 11
+function outOffLimit(numberArray) {
+  for (let number of numberArray) {
+    if (number < 0 || number > 9) {
+      return number;
+    }
+  }
+  return 0;
+}
+
+// part of Desafio 11
 function joinNumber(numberArray) {
   let phoneTempArray = [];
 
@@ -29,7 +39,7 @@ function joinNumber(numberArray) {
   phoneTempArray.push((numberArray.slice(2, 7)).join(''));
   phoneTempArray.push((numberArray.slice(7, 11)).join(''));
 
- return `(${phoneTempArray[0]})${phoneTempArray[1]}-${phoneTempArray[2]}`;
+  return `(${phoneTempArray[0]})${phoneTempArray[1]}-${phoneTempArray[2]}`;
 }
 
 // part of Desafio 11
@@ -49,15 +59,14 @@ function verifyRepeatedNumber(numberArray) {
 
 // Desafio 11
 function generatePhoneNumber(numberArray) {
+  if (numberArray.length !== 11) return 'Array com tamanho incorreto.';
 
-  if (numberArray.length !== 11) return 'Array com tamanho incorreto.'
+  if (outOffLimit(numberArray) !== 0) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
 
-  if (verifyRepeatedNumber(numberArray) >= 3) return 'não é possível gerar um número de telefone com esses valores'
-
-  for (let number of numberArray) {
-    if (number < 0 || number > 9) {
-      return 'não é possível gerar um número de telefone com esses valores'
-    }
+  if (verifyRepeatedNumber(numberArray) >= 3) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
 
   return joinNumber(numberArray);
