fc775887561fb7cf589389d7ce176f8987f91bd5
Savio Moraes <smmsavio@hotmail.com>
2021-04-02 14:05:11 -0300

    Inicia resolucao do requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -65,9 +65,15 @@ console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) {
+    if (lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineA - lineB)) {
+      return true;
+    }
+  }
+  return false;
 }
+console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
