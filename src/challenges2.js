7f2443d35f38a135a415391e90358f64cddad717
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-01 21:59:12 -0300

    Exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,27 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  // Criação de variáveis para armazenar info
+  let newArr=[];
+  let objNew = {};
+  // Checar se o array tem conteudo
+  if (array.length === 0) {
+    return 'Vazio!';
+  }
+  // Criação do conteudo do objNew
+  for (let i = 0; i < array.length; i += 1) {
+      objNew = {
+      tech: array[i],
+      name: name
+      }
+      newArr.push(objNew);
+  }
+  // Colocar em ordem alfabetica
+  newArr = newArr.sort(function (a, b) {if (a.tech > b.tech) {return 1;}})
+  return newArr;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Thyago'));
+console.log(techList(["Python"], 'Thyago'));
+console.log(techList([], 'Thyago'));
 
 // Desafio 11
 function generatePhoneNumber() {
