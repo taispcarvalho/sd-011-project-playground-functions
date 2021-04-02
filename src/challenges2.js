f5fd545aaf211d1bb0be81456b2721ce5fccd1da
Maran <maranmorimoto@gmail.com>
2021-04-02 17:25:29 -0300

    exercise 10, changes

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,105 +1,18 @@
 // Desafio 10
 function techList(techsToLearn,myName) {
-  techToLearn = techsToLearn.sort()
+  techToLearn = techsToLearn.sort();
   let output = [];
   for(index = 0; index < techsToLearn.length; index += 1) {
   var sorted = {
     tech: techsToLearn[index],
     name: myName,
-  }
+  };
   output.push(sorted)
   } 
   return output;
   }
 
   console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Maran'))
-
-
-
-
-  unction techList(array, myName) {
-    let techName = [];
-    for (let index in array.sort()) {
-    if ({}.hasOwnProperty.call(array, index)) {
-    techName[index] = {
-    tech: array[index],
-    name: myName,
-    };
-    }
-    }
-    if (techName.length === 0) {
-    return 'Vazio!';
-    }
-    return techName;
-    } 
-
-
-
-
-
-    function techList(array, myName) {
-      let techName = [];
-      // Nessa parte, para resolver o problema do lint ('Guarding for-in (guard-for-in)') utilizei este material: https://eslint.org/docs/2.0.0/rules/guard-for-in
-      for (let index in array.sort()) {
-      if ({}.hasOwnProperty.call(array, index)) {
-      techName[index] = {
-      tech: array[index],
-      name: myName,
-      };
-      }
-      }
-      if (techName.length === 0) {
-      return 'Vazio!';
-      }
-      return techName;
-      } 
-
-
-
-      retorno[index] = {
-        tech: array[index],
-        name: myName,
-        }; 
-
-
-
-
-
-
-
-var teste = 'algo';
-var car = {
-  marca: 'nissan',
-  modelo: 'tiida',
-  ano: 2012,
-  odometro: '70.000',
-}
-
-console.log(car)
-console.log(car.marca);
-console.log(car.modelo);
-console.log(car['odometro'])
-
-car.cor = teste + 'prata';
-console.log(car);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Desafio 11
 function generatePhoneNumber() {
