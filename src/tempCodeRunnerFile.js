42aed5274f9d29798ef323f80c782fba8453edae
IsleneGomes <islenegomes@gmail.com>
2021-04-02 10:53:38 -0300

    Corrigindo Desafio 10 metodo sort()

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,15 +1,15 @@
 function techList(array, name) {
   let test = [];
-  array.sort();
   for (let index = 0; index < array.length; index += 1) {
     test.push({
       tech: array[index],
       nome: name,
     });
-  }
+  } test.sort();
   if (array.length === 0) {
     return 'Vazio!';
   }
   return test;
 }
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+let array = ['CSS', 'JS', 'Node'];
+console.log(techList(array, 'Lucas'));
