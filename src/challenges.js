f60418ddecf83b491534b95429f44e8c452ef2fd
nick <pereiranicolas713@gmail.com>
2021-04-03 19:54:58 -0300

    Desafios 1,2 e 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // 
+function compareTrue(value1,value2) {
+  return value1 && value2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  return phrase.split('');
 }
 
 // Desafio 4
