2eed408e4ff9ab60a13210125f7d8ce559690cce
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-04-03 04:45:03 -0300

    task 11 updates

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,7 +19,6 @@ function techList(tech, name) {
 // References:
 // Arrow functions: https://www.w3schools.com/js/js_arrow_function.asp
 // How to Get all Non-Unique Values: https://stackoverflow.com/questions/840781/get-all-non-unique-values-i-e-duplicate-more-than-one-occurrence-in-an-array
-// slice(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 
 // I used Arrow Functions just to try to lessen the code's complexity, and to learn how to use it, no particular use.
 const lengthValidation = (phone) => {
@@ -37,20 +36,13 @@ const digitValidation = (phone) => {
   return invalid;
 };
 
-// slice() gets a copy of the array to not mess with the code, adding the sort() method with a basic function to work corretly with numbers,
-// cause the standard behaviour is for alphabetic order of strings
-// Reference:
-// sort() with function for numbers: https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
-const counterValidation = (phone, count) => {
-  let invalid = false;
-  let sorted = phone.slice().sort((a, b) => a - b);
-  count = 0;
-  for (let i = 0; i < sorted.length - 1; i += 1) { /* thanks to Stackoverflow, I have learn how to count the repeated
-                                                      numbers without using two fors loops */
-    if (sorted[i] === sorted[i + 1]) count += 1;
-    if (count >= 3) invalid = true;
+const counterValidation = (value, phone) => {
+  let count = 0;
+  for (let i = 0; i < phone.length; i += 1) {
+    if (value === phone[i]) count += 1;
   }
-  return invalid;
+  if (count >= 3) return true;
+  return false;
 };
 
 // Desafio 11
@@ -59,23 +51,30 @@ const counterValidation = (phone, count) => {
 // Break Lines in TL: https://stackoverflow.com/questions/37321047/wrap-long-template-literal-line-to-multiline-without-creating-a-new-line-in-the
 function generatePhoneNumber(phone) {
   let result;
-  let count = 0;
   if (lengthValidation(phone)) {
     result = 'Array com tamanho incorreto.';
   } else if (digitValidation(phone)) {
     result = 'não é possível gerar um número de telefone com esses valores';
-  } else if (counterValidation(phone, count)) {
-    result = 'não é possível gerar um número de telefone com esses valores';
-  } else {
-    result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}`
-    + `${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
   }
+
+  for (let i = 0; i < phone.length; i += 1) {
+    if (counterValidation(phone[i], phone)) {
+      result = 'não é possível gerar um número de telefone com esses valores';
+    } else {
+      result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}`
+    + `${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
+    }
+  }
+
   return result;
 }
+
+console.log(generatePhoneNumber([1, 1, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 // References:
 // Math.abs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+// sort() with function for numbers: https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
 function triangleCheck(lineA, lineB, lineC) {
   let lines = []; // empty array for lines of the triangle
   lines.push(Math.abs(lineA), Math.abs(lineB), Math.abs(lineC)); // just add the parameters (lines) to the array
