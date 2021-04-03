6bdc6a9cba37820db2c9d7e0f5a802c3f2503a60
IsaacMagno <isaacmagno98@gmail.com>
2021-04-03 11:09:49 -0300

    desafio 9 - correcoes

--- src/challenges.js
+++ src/challenges.js
@@ -77,36 +77,36 @@ function fizzBuzz(fizzArray) {
 // Desafio 9
 function encode(strCode) {
   let listSub = {
-    a:"1",
-    e:"2",
-    i:"3",
-    o:"4",
-    u:"5"
+    a: "1",
+    e: "2",
+    i: "3",
+    o: "4",
+    u: "5"
   };
-  encoded = strCode.replace(/a|e|i|o|u/gi, function(letra) {
+  encoded = strCode.replace(/a|e|i|o|u/gi, function (letra) {
     let lt = listSub[letra];
-    let letraList = lt.replace(/(?:^|\s)\S/g, function(element){return element; });
+    let letraList = lt.replace(/(?:^|\s)\S/g, function (element){return element; });
     return letraList
   });
-  return encoded
+  return encoded;
 }
 
 function decode(numCode) {
   let listSub = {
-    "1":"a",
-    "2":"e",
-    "3":"i",
-    "4":"o",
-    "5":"u"
+    "1": "a",
+    "2": "e",
+    "3": "i",
+    "4": "o",
+    "5": "u"
   };
   decoded = numCode.replace(/1|2|3|4|5/gi, function(num) {
     let n = listSub[num];
-    let numList = n.replace(/(?:^|\s)\S/g, function(element){return element; });
+    let numList = n.replace(/(?:^|\s)\S/g, function(element) {return element; });
     return numList
   });
-  return decoded
+  return decoded;
 }
-  
+
 module.exports = {
   calcArea,
   catAndMouse,
