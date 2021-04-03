aa8d6edd0f096322f871bb37a1a43304aa110e7d
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-03 11:09:21 -0300

    finaliza desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+/*
+referencia para o uso do metodo sort > https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
+Referencia sobre o uso do 'ternary operator': https://www.w3schools.com/jsref/jsref_operators.asp
+*/
+function techList(tech, name) {
+  let technologies = [];
+  for (let techno of tech) {
+    let technology = { tech: '', name: '' };
+    technology.tech = techno;
+    technology.name = name;
+    technologies.push(technology);
+  }
+  return (technologies.sort((a, b) => ((a.tech > b.tech) ? 1 : -1)));
 }
 
 // Desafio 11
