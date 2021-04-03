69695c46fc046df434d19ac95bd372b3db9c2a8d
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-03 17:03:19 -0300

    conclusão desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -42,39 +42,80 @@ console.log(footballPoints(14, 8));
 function highestCount(array) {
   let maiorNumero = 0;
   let numeroRepetido = 0;
-for (let index = 0; index < array.length; index += 1) {
-  let possivelMaiorNumero = maiorNumero;
-  maiorNumero = array[index];
-  if (maiorNumero < possivelMaiorNumero) {
-    maiorNumero = possivelMaiorNumero;
+  for (let index = 0; index < array.length; index += 1) {
+    let possivelMaiorNumero = maiorNumero;
+    maiorNumero = array[index];
+    if (maiorNumero < possivelMaiorNumero) {
+      maiorNumero = possivelMaiorNumero;
+    }
   }
-}
-for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
-  if (maiorNumero === array[secondIndex]) {
-    numeroRepetido += 1;
+  for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+    if (maiorNumero === array[secondIndex]) {
+      numeroRepetido += 1;
+    }
   }
-}
-return numeroRepetido;
+  return numeroRepetido;
 }
 console.log(highestCount([9, 3, 4, 6, 9, 5, 7]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = Math.abs(cat1 - mouse);
+  let distanceCat2 = Math.abs(cat2 - mouse);
+  if (cat1 > cat2) {
+    return 'cat2';
+  } else if (cat1 < cat2) {
+    return 'cat1';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
+console.log(catAndMouse(0, 3, 2));
+console.log(catAndMouse(0, 6, 12));
+console.log(catAndMouse(0, 1, 1));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(num) {
+  let array = [];
+for (let index = 0; index < num.length; index += 1) {
+  if (num[index] % 3 === 0 && num[index] % 5 === 0) {
+    array.push("fizzBuzz");
+  } else if (num[index] % 3 === 0 && num[index] % 5 != 0) {
+    array.push("fizz");
+  } else if (num[index] % 5 === 0 && num[index] % 3 != 0) {
+   array.push("buzz");
+  } else {
+    array.push("bug");
+  }
 }
+return array
+}
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(sentence) {
+  for (var index = 0; index < sentence.length; index += 1){
+    sentence = sentence.replace('a', 1);
+    sentence = sentence.replace('e', 2);
+    sentence = sentence.replace('i', 3);
+    sentence = sentence.replace('o', 4);
+    sentence = sentence.replace('u', 5);
+  }
+  return sentence;
+ }
+ console.log(encode('hi there!'));
+
+function decode(sentence2) {
+  for (var secondIndex = 0; secondIndex < sentence2.length; secondIndex += 1) {
+    sentence2 = sentence2.replace(1, 'a');
+    sentence2 = sentence2.replace(2, 'e');
+    sentence2 = sentence2.replace(3, 'i');
+    sentence2 = sentence2.replace(4, 'o');
+    sentence2 = sentence2.replace(5, 'u');
+  }
+  return sentence2
 }
-function decode() {
-  // seu código aqui
-}
+console.log(decode('h3 th2r2!'));
 
 module.exports = {
   calcArea,
