86fc07022f236f5da974bd6237633b3ec9d4d2b3
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-03 06:43:51 -0300

    desafio 11.2

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,16 @@
 // Desafio 10
-function techList(nametf) {
+function techList() {
+  // seu código aqui
 }
  
 // Desafio 11
 function generatePhoneNumber(numberArray) {
-  let counts = {}
-  for (i = 0; i <= numberArray.length; i += 1) {
-    if (numberArray[i] < 0 || numberArray[i] > 9) {
+  let counts = {};
+  for (let i = 0; i <= numberArray.length; i += 1) {
+    if (numberArray.length != 11) {
+      return 'Array com tamanho incorreto.'
+    }
+    else if (numberArray[i] < 0 || numberArray[i] > 9) {
     return 'não é possível gerar um número de telefone com esses valores'
     }
     else if (counts[numberArray[i]]) {
@@ -15,26 +19,22 @@ function generatePhoneNumber(numberArray) {
     counts[numberArray[i]] = 1;
     }
   }
-  //https://dev.to/huyddo/find-duplicate-or-repeat-elements-in-js-array-3cl3
+  // https://dev.to/huyddo/find-duplicate-or-repeat-elements-in-js-array-3cl3
   for (let prop in counts) {
     if (counts[prop] >= 3) {
     return 'não é possível gerar um número de telefone com esses valores'
     }
   }
-  //Filter only numbers from the input
-  //https://learnersbucket.com/examples/javascript/how-to-format-phone-number-in-javascript/
-  let cleaned = ('' + numberArray).replace(/\D/g, '');
-  //Check if the input is of correct length
-  let match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
-  if (match) {
-  return '(' + match[1] + ') ' + match[2] + '-' + match[3]
-  }
-  else if (numberArray !== 11) {
-  return 'Array com tamanho incorreto.'
-  }
-};
-numberArray=[9, 2, 3, 0, 5, ]
-console.log(generatePhoneNumber(numberArray))
+  // Filter only numbers from the input
+  // https://learnersbucket.com/examples/javascript/how-to-format-phone-number-in-javascript/
+    let cleaned = ('' + numberArray).replace(/\D/g, '');
+    // Check if the input is of correct length
+    let match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
+    if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    }
+}
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
