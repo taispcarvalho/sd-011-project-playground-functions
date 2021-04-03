22676f8923de5d3d6970349df70af03730af3ce4
gab-boff <gmeninboff@gmail.com>
2021-04-02 21:54:05 -0300

    Adicionando Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,12 +16,55 @@ function techList(techArray, names) {
 
   return finalArray;
 }
-console.log(techList([], 'Gabriel'));
 // Para o desenvolvimento do código do Desafio 10 utilizei o código do Inacio como inspiração
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function makePhoneNumber(numberArray) {
+  let phoneNumber = '';
+  phoneNumber += '(';
+  for (let index = 0; index < 2; index += 1) {
+    phoneNumber += numberArray[index];
+  }
+  phoneNumber += ')';
+  for (let index = 2; index < 6; index += 1) {
+    phoneNumber += numberArray[index];
+  }
+  phoneNumber += '-';
+  for (let index = 6; index < 11; index += 1) {
+    phoneNumber += numberArray[index];
+  }
+
+  return phoneNumber;
+}
+
+function repeatNumberVerify(value) {
+  for (let numberMaxMin of value) {
+    if (numberMaxMin < 0 || numberMaxMin > 9) {
+      return true;
+    }
+  }
+
+  let phoneNumberArraySort = value.sort((a, b) => a - b);
+  for (let index = 0; index < value.length - 1; index += 1) {
+    if (phoneNumberArraySort[index - 1] === phoneNumberArraySort[index] && phoneNumberArraySort[index] === phoneNumberArraySort[index + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(phoneNumberArray) {
+  let finalNumber = makePhoneNumber(phoneNumberArray);
+  if (phoneNumberArray.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  let repeatTest = repeatNumberVerify(phoneNumberArray);
+  if (repeatTest === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  return finalNumber;
 }
 
 // Desafio 12
