e6e596197fa8873ab2cfc05fb53d1cdb10849cbc
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-03 17:29:24 -0300

    realizado identação

--- src/challenges.js
+++ src/challenges.js
@@ -60,15 +60,14 @@ console.log(highestCount([9, 3, 4, 6, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanceCat1 = Math.abs(cat1 - mouse);
-  let distanceCat2 = Math.abs(cat2 - mouse);
-  if (cat1 > cat2) {
+  let distCat1 = mouse - cat1;
+  let distCat2 = mouse - cat2;
+  if (Math.abs(distCat1) > Math.abs(distCat2)) {
     return 'cat2';
-  } else if (cat1 < cat2) {
+  } else if (Math.abs(distCat1) < Math.abs(distCat2)) {
     return 'cat1';
-  } else {
-    return 'os gatos trombam e o rato foge';
   }
+    return 'os gatos trombam e o rato foge';
 }
 console.log(catAndMouse(0, 3, 2));
 console.log(catAndMouse(0, 6, 12));
@@ -80,21 +79,21 @@ function fizzBuzz(num) {
 for (let index = 0; index < num.length; index += 1) {
   if (num[index] % 3 === 0 && num[index] % 5 === 0) {
     array.push("fizzBuzz");
-  } else if (num[index] % 3 === 0 && num[index] % 5 != 0) {
+      } else if (num[index] % 3 === 0 && num[index] % 5 !== 0) {
     array.push("fizz");
-  } else if (num[index] % 5 === 0 && num[index] % 3 != 0) {
-   array.push("buzz");
-  } else {
+      } else if (num[index] % 5 === 0 && num[index] % 3 !== 0) {
+    array.push("buzz");
+      } else {
     array.push("bug");
+      }
+    }
+    return array;
   }
-}
-return array
-}
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode(sentence) {
-  for (var index = 0; index < sentence.length; index += 1){
+  for (let index = 0; index < sentence.length; index += 1) {
     sentence = sentence.replace('a', 1);
     sentence = sentence.replace('e', 2);
     sentence = sentence.replace('i', 3);
@@ -103,10 +102,10 @@ function encode(sentence) {
   }
   return sentence;
  }
- console.log(encode('hi there!'));
+console.log(encode('hi there!'));
 
 function decode(sentence2) {
-  for (var secondIndex = 0; secondIndex < sentence2.length; secondIndex += 1) {
+  for (let secondIndex = 0; secondIndex < sentence2.length; secondIndex += 1) {
     sentence2 = sentence2.replace(1, 'a');
     sentence2 = sentence2.replace(2, 'e');
     sentence2 = sentence2.replace(3, 'i');
