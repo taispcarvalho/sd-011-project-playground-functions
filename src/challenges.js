ad8fd2304212b637d1e07722896d5528e88f5ba8
Wadson <wadson.dev92@gmail.com>
2021-04-03 13:46:10 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -51,8 +51,31 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1, distCat2;
+  if (cat1 === cat2){
+  return "os gatos trombam e o rato foge";
+}
+else if (mouse - cat1 < 0){
+  distCat1 = mouse - cat1 * (-1);
+}
+else {
+  distCat1 = mouse - cat1;
+}
+
+if (mouse - cat2 < 0){
+  distCat2 = mouse - cat2 * (-1);
+}
+
+else {
+  distCat2 = mouse - cat2;
+}
+if (distCat1 > distCat2){
+  return "cat2";
+}
+else {
+  return "cat1";
+}
 }
 
 // Desafio 8
