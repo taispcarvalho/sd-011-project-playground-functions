9686794204823d0277227d74a891179c4c295eb6
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-02 21:04:35 -0300

    exercicio 11 com correcoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,59 +19,60 @@ function generatePhoneNumber(numbers) {
   let numbersOutput = [];
   for (let index = 0; index < numbers.length; index += 1) {
     switch (numbers[index]) {
-      case 0:
-        zero += 1;
-        break;
-      case 1:
-        one += 1;
-        break;
-      case 2:
-        two += 1;
-        break;
-      case 3:
-        three += 1;
-        break;
-      case 4:
-        four += 1;
-        break;
-      case 5:
-        five += 1;
-        break;
-      case 6:
-        six += 1;
-        break;
-      case 7:
-        seven += 1;
-        break;
-      case 8:
-        eight += 1;
-        break;
-      case 9:
-        nine += 1;
-        break;
-      default:
-        error += 1;
+    case 0:
+      zero += 1;
+      break;
+    case 1:
+      one += 1;
+      break;
+    case 2:
+      two += 1;
+      break;
+    case 3:
+      three += 1;
+      break;
+    case 4:
+      four += 1;
+      break;
+    case 5:
+      five += 1;
+      break;
+    case 6:
+      six += 1;
+      break;
+    case 7:
+      seven += 1;
+      break;
+    case 8:
+      eight += 1;
+      break;
+    case 9:
+      nine += 1;
+      break;
+    default:
+      error += 1;
     }
   }
-  if (numbers.length != 11) {
+  if (numbers.length !== 11) {
     return ('Array com tamanho incorreto.');
-  } else if (zero > 3 || one > 3 || two > 3 || three > 3 || four > 3 || five > 3 || six > 3 || seven > 3 || eight > 3 || nine > 3 || error > 0) {
+  } else if (zero > 2 || one > 2 || two > 2 || three > 2 || four > 2 || five > 2 || six > 2 || seven > 2
+    || eight > 2 || nine > 2 || error > 0) {
     return ('não é possível gerar um número de telefone com esses valores');
   } else {
-  numbersOutput.push('(');
-  for (let index = 0; index < 2; index += 1) {
-    numbersOutput.push(numbers[index]);
-  }
-  numbersOutput.push(') ')
-  for (let index = 2; index < 7; index += 1) {
-    numbersOutput.push(numbers[index]);
-  }
-  numbersOutput.push('-');
-  for (let index = 7; index < 11; index += 1) {
-    numbersOutput.push(numbers[index]);
-  }
-  numbersOutput = numbersOutput.join('');
-  return numbersOutput
+    numbersOutput.push('(');
+    for (let index = 0; index < 2; index += 1) {
+      numbersOutput.push(numbers[index]);
+    }
+    numbersOutput.push(') ');
+    for (let index = 2; index < 7; index += 1) {
+      numbersOutput.push(numbers[index]);
+    }
+    numbersOutput.push('-');
+    for (let index = 7; index < 11; index += 1) {
+      numbersOutput.push(numbers[index]);
+    }
+    numbersOutput = numbersOutput.join('');
+    return numbersOutput;
   }
 }
 
