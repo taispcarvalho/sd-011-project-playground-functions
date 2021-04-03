8b46f069dabd6b97df9555c0599b4662b8767fc6
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-03 20:43:07 -0300

    Resolve problemas do lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,7 +16,7 @@ function techList(array, myName) {
   return techName;
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Vitor'));
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Vitor'));
 
 // Desafio 11
 function countNum(array, num) {
@@ -76,10 +76,11 @@ function triangleCheck(lineA, lineB, lineC) {
 
 console.log(triangleCheck(10, 14, 8));
 
-// Desafio 13
+// Desafio 13 
+// Deixo meus agradecimentos aos colegas de turma Julio Filizzola e Vinicius Gouveia por me auxiliarem com realizacao deste desafio. Para resolver o problema de encontrar valores numericos com o objeto 'Number()' foi utilizado o material do seguinte local: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number
 function hydrate(string) {
   string = string.replace(/\D/g, '');
-  let num = 0; 
+  let num = 0;
 
   for (let index = 0; index < string.length; index += 1) {
     num += Number(string[index]);
@@ -92,7 +93,7 @@ function hydrate(string) {
   return `${num} copos de água`;
 }
 
-console.log(hydrate("1 cachaça"));
+console.log(hydrate('1 cachaça'));
 
 module.exports = {
   generatePhoneNumber,
