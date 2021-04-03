7d5f7073f72713047f715ec2627ac46ef74c5e14
WalterTNFC <walter.tad@hotmail.com>
2021-04-03 17:08:03 -0300

    Item 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array10, name) {
+
+array10 = array10.sort();
+let array11 = [];
+
+if (array10.length === 0){
+  return ('Vazio!');
 }
+
+else{
+  for (let index = 0; index < array10.length; index +=1){
+    let object = {
+      tech : array10[index],
+      name: name
+    };
+
+    array11.push(object) 
+  }
+  return (array11);
+  }
+}
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Lucas'));
+console.log(techList([],'Lucas'));
+
 
 // Desafio 11
 function generatePhoneNumber() {
