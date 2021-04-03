8544d4710ab8df62dec2708012fc12bb5f37fe36
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-04-03 17:47:51 -0400

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -4,8 +4,8 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, hight) {
+  return (base * hight) / 2;
 }
 
 // Desafio 3
