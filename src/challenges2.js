04685c4e7589712ae43e783a351c546b5f9c906a
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-03 19:04:21 -0300

    Alteraçao desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,14 +2,13 @@
 function techList(tech, name) {
   tech = tech.sort();
   for (let index = 0; index < tech.length; index += 1) {
-    tech[index] = { 
-      tech: tech[index], 
-      name: name
+    tech[index] = {
+      tech: tech[index],
+      name: name,
     };
   }
   return tech;
 }
-console.log(techList(['jesus','bscoito','amendon'], 'Jess'));
 
 // Desafio 11
 function generatePhoneNumber() {
