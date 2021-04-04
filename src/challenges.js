5057daf3578298b0aa0f42a3b2deeba12d33ecfe
Aline Hofner <alinehofner@gmail.com>
2021-04-04 20:22:01 -0300

    function catandmouse

--- src/challenges.js
+++ src/challenges.js
@@ -34,13 +34,21 @@ function highestCount(conjunto, ocorrencia) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distance1 = cat1 - mouse;
+  let distance2 = cat2 - mouse;
+  if (distance1 > distance2) {
+    return 'cat2';
+  } if (distance2 > distance1) {
+    return 'cat1';
+  } if (distance1 === distance2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+
 }
 
 // Desafio 9
