bc2a3b3afdc78adc75388f5758a224edf34dd164
OldRoge <rogeriomrjr@icloud.com>
2021-04-04 14:06:20 -0300

    Fixing lint errors

--- src/challenges.js
+++ src/challenges.js
@@ -65,22 +65,21 @@ function highestCount(numbers) {
   let biggest = 0;
 
   for (let index = 0; index < numbers.length; index += 1) {
-    if(numbers[index] > biggest) {
+    if (numbers[index] > biggest) {
       biggest = numbers[index];
     }
   }
   for (let index = 0; index < numbers.length; index += 1) {
-    if(numbers[index] === biggest) {
+    if (numbers[index] === biggest) {
       biggest = numbers[index];
       counter += 1;
     }
-  }  
-  return counter
+}  
+  return counter;
 }
-
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
-console.log(highestCount([0, 0, 0]))
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 // Desafio 7
 /** function catAndMouse(mouse, cat1, cat2) {
   mouse;
