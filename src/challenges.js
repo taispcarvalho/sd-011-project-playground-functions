4686b101c98e1794bdf7c26815c6f22d1e634918
Reginara Gomes <refagomes@gmail.com>
2021-04-04 18:31:53 -0300

    Adiciona desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -6,46 +6,48 @@ function compareTrue(value1, value2) {
     return false;
   }
 }
-console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, height) {
   return (base * height) / 2;
 }
-console.log(calcArea(10, 50));
-console.log(calcArea(5, 2));
-console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
 }
-console.log(splitSentence('go Trybe'));
-console.log(splitSentence('vamo que vamo'));
-console.log(splitSentence('foguete'));
 
 // Desafio 4
 function concatName(string) {
   let firstString = string[0];
-  let lastString = string[string.lenght-1]; 
+  let lastString = string[string.lenght-1];
   return firstString + ', ' + lastString;
 }
-console.log(concatName('Lucas', 'Cassiano', 'Ferraz', 'Paolillo'));
-console.log(concatName('foguete', 'não', 'tem', 'ré'));
-console.log(concatName('captain', 'my', 'captain'));
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins * 3) + ties  
+  return (wins*3) + ties;  
 }
-console.log(footballPoints(14,8));
-console.log(footballPoints(1,2));
-console.log(footballPoints(0,0));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(listNumbers) {
+  let highestNumber = listNumbers[0];
+  let appearNumber = 0;
+
+  for (let index =0; index < listNumbers.lenght; index +=1)){
+    if (listNumbers[index] > highestNumber){
+      highestNumber = listNumbers[index];
+    }
+  }
+  
+  for (let index =0; index < listNumbers.lenght; index +=1)) {
+    if (listNumbers[index] === highestNumber){
+      appearNumber += 1;
+      }
+  }
+  return appearNumber;
 }
+
 
 // Desafio 7
 function catAndMouse() {
