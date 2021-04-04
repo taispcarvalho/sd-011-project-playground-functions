776f82472692faef16a215e6dd1c4d3f5f08e58a
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-04 17:55:46 -0300

    Exercicio 13 - correçao lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -24,7 +24,7 @@ console.log(techList(['Python'], 'Thyago'));
 console.log(techList([], 'Thyago'));
 
 // Desafio 11
-function generatePhoneNumber(array) {
+function generatePhoneNumber() {
   // escreva seu código aqui
 }
 
