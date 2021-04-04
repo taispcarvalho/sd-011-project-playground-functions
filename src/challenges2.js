408b95a5460ffb0a18b2ce1f874cdb81679619a4
MIkaela Braga <bragamikaela@gmail.com>
2021-04-04 20:54:18 -0300

    correçao do lint desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,16 @@
 // Desafio 10
-function techList(arrayTech, name) {
+function techList(arrayTech, nameDev) {
   let newTech = [];
   let newObj = [];
 
   for (let index = 0; index < arrayTech.length; index += 1) {
     newTech.push(arrayTech.sort()[index]);
   }
-  for (let index = 0; index < name.length; index += 1) {
+  for (let index = 0; index < nameDev.length; index += 1) {
     let object = {
       tech: newTech[index],
-      name: name,
-    }
+      name: nameDev,
+    };
     newObj.push(object);
   }
   if (arrayTech.length === 0) {
