8d6bed52ebc9c9409fcbcbab1d2a50366d97de53
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-04 19:16:17 -0300

    desafio 11 feito talvez

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,9 +13,46 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function repeatNumbers(number, array) {
+  let counter = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === number) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
+
+function numbersCondition(array) {
+  let condition = true;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9 || repeatNumbers(array[index], array) >= 3) {
+      condition = false;
+    }
+  }
+  return condition;
+}
+
+function phoneNumber(array) {
+  let phone = '(xx)xxxxx-xxxx';
+  array.forEach((item) => {
+    phone = phone.replace('x', item);
+  });
+  return phone;
+}
+
+function generatePhoneNumber(array) {
+  let errorText = 'Array com tamanho incorreto.';
+  let errorText2 = 'não é possível gerar um número de telefone com esses valores';
+  let phone = phoneNumber(array);
+  if (array.length !== 11) {
+    return errorText;
+  } if (numbersCondition(array) === false) {
+    return errorText2;
+  }
+  return phone;
+}
+/* https://www.florin-pop.com/blog/2019/04/jcc-create-a-phone-number/ */
 
 // Desafio 12
 function triangleCheck() {
