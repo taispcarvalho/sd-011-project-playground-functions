9b8d42ff183925ffe0caca2e4e522471c11c5606
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-04 17:18:15 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,31 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function moveBiggest(arrayOfObjects, sizeOfRepet) {
+  for (let position = 0; position < sizeOfRepet; position += 1) {
+    let afterPosition = position + 1;
+    if (arrayOfObjects[position].tech > arrayOfObjects[afterPosition].tech) {
+      let objectCache = arrayOfObjects[position];
+      arrayOfObjects[position] = arrayOfObjects[afterPosition];
+      arrayOfObjects[afterPosition] = objectCache;
+    }
+  }
+  return arrayOfObjects;
+}
+
+function orderList(arrayOfObjects) {
+  for (let index = 1; index < arrayOfObjects.length; index += 1) {
+    let sizeOfRepet = arrayOfObjects.length - index;
+    arrayOfObjects = moveBiggest(arrayOfObjects, sizeOfRepet);
+  }
+  return arrayOfObjects;
+}
+
+function techList(array, name) {
+  let techObjects = [];
+  for (let tech of array) {
+    let newObject = { tech, name };
+    techObjects.push(newObject);
+  }
+  return orderList(techObjects);
 }
 
 // Desafio 11
