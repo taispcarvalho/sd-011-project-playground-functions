cc034cc23d76a7d841a5cd380e65bde45ec93e78
Jessica Arruda <sicarruda@gmail.com>
2021-04-04 19:06:33 -0300

    Correçao desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,35 +10,31 @@ function techList(array, student) {
       name: student,
     });
   }
-  return comparator(newArray);
+  newArray.sort(ordenadorTech);
+  return;
 }
 
-function comparator(comparator) {
-  comparator.sort(function (techA, techB) {
-    if (techA.tech > techB.tech) {
-      return 1;
-    } else if (echA.tech < techB.tech) {
-      return -1;
-    }
-    return 0;
-  });
-  return comparator;
+function ordenadorTech(a, b) {
+  if (a.tech > b.tech) {
+    return 1;
+  } else if (a.tech < b.tech) {
+    return -1;
+  }
+  return 0;
 }
 
 // Desafio 11
 function generatePhoneNumber(array) {
   console.log(array);
-  if (array.length > 11) {
+  if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-
   for (let index = 0; index < array.length; index += 1) {
     let count = 0;
     for (let indexJ = 0; indexJ < array.length; indexJ += 1) {
       let avaliator = array[index];
       if (avaliator === array[indexJ]) {
         count += 1;
-        console.log(count);
       }
       if (count > 2 || array[indexJ] < 0 || array[indexJ] > 9) {
         return 'não é possível gerar um número de telefone com esses valores';
