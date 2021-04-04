c6cd0dd1e06e90f5a8b633bfffcad50dd4cc5002
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-04 11:17:45 -0300

    Desafio 11 corrigido

--- src/challenges2.js
+++ src/challenges2.js
@@ -43,13 +43,17 @@ function generatePhoneNumber(numbers) {
   }
   
   if (cont < 2) {
-  return numbers;
+    numbers.splice(0, 0, '(');
+    numbers.splice(3, 0, ') ');
+    numbers.splice(9, 0, '-');
+    return numbers.join('');
   }
 
   else {
     return 'não é possível gerar um número de telefone com esses valores.';
   }
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
