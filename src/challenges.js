ff89286e4f82760c7d51c54a902d75a73f0806c5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-04 17:49:20 -0300

    Corrige o exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -66,16 +66,16 @@ console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (cat1 < cat2) {
-    return 'cat1'
-  } else if (cat2 < cat1) {
-    return 'cat2'
+  if ((cat1 - mouse) < (cat2 - mouse)) {
+    return 'cat1';
+  } else if ((cat2 - mouse) < (cat1 - mouse)) {
+    return 'cat2';
   } else {
-    return 'os gatos trombaram e o rato foge'
+    return 'os gatos trombaram e o rato foge';
   }
 }
 
-console.log(catAndMouse(0, 2, 2));
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz(numbersArray) {
