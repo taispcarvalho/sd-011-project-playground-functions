5b3c8cb13720dfb0f7159644af735e0860d5e520
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-04 16:13:57 -0300

    Desafio 11 dividindo para conquistar

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,37 +20,44 @@ function techList(lista, nameName) {
 }
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
+//Função auxiliar
+function repetition(numbers) {
   let cont = 0;
+  for (let index of numbers) {
+    for (let index2 = 1; index2 < numbers.length; index2 += 1) {
+      if (index2 === numbers[index]) {
+        cont += 1;
+      }
+    }
+    if (cont >= 3) {
+      return false;
+    }
+    cont = 0;
+  } 
+  
+  return true;
+}
+
+//Função principal
+function generatePhoneNumber(numbers) {
   if (numbers.length !== 11) {
     return 'Array com tamanho incorreto.';
-
   }
 
-  for (index of numbers) {
+  for (let index of numbers) {
    if (numbers[index] < 0 || numbers[index] > 9) {
      return 'não é possível gerar um número de telefone com esses valores.';
    }
   }
-
-  for (let index2 of numbers) {
-    for (let index3 = 1; index3 < numbers.length; index += 1) {
-      if (index2 === numbers[index3]) {
-        cont += 1;
-      }
-      break;
-    }
-  }
   
-  if (cont >= 3) {
-    return 'não é possível gerar um número de telefone com esses valores.';
+  if (!repetition(numbers)) {  //se a condição da função repetition for verdadeira
+    numbers.splice(0, 0, '(');
+    numbers.splice(3, 0, ') ');
+    numbers.splice(9, 0, '-');
+    return numbers.join('');
   }
 
-  numbers.splice(0, 0, '(');
-  numbers.splice(3, 0, ') ');
-  numbers.splice(9, 0, '-');
-  return numbers.join('');
-  
+  return 'não é possível gerar um número de telefone com esses valores.';
 }
 
 // Desafio 12
