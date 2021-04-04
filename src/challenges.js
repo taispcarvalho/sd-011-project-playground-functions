34fea6ebfba7931e7e04801358bba164fde15a3d
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-04 17:15:38 -0300

    deafio 7 - eslint

--- src/challenges.js
+++ src/challenges.js
@@ -42,13 +42,10 @@ function catAndMouse(mouse, cat1, cat2) {
   let distance1 = Math.abs(mouse - cat1);
   let distance2 = Math.abs(mouse - cat2);
   if (distance1 < distance2) {
-    return "cat1"
-  }
+    return 'cat1'; }
   else if (distance2 < distance1) {
-    return "cat2"
-  }
-  else {
-    return "os gatos trombam e o rato foge"
+    return 'cat2'; }
+    return 'os gatos trombam e o rato foge';
   }
 }
 
