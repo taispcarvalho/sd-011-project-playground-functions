0c2af4037b5ed9731216a204f8dee4b1c6b26aea
Caio-Veloso <caio.veloso@gmail.com>
2021-04-04 10:59:05 -0300

    Calculando área do triangulo

--- src/challenges.js
+++ src/challenges.js
@@ -13,9 +13,14 @@ function compareTrue(valueA, valueB) {
 console.log(compareTrue(false, true))
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
+
 }
+
+console.log(calcArea(10,50));
+console.log(calcArea(5,2));
+console.log(calcArea(51,1));
 
 // Desafio 3
 function splitSentence() {
