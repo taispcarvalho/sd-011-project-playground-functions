9a736a8480f1af0a295d389e99e6e7c5f7e83557
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-04-04 17:26:28 -0300

    correçao lint

--- src/challenges.js
+++ src/challenges.js
@@ -55,7 +55,7 @@ function highestCount() {
 function catAndMouse(mouse, cat1, cat2) {
   if (mouse - cat1 === mouse - cat2) {
     return 'os gatos trombam e o rato foge';
-  } else (mouse - cat1 > mouse - cat2) {
+  } else if (mouse - cat1 > mouse - cat2) {
     return 'cat1';
   } 
   return 'cat2';
