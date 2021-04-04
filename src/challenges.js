4342e9abb11f1e59a9e59051af22d63b4aa5de13
Ana Carolina Cavalheiro Giorgiani <cavalheiro.anacarolina@gmail.com>
2021-04-03 21:03:22 -0300

    Correçao requisito 9 Projects Playground Functions

--- src/challenges.js
+++ src/challenges.js
@@ -78,7 +78,7 @@ function fizzBuzz(number) {
 // Desafio 9
 function encode(palavra) {
   let stringNovas = '';
-  stringNovas = palavra.replace(/a/, '1');
+  stringNovas = palavra.replace(/a/g, '1');
   stringNovas = stringNovas.replace(/e/g, '2');
   stringNovas = stringNovas.replace(/i/g, '3');
   stringNovas = stringNovas.replace(/o/g, '4');
@@ -88,7 +88,7 @@ function encode(palavra) {
 
 function decode(palavra) {
   let stringNumbers = '';
-  stringNumbers = palavra.replace(/1/, 'a');
+  stringNumbers = palavra.replace(/1/g, 'a');
   stringNumbers = stringNumbers.replace(/2/g, 'e');
   stringNumbers = stringNumbers.replace(/3/g, 'i');
   stringNumbers = stringNumbers.replace(/4/g, 'o');
