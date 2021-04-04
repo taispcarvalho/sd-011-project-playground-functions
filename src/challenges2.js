c0e35c6c8e53a8a086c571b92d0c28e7e65f6a74
Ana Carolina Cavalheiro Giorgiani <cavalheiro.anacarolina@gmail.com>
2021-04-04 18:47:58 -0300

    Correcao requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,7 +33,6 @@ function validaOcorrenciasArrayMenorTres(numbers) {
   }
   return true;
 }
-
 function generatePhoneNumber(numbers) {
   const erroGerarNumero = 'não é possível gerar um número de telefone com esses valores';
 
@@ -49,9 +48,9 @@ function generatePhoneNumber(numbers) {
   if (numbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}
-      ${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}
-      ${numbers[10]}`;
+  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}\
+${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}\
+${numbers[10]}`;
 }
 
 // Desafio 12
