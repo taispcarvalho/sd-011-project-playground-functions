534eda298fd4e8e32b2341d1b39ba2a3e993983e
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-04-04 19:52:15 -0400

    Desafio 9.

--- src/challenges.js
+++ src/challenges.js
@@ -69,11 +69,21 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(text) {
+  let word = text.replace(/a/g, '1');
+  word = word.replace(/e/g, '2');
+  word = word.replace(/i/g, '3');
+  word = word.replace(/o/g, '4');
+  word = word.replace(/u/g, '5');
+  return word;
 }
-function decode() {
-  // seu código aqui
+function decode(text) {
+  let word = text.replace(/1/g, 'a');
+  word = word.replace(/2/g, 'e');
+  word = word.replace(/3/g, 'i');
+  word = word.replace(/4/g, 'o');
+  word = word.replace(/5/g, 'u');
+  return word;
 }
 
 module.exports = {
