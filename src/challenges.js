2900d8b30f9b8a6a085a938046deeceac276e94a
nick <pereiranicolas713@gmail.com>
2021-04-04 15:59:05 -0300

    refazendo desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -51,16 +51,16 @@ function fizzBuzz(numbers) {
   let results = [];
   
   for(let index= 0; index < numbers.length; index += 1){
-   if(numbers[index % 3 === 0]){
+   if(numbers[index % 3 === 0 && numbers[index] % 5 !== 0]){
      results.push('fizz');
-   }else if(numbers[index % 5 === 0]){
+   }else if(numbers[index % 5 === 0 && numbers[index] % 3 !==0]){
      results.push('buzz')
    }else if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0 ){
      results.push('fizzBuzz')
    }else {
      results.push('bug!')
    }
-
+   
   }
   return results;
 }
