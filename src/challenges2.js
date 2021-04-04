cf6ae463637ed87e75861a9980ee3e4974beb21c
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-04 10:28:29 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,7 @@
 // Desafio 10
 function techList(lista, nameName) {
   let list = [];
+  lista.sort();
   for (let index = 0; index < lista.length; index += 1) {
     let object = {
       tech: lista[index],
@@ -9,23 +10,45 @@ function techList(lista, nameName) {
   list.push(object);
   }
 
-  list.sort(function(a, b) {
-  return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0);
-  });
-
   if (lista.length === 0) {
     return 'Vazio!';
     
   }
 
-  else {
-    return list;
-  }
+  return list;
+  ///https://pt.stackoverflow.com/questions/46600/como-ordenar-uma-array-de-objetos-com-array-sort
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let cont = 0;
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+
+  }
+
+  for (index of numbers) {
+   if (numbers[index] < 0 || numbers[index] > 9) {
+     return 'não é possível gerar um número de telefone com esses valores.';
+   }
+  }
+
+  for (index2 of numbers) {
+    for (index3 = 1; index3 < numbers.length; index += 1) {
+      if (index2 === numbers[index3]) {
+        cont += 1;
+      }
+      break;
+    }
+  }
+  
+  if (cont < 2) {
+  return numbers;
+  }
+
+  else {
+    return 'não é possível gerar um número de telefone com esses valores.';
+  }
 }
 
 // Desafio 12
