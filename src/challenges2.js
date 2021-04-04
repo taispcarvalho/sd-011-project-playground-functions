2bbc6dbe722d664572e6e4b06adb530e64c76286
BeatrizR <biarl.ramalhorl@gmail.com>
2021-04-03 21:01:58 -0300

    ex10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  let objecTemplate = {};
+  let listOfObjects = [];
+  if (techs.length === 0) {
+    return 'Vazio!';
+  }
+  techs.sort();
+  for (let index = 0; index < techs.length; index += 1) {
+    objecTemplate = {};
+    objecTemplate.tech = techs[index];
+    objecTemplate.name = name;
+    listOfObjects.push(objecTemplate);
+  }
+  return listOfObjects;
 }
 
 // Desafio 11
