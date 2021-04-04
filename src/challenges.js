d971aef0799eff922001c8a52c791830742cfb2b
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-04 17:04:49 -0300

    desafio 7 - errei o operador de comparaçao

--- src/challenges.js
+++ src/challenges.js
@@ -41,14 +41,14 @@ function highestCount(arrayNumber) {
 function catAndMouse(mouse, cat1, cat2) {
   let distance1 = Math.abs(mouse - cat1);
   let distance2 = Math.abs(mouse - cat2);
-  if (distance1 > distance2) {
-    return 'cat1';
+  if (distance1 < distance2) {
+    return "cat1"
   }
-  else if (distance2 > distance1) {
-    return 'cat2';
+  else if (distance2 < distance1) {
+    return "cat2"
   }
   else {
-    return 'os gatos trombam e o rato foge';
+    return "os gatos trombam e o rato foge"
   }
 }
 
