b9fbd4676fa0e94e5f6e96b7e0d91c4a549b4f54
Michel Pereira <michelbecauro@gmail.com>
2021-04-04 23:43:32 -0300

    Desafio 12 - lint: recomendacoes ES6 - method shorthand

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,16 +11,16 @@ function generatePhoneNumber() {
 // Desafio 12
 function triangleCheck(x, y, z) {
   let obj = {
-    soma: function (a, b, c) {
+    soma (a, b, c) {
       return (a + b > c && a + c > b && c + b > a);
     },
-    abs: function (a, b, c) {
+    abs (a, b, c) {
       return Math.abs(a - b) < c && Math.abs(a - c) < b && Math.abs(c - b) < a;
     },
   };
   return obj.soma(x, y, z) && obj.abs(x, y, z);
 }
-console.log (triangleCheck(10, 14, 8));
+
 // Desafio 13
 function hydrate() {
   // seu código aqui
