f4ddd9625e9831b67a10e22f220a753baa7cb976
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-05 09:34:04 -0300

    Corrige Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,34 +1,21 @@
-const { TestScheduler } = require("@jest/core");
-
 // Desafio 10
 function techList(techNames, name) {
-if (techNames === 0) {
-  return 'Vazio!'
+if (techNames.length === 0) {
+  return 'Vazio!';
 }
 const techList = techNames.sort().map((tech) => ({tech, name}));
-  return techList
+  return techList;
 }
 
 // Desafio 11
 function generatePhoneNumber(arrayOfNumbers) {
   let telefone = [];
-  let count = 0;
   telefone = ('(' + arrayOfNumbers[0] + arrayOfNumbers[1] + ') ' + arrayOfNumbers[2] + arrayOfNumbers[3] + arrayOfNumbers[4] + arrayOfNumbers[5] +  arrayOfNumbers[6] + '-' + arrayOfNumbers[7] + arrayOfNumbers[8] + arrayOfNumbers[9] + arrayOfNumbers[10]);
-  let size = arrayOfNumbers.length;
-   
-  
+
   for (let index in arrayOfNumbers) {
     if (arrayOfNumbers[index] < 0 || arrayOfNumbers[index] > 9) {
       return 'Não é possível gerar um número de telefone com esses valores.'
     } 
-  }
-
-  for (index in arrayOfNumbers) {
-    
-    if (arrayOfNumbers[index] === size) {
-      count += 1;
-    }
-    return count;
   }
   
   if (arrayOfNumbers.length === 11) {
@@ -39,7 +26,6 @@ function generatePhoneNumber(arrayOfNumbers) {
     false
     return 'Array com tamanho incorreto.'
   }
-  
 }
   
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
