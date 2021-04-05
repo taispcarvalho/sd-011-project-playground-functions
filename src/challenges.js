a5aa6ed031dbc3af4bec7dffb4355558c50a9c5c
Halysson2 <halyssonsena83@gmail.com>
2021-04-05 19:27:25 -0300

    ajeitando o erro da questão 9

--- src/challenges.js
+++ src/challenges.js
@@ -81,7 +81,7 @@ function fizzBuzz(array) {
 // Desafio 9
 function encode(number) {
   number = number.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4');
-  number = number.replace(/u/g, '5';) 
+  number = number.replace(/u/g, '5'); 
   return number
 }
 
