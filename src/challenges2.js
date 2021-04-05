426713843c348a31ef269ab2e26b145511f85186
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-05 11:23:41 -0300

    primeira versão desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,9 +11,48 @@ function techList(tech, name) {
   return result;
 }
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// let vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,];
+function checkSize (array) {
+  if (array.length !== 11){
+    return false;
+  }
+  return true;
 }
+
+function checkArray (array) {
+  let number = 0;
+  let count = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return false;
+    }
+    number = array[index];
+    for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1){
+      if (number === array[secondIndex]){
+        count += 1;
+      }
+    }
+  count = 0;
+  if (count >= 3) {
+    return false;
+  }
+  return true;
+  }
+}
+
+function generatePhoneNumber (vetor){
+  if (!checkSize(vetor)) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (!checkArray(vetor)) {
+    return 'não é possível gerar um número de vetorefone com esses valores';
+  }
+  let ddd = `(${vetor[0]}${vetor[1]}) `;
+  let firstFive = `${vetor[2]}${vetor[3]}${vetor[4]}${vetor[5]}${vetor[6]}`;
+  let lastFour = `-${vetor[7]}${vetor[8]}${vetor[9]}${vetor[10]}`;
+  return `${ddd}${firstFive}${lastFour}`;
+}
+// console.log(generatePhoneNumber(vetor));
 
 // Desafio 12
 function triangleCheck() {
