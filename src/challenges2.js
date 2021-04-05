d29a27f64aa24fb2e27e49bd903dd676648f3f58
LeoLacana <wilson17414@gmail.com>
2021-04-05 12:26:14 -0300

    exer 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,28 @@ function techList(arrayValue, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  if (number.length !== 11) {
+    return 'Array com tamanho incorreto';
+  }
+  let repetNumber = 0;
+  for (let index in number) {
+    for (let index2 in number) {
+      if (number[index2] === number[index]) {
+        if (index2 === index) {
+          break;
+        } else if (index2 !== index) {
+          repetNumber += 1;
+        }
+      }
+    }
+    if(number[index] < 0 || number[index] > 9 || repetNumber >= 5) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  return '(' + number[0] + number[1] + ') ' + number[2] + number[3] + 
+  number[4] + number[5] + '-' + number[6] + number[7] + number[8] + number[9];
 }
 
 // Desafio 12
