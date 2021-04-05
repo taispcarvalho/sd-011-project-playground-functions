d05b0e048ab224d00af4400db4cb8964df4a7452
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-05 18:40:55 -0300

    concluido ex 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,21 @@
 // Desafio 10
 function techList(value10, name) {
-let objectTest = {};
-let objectTest2 = [];
+let objectTest = [];
 
-for (let i = 0; i < arrayTest.length; i+=1){
+for (let i = 0; i < value10.length; i+=1){
 objectTest = {
-  tech: arrayTest[i],
+  tech: value10[i],
   name: name,
 }
-objectTest2.push(objectTest);
+objectTest.push(objectTest);
 }
 
-if (arrayTest.length === 0){
+if (value10.length === 0){
   let arrayVazio = 'Vazio!';
-  objectTest2.push(arrayVazio);
+  objectTest.push(arrayVazio);
 
 }
-  return objectTest2;
+  return objectTest;
 }
 
 // Desafio 11
