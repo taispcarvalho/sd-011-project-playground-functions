435aff5bcf165847c362023143e2977a013ef349
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-05 01:30:05 -0300

    Resolve desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,20 @@
 // Desafio 1
 function compareTrue(param1, param2) {
   if (param1 && param2 === true) {
-    return true
+    return true;
+  } else {
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
