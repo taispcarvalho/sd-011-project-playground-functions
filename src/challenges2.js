90970fbf6c4432b2efcdcb06e3c5794175a311fd
Michel Pereira <michelbecauro@gmail.com>
2021-04-04 23:27:03 -0300

    Desafio 12 - lint: recomendacoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,18 +9,18 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck(a, b, c) {
+function triangleCheck(x, y, z) {
   let obj = {
-    'soma': function (a, b, c) {
+    soma: function (a, b, c) {
       return (a + b > c && a + c > b && c + b > a);
     },
-    'abs': function (a, b, c) {
+    abs: function (a, b, c) {
       return Math.abs(a - b) < c && Math.abs(a - c) < b && Math.abs(c - b) < a;
     },
   };
-  return obj['soma'](a, b, c) && obj['abs'](a, b, c);
+  return obj.soma(x, y, z) && obj.abs(x, y, z);
 }
-
+console.log (triangleCheck(10, 14, 8));
 // Desafio 13
 function hydrate() {
   // seu código aqui
