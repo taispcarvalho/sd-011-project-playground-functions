501c29a630e9cedf06cc3cad46ac31f7bf67e028
Maran <maranmorimoto@gmail.com>
2021-04-05 13:45:03 -0300

    refactoring code

--- src/challenges2.js
+++ src/challenges2.js
@@ -26,15 +26,15 @@ function generatePhoneNumber() {
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA > lineB + lineC || lineA < Math.abs(lineB - lineC)) {
-    return ;
+    return false;
   } else if (lineB > lineA + lineC || lineB < Math.abs(lineA - lineC)) {
     return false;
   } else if (lineC > lineA + lineB || lineC < Math.abs(lineA - lineB)) {
     return false;
-  } else {
-    return true;
-  }
+  } 
+  return true;
 }
+console.log(triangleCheck(14, 14, 30))
 
 // Desafio 13
 function hydrate() {
