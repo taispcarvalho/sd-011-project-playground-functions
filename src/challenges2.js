6658a153117fd455d81af13e280b9dea4c7d5399
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-05 13:40:34 -0300

    requisito 11 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,12 +10,30 @@ function techList(array, name) {
   }
   return newObject;
 }
-// let lista = ["React", "Jest", "HTML", "CSS", "JavaScript"]
-// let name = "Lucas"
-// console.log(techList(lista, name));
 // Desafio 11
-function generatePhoneNumber() {
+function checkNumbers(array) {
+  for (let numbers of array) {
+    let count = 0;
+    for (let element of array) {
+      if (numbers === element) {
+        count += 1;
+      }
+    }
+    if (count >= 3) return 'não é possível gerar um número de telefone com esses valores';
+  }
 }
+
+function generatePhoneNumber(array) {
+  if (array.length !== 11) return 'Array com tamanho incorreto.';
+  for (let numbers of array) {
+    if (numbers < 0 && numbers > 9) return 'não é possível gerar um número de telefone com esses valores';
+  }
+  checkNumbers(array);
+  return `(${array[0]}${array[1]})${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+}
+
+let randomNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+console.log(generatePhoneNumber(randomNumbers));
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineC < lineA + lineB && lineC > Math.abs(lineB - lineA)) return true;
@@ -23,7 +41,6 @@ function triangleCheck(lineA, lineB, lineC) {
   if (lineB < lineA + lineC && lineB > Math.abs(lineC - lineA)) return true;
   return false;
 }
-console.log(triangleCheck(10, 14, 8));
 // Desafio 13
 function transformStringToArray(string) {
   return string.split(' ');
@@ -52,8 +69,6 @@ function hydrate(string) {
   if (waterCups === 1) return waterCups + ' copo de água';
   return waterCups + ' copos de água';
 }
-console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
-
 module.exports = {
   generatePhoneNumber,
   techList,
