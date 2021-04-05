24bb556bd03b3bf121338452e876ae1d4bc44171
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-05 20:12:51 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,52 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkRequirements(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    let contador = 0;
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
+      if (array[index] === array[index2]) {
+        contador += 1;
+      }
+    }
+    if (array[index] < 0 || array[index] > 9 || contador >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+}
+
+function generatePhoneNumber(array) {
+  if (checkRequirements(array) !== undefined) {
+    return checkRequirements(array);
+  }
+
+  let resultado = '';
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (index === 0) {
+      resultado += '(';
+      resultado += array[index];
+    } else if (index === 1) {
+      resultado += array[index];
+      resultado += ')';
+    } else if (index === 2) {
+      resultado += array[index];
+      resultado += array[index + 1];
+      resultado += array[index + 2];
+      resultado += array[index + 3];
+      resultado += array[index + 4];
+      resultado += '-';
+    } else if (index === 7) {
+      resultado += array[index];
+      resultado += array[index + 1];
+      resultado += array[index + 2];
+      resultado += array[index + 3];
+    } else if (array.length !== 11) {
+      resultado = 'Array com tamanho incorreto.';
+    }
+  }
+
+  return resultado;
 }
 
 // Desafio 12
