046e27f84dae5fe082a52ff03ac441840475e640
nick <pereiranicolas713@gmail.com>
2021-04-05 13:12:53 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,9 @@
 // Desafio 10
-/* function techList(techArray, name) {
+function techList(techArray, name) {
   let technologyList = [];
   techArray.sort();
   if (techArray.lengthlength === 0) {
-    return 'Vazio';
+    return 'Vazio!';
   }
   for (let index = 0; techArray.length; index += 1) {
     technologyList.push({
@@ -13,45 +13,10 @@
   }
   return technologyList;
 }
-  */
+
 // Desafio 11
 function generatePhoneNumber() {
 
-  function repeatedNumber(n, number){
-    let repeated = 0;
-    for(let index = 0; index < number.length; index += 1){
-      if(number[index] === n){
-        repeated += 1;
-      }
-    }
-    return repeated;
-  }
-
-  function validateNumbers(number){
-    let numberValidity = true;
-    for(let index = 0; index < number; index += 1){
-      if (number[index] < 0 || number[index] > 9 || repeatedNumber(number[index],number >= 3)){
-        numberValidity = false;
-      }
-    }
-    return numberValidity;
-  }
-  function standardizationNumber(number) {
-    let standard = '(xx) xxxxx-xxxx';
-    number.array.forEach((item) => {
-       standard = standard.replace('x', item);
-    });
-   return standard;
-  }
-  function generatePhoneNumber(number) {
-    let phone = standardizationNumber(number);
-    if(number.length !== 11){
-      return 'Array com tamanho incorreto';
-    } if (validateNumbers(number) === false) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-    return phone;
-  }
 }
 
 // Desafio 12
