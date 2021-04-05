355c2ea00c271575e59010204270d1acf1545fb0
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-04 22:13:10 -0300

    Exercicio 11 - Cria uma funcao de numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,33 +1,46 @@
 // Desafio 10
 function techList(techNames, name) {
   let techObject = {};
-  
-  for (let index = 0; index < techNames.length; index += 1){
+  for (let index = 0; index < techNames.length; index += 1) {
     techObject.tech = techNames[index];
     techObject.name = name;
     console.log(techObject);  
-  }  
+  }
 }
-    
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Gabriela'));
 
-/* function techList(techNames) {
-  techNames.sort
-  let techObject = {};
-  for (let index = 0; index < techNames.length; index += 1){
-    for (index in techNames[index]) {
-      techObject.tech = techNames[index];
-    }
-  }  
-    return techObject;
-  }
-    
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"])); */
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(telNumbers) {
+  if (telNumbers.length === 11 && checkNumbers === false) {
+    console.log('(' + telNumbers[0] + telNumbers[1] + ') ' + telNumbers[2] + telNumbers[3] + telNumbers[4] + telNumbers[5] + telNumbers[6] + '-' + telNumbers[7] + telNumbers[8] + telNumbers[9] + telNumbers[10]);
+  } else if (telNumbers.length !== 11 && checkNumbers === false) {
+    console.log('Array com tamanho incorreto.');
+  } else if (checkNumbers === true) {
+    console.log('não é possível gerar um número de telefone com esses valores');
+  }
 }
+function checkNumbers(telNumbers) {
+  let check = false;
+  for (let index = 0; index < telNumbers.length; index += 1) {
+    if (telNumbers[index] < 0 || telNumbers[index] > 9) {
+      check === true;
+      return check;
+    } 
+    let repetNumber = 0;
+    for (let index2 = 0; index2 < telNumbers.length; index +=1) {
+      if (telNumbers[index] ===  telNumbers[index2]) {
+        repetNumber += 1; 
+        if (repetNumber >= 3) {
+          check === true;
+          return check;
+        }
+      }
+    }
+  }
+}
+
+console.log(checkNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
