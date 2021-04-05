0ae925f155d52712018f14e8b33397763b4e0edb
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-05 13:37:59 -0300

    Corrige exercicio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -69,12 +69,16 @@ console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate(drinks) {
-  let totalWater = 0; 
+  let totalWater = 0;
   let numberDrinks = drinks.match(/\d+/g).map(Number);
   for (let index = 0; index < numberDrinks.length; index += 1) {
     totalWater += numberDrinks[index];
   }
-  return totalWater + ' copos de água';
+  if (totalWater === 1)
+    totalWater += ' copo de água';
+  if (totalWater > 1)
+    totalWater += ' copos de água';
+  return totalWater;
 }
 console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
