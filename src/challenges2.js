372c1f886fc7c22e50f281f32b6d216ec27f43b6
Alberto Candido <albertocandido98@gmail.com>
2021-04-05 13:06:19 -0300

    Quinta correçao - requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -52,7 +52,6 @@ function generatePhoneNumber(numbers) {
   }
   return result;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 1, 8, 9]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
