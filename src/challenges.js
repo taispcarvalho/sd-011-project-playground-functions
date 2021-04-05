bf9f61d150c0371715ca54b479933c3d07ba1bff
Halysson2 <halyssonsena83@gmail.com>
2021-04-05 19:23:01 -0300

    refazendo a questão 9

--- src/challenges.js
+++ src/challenges.js
@@ -79,11 +79,15 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(number) {
+  number = number.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4');
+  number = number.replace(/u/g, '5';) 
+  return number
 }
-function decode() {
-  // seu código aqui
+
+function decode(text) {
+ text = text.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u')
+return text
 }
 
 module.exports = {
