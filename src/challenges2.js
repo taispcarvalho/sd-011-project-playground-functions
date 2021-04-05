8269545f0edde283de9e8a1ba04e187ea781f4f5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-05 13:51:53 -0300

    Exercicio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -74,10 +74,12 @@ function hydrate(drinks) {
   for (let index = 0; index < numberDrinks.length; index += 1) {
     totalWater += numberDrinks[index];
   }
-  if (totalWater === 1)
+  if (totalWater === 1) {
     totalWater += ' copo de água';
-  if (totalWater > 1)
+  }
+  if (totalWater > 1) {
     totalWater += ' copos de água';
+  }
   return totalWater;
 }
 console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
