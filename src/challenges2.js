8fe3a0046155e0129dc36deee16f5c88eea0c731
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-04-05 14:11:54 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,14 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  if (tech.length === 0) {
+    return 'Vazio!';
+  }
+  let lista = [];
+  tech.sort();
+  for (let index = 0; index < tech.length; index += 1) {
+    lista.push({ tech: tech[index], name });
+  }
+  return lista;
 }
 
 // Desafio 11
