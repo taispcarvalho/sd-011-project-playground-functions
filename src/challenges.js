72713a06ec664754f55771144c8b1a1d5b14eac4
Kevin Willian <kevinwillian084@gmail.com>
2021-04-04 21:13:32 -0300

    Resolvendo desafios 1, 2 e 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,20 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(bool1,bool2) {
+  return bool1 && bool2
 }
+console.log(compareTrue(true,false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return base * height / 2
 }
+console.log(calcArea(51,1));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(originalString) { 
+  return originalString.split(' ');
 }
+console.log(splitSentence('vamo que vamo'));
 
 // Desafio 4
 function concatName() {
