78e7a1add94864818e0ed4b1820567fdb2a5656c
Genivaldo <cgenivaldo46@gmail.com>
2021-04-05 05:45:41 -0300

    Exercicio06.5

--- src/challenges.js
+++ src/challenges.js
@@ -47,7 +47,7 @@ function highestCount() {
   let numeroMaior = Math.max(...numeros);
   for (let index = 0; index < numeros.length; index += 1) {
     if (numeroMaior === numeros[index]) {
-      return numeroRepetido + 1;
+      return numeroRepetido += 1;
     }
   }
 }
