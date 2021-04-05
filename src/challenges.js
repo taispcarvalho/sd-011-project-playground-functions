48c9046f54ef7c08d46b1505381cc1dc659266de
Caio-Veloso <caio.veloso@gmail.com>
2021-04-05 16:17:47 -0300

    Função gatos e rato

--- src/challenges.js
+++ src/challenges.js
@@ -84,10 +84,20 @@ for(let index =0; index < numbers.length; index +=1  ){
 console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceMouseCat1 = mouse - cat1;
+  let distanceMouseCat2 = mouse - cat2;
+  if(distanceMouseCat1<distanceMouseCat2){
+    return 'cat1'
+  }else if(distanceMouseCat1 > distanceMouseCat2){
+    return 'cat2'
+  }else{
+    return 'Os gatos trombam e o rato foge'
+  }
 }
-
+console.log(catAndMouse(4,2,1));
+console.log(catAndMouse(4,1,2));
+console.log(catAndMouse(4,2,2));
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
