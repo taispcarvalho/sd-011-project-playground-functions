dd8ea4c030881f93976e1ab82144224c7c3fcb3b
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-05 09:52:55 -0700

    Exercício 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -27,7 +27,6 @@ function repeatedNumber(array, number) {
 }
 
 function generatePhoneNumber(array) {
-  let result = 0;
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
@@ -44,9 +43,15 @@ function generatePhoneNumber(array) {
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let cond1 = Math.abs(lineB - lineC) < lineA < lineB + lineC;
+  let cond2 = Math.abs(lineA - lineC) < lineB < lineA + lineC;
+  let cond3 = Math.abs(lineA - lineB) < lineC < lineA + lineB;
+  return cond1 && cond2 && cond3;
+
+// | b - c | < a < b + c
+// | a - c | < b < a + c
+// | a - b | < c < a + b
 
 // Desafio 13
 function hydrate() {
