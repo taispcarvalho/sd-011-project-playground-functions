b3895d620786e08736533b082d3d3259655a12a1
Genivaldo <cgenivaldo46@gmail.com>
2021-04-05 14:48:46 -0300

    Exercicio07.9

--- src/challenges.js
+++ src/challenges.js
@@ -56,7 +56,7 @@ function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let distCat01 = Math.abs(mouse - cat1);
   let distCat02 = Math.abs(mouse - cat2);
-  if (distCat01 < distCat02) {
+  if (distCat01 > distCat02) {
     return 'cat1';
   }
   if (distCat02 === distCat01) {
