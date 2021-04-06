e469363a8156e1804bb5bbaec0f9478addfae999
Genivaldo <cgenivaldo46@gmail.com>
2021-04-06 06:00:01 -0300

    Exercicio09 deve ta errado

--- src/challenges.js
+++ src/challenges.js
@@ -91,12 +91,23 @@ function fizzBuzz(arrayNumber) {
 let arrayNumber = [2, 15, 7, 9, 45];
 fizzBuzz(arrayNumber);
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
-
+  string = string.replace('a', '1');
+  string = string.replace('e', '2');
+  string = string.replace('i', '3');
+  string = string.replace('o', '4');
+  string = string.replace('u', '5');
+  return string;
 }
-function decode() {
+function decode(string) {
   // seu código aqui
+  string = string.replace('1', 'a');
+  string = string.replace('2', 'e');
+  string = string.replace('3', 'i');
+  string = string.replace('4', 'o');
+  string = string.replace('5', 'u');
+  return string;
 }
 
 module.exports = {
