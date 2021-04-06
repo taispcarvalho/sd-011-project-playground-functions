a0f3d0e8be5f8f03d14d31ef577d304163c39a05
Laura Gusmao <laurargus@gmail.com>
2021-04-05 21:34:34 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,19 +23,58 @@ function techList(array, nameName) {
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Laura'));
 
 // Desafio 11
-// function countNum(array, number) {
-//   let count = 0;
-//   for (let num of array) {
-//     if (num === number) {
-//       count += 1;
-//     }
-//   }
-//   return count;
-// }
-
-function generatePhoneNumber() {
-  // seu código aqui
+function phoneStructure(array) {
+  let ddd = `${array[0]}${array[1]}`;
+  let beforeDash = `${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}`;
+  let afterDash = `${array[7]}${array[8]}${array[9]}${array[10]}`;
+  return `(${ddd}) ${beforeDash}-${afterDash}`;
 }
+
+function countRep(array, number) {
+  let count = 0;
+  for (let num of array) {
+    if (num === number) {
+      count += 1;
+    }
+  }
+  return count;
+}
+function arrayOfRepeated(array) {
+  let repeated = [];
+  for (let num of array) {
+    repeated.push(countRep(array, num));
+  }
+  return repeated;
+}
+function lessThan3(array) {
+  let numOfRep = arrayOfRepeated(array);
+  let lessThan = false;
+  for (let value of numOfRep) {
+    if (value >= 3) {
+      lessThan = true;
+    }
+  }
+  return lessThan;
+}
+function validDigit(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (lessThan3(array) || validDigit(array)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return phoneStructure(array);
+}
+console.log(generatePhoneNumber([3, 3, 1, 4, 2, 4, 1, 7, 7, 8, 5]));
 
 // Desafio 12
 function triangleCheck() {
