84a5a12d3fa2fc295e80658e09b17d66d45f0b20
Kevin Willian <kevinwillian084@gmail.com>
2021-04-06 13:10:09 -0300

    Desafio 10 pronto

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,18 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrai, name) {
+  if (arrai.length === 0) {
+    return 'Vazio!';
+  }
+  arrai = arrai.sort();
+  let listTech = [];
+  for (let index = 0; index < arrai.length; index += 1) {
+    listTech.push({
+      tech: arrai[index],
+      name
+    });
+  }
+  return listTech;
 }
-
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
