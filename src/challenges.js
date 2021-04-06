fe4774fa9bc8f252053048c4a08f856d11fb7628
Felipe Carvalho <fecarvalho066@gmail.com>
2021-04-06 10:58:56 -0300

    [Felipe Carvalho] - Requisito 7.4

--- src/challenges.js
+++ src/challenges.js
@@ -56,14 +56,12 @@ highestNumber([9, 1, 2, 3, 9, 5, 7]);
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distan1 = cat1 - mouse;
-  let distan2 = cat2 - mouse;
-  if (distan1 === distan2) {
-    return 'os gatos trombam e o rato foge'
-  } else if (distan1 > distan2) {
-    return 'cat2'
+  if (cat1 > cat2) {
+    return ('cat2')
+  } else if (cat1 < cat2) {
+    return ('cat1')
   } else {
-    return 'cat1'
+    return ('os gatos trombam e o rato foge')
   }
 }
 catAndMouse(10, 12, 13);
