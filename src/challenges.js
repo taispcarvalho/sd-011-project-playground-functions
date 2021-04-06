4ab2bc6e29ea9ec5c4069a676e85d7793801ac06
Isabella Antunes <bella.antunes2@gmail.com>
2021-04-05 23:03:27 -0300

    Exercício 6

--- src/challenges.js
+++ src/challenges.js
@@ -30,8 +30,19 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let highestNumber = 0;
+  for (let index = 0; index < numbers.lenght; index += 1) {
+    if (numbers[index] > highestNumber) {
+      highestNumber = numbers[index];
+    }
+  }
+  let sumHighestNumber = 0;
+  for (let index2 = 0; index2 < numbers.lenght; index2 += 1) {
+    if (numbers[index2] === highestNumber) {
+      sumHighestNumber += 1;
+    }
+  }
 }
 
 // Desafio 7
