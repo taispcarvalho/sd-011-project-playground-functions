bc6f5840ac022dad1cfc1d2525911a6123469d98
AlyssonSene <alysson_silva@outlook.com>
2021-04-06 19:35:21 -0300

    ultimo challenge

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,8 +14,57 @@ function techList(techNames, name) {
   return arrayAux;
 }
 // Desafio 11
-function generatePhoneNumber() {
+let numbers = [1, 2, 3, 5, 6, 7, 8, 9, 0, 1];
+function checkArray(numbers) {
+  if (numbers.lenght === 11) {
+    return false;
+  }
 }
+function  checkNumbers (numbers) {
+  let status = true;
+  for (let index = 0; index < 11; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      status = false;
+    }
+  }
+  return status;
+}
+function checkRepeatNumbers (numbers) {
+  let status = true;
+  let aux = [...numbers].sort();
+  for (let index = 0; index < 11; index +=1) {
+    if (aux[index] === aux[index +1] && (aux[index] === aux[index + 2])){
+      status = false;
+    }
+  }
+  return status;
+}
+function lastStep(numbers) {
+  if (checkArray(numbers) === false) {
+    return 'Array com tamanho incorreto';
+  } if (checkNumbers(numbers) === false || checkRepeatNumbers(numbers) === false) {
+    return 'não é possivel gerar um número de telefone com esses valores';
+  }
+  return true;
+}
+function phone(numbers) {
+  let phoneNumber = `(${numbers[0]}${numbers[1]}) `;
+  for (let index = 2; index < 7; index += 1) {
+    phoneNumber += numbers[index];
+  }
+  phoneNumber += '-';
+  for (let index = 7; index < 11; index += 1) {
+    phoneNumber += numbers[index];
+  }
+  return phoneNumber;
+}
+function generatePhoneNumber(numbers) {
+  if (lastStep(numbers) === true) {
+    return phone(numbers);
+  }
+  return lastStep(numbers);
+}
+console.log(generatePhoneNumber(numbers));
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA <= 0 || lineB <= 0 || lineC <= 0) {
