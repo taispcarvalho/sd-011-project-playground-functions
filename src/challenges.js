1e422b5b9240c7fab154aed5f746c6f3280512b0
Felipe Carvalho <fecarvalho066@gmail.com>
2021-04-06 12:24:19 -0300

    [Felipe Carvalho] - Requisitio 8

--- src/challenges.js
+++ src/challenges.js
@@ -69,9 +69,24 @@ function catAndMouse(mouse, cat1, cat2) {
 catAndMouse(13, 10, 11);
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let finalFrase = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      finalFrase.push('fizzBuzz');
+    } else if (array[index] % 5 === 0) {
+      finalFrase.push('buzz');
+    } else if (array[index] % 3 === 0) {
+      finalFrase.push('fizz');
+    } else {
+      finalFrase.push('bug!');
+    }
+  }
+  return finalFrase;
 }
+fizzBuzz([2, 15, 7, 9, 45]);
+fizzBuzz([7, 9]);
+fizzBuzz([9, 25]);
 
 // Desafio 9
 function encode() {
