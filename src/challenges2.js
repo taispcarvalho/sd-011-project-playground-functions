ced44b28cd495a7da1a77873b6bdde5bda8e4f0f
Aline Hofner <alinehofner@gmail.com>
2021-04-06 00:48:32 -0300

    funcao 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,13 +13,51 @@ function techList(array, name) {
   return newarray;
 }
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function repeatNumber (repetido, teleList) {
+  let contagem = 0;
+  for (let index1 = 0; index1 < teleList.length; index1 += 1) {
+    if (repetido === teleList[index1]) {
+      contagem += 1;
+    } if (contagem >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
 }
+function restricoes (repetido) {
+  if (repetido < 0 || repetido > 9) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+}
+function generatePhoneNumber(teleList) {
+  let repitiu = 0;
+  for (let index = 0; index < teleList.length; index += 1) {
+    repetiu += 1;
+  }
+}
+function tamanhoPhone (teleList) {
+  if (telelist.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function condicaoDeExistencia(lineA, lineB ,lineC) {
+ let existencia = false;
+ if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
+    existencia = true;
+ } 
+ return existencia;
+}
+function triangleCheck(lineA, lineB, lineC) {
+  let existencia = false;
+  let somaAC= lineA + lineC;
+  let somaAB= lineA + lineB;
+  let absoluteAC= Math.abs(lineA - lineC);
+  let absoluteAB= Math.abs(lineA - lineB);
+  if (lineB < somaAC && lineB > absoluteAC && lineC < somaAB && lineC > absoluteAB) {
+    existencia =condicaoDeExistencia(lineA, lineB, lineC);
+  }
+  return existencia;
 }
 
 // Desafio 13
