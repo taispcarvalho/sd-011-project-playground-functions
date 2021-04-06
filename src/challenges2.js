f24599f9ec60939042863f1260bb79b8c334c92b
AlyssonSene <alysson_silva@outlook.com>
2021-04-06 16:01:57 -0300

    lint seu chato

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,19 +6,16 @@ function techList() {
 function generatePhoneNumber() {
   
 }
-
 // Desafio 12
-
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA + lineB > lineC && lineB + lineC > lineA && lineA + lineC > lineB) {
     return true;
-  } else if (Math.abs(lineB - lineC) < lineA && Math.abs(lineA - lineC) < lineB && Math.abs(lineA - lineB) < lineC) {
-    return true;
-  }
+  } else if (Math.abs(lineB - lineC) < lineA && Math.abs(lineA - lineC) < lineB 
+&& Math.abs(lineA - lineB) < lineC) {
+      return true;
+    }
   return false;
 }
-console.log(triangleCheck(5, 1, 2));
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
