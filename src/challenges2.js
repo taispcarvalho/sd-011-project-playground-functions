c8d6befd536b2e0e2a5988bb02827a6fc84b6204
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-05 21:47:34 -0300

    requisito 11 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -37,7 +37,7 @@ function generatePhoneNumber(array) {
   return `(${array[0]}${array[1]})${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
 }
 
-let phoneNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 1];
+let phoneNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
 console.log(generatePhoneNumber(phoneNumber));
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
