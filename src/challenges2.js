fd07489d72ea0ddb3678d97644b650a07f5c95ac
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-04-06 11:33:33 -0400

    ajusta lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,8 @@
 // Desafio 10
 function techList(arrTech, nameDev) {
-  // seu código aqui
-  // 1.1 - Caso vazio return vazio
   if (arrTech.length === 0) {
     return 'Vazio!';
   }
-  // 1.2 - criar um novo array em ordem alfabética.
   let orderArrTech = [];
   let arrObj = [];
   for (let index = 0; index < arrTech.length; index += 1) {
@@ -17,7 +14,6 @@ function techList(arrTech, nameDev) {
       tech: orderArrTech[index],
       name: nameDev,
     };
-    // 4- criar um array de objetos pushando as infos já em ordem alfabética da variável newObj
     arrObj.push(newObj);
   }
   // 5 - retornar o array de obejtos
