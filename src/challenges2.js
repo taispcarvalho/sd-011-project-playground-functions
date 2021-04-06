4c32d159c234e9ac790719d9e405e43ec5bab3ee
OldRoge <rogeriomrjr@icloud.com>
2021-04-06 11:38:02 -0300

    Challenge 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -32,9 +32,22 @@ function triangleCheck(lineA, lineB, lineC) {
 console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(liquor = 0, beer = 0, wine = 0) {
+  let drinkWater = 0;
+  let drinks = liquor + beer + wine;
+  for (let index = 0; index < drinks; index += 1) {
+    drinkWater = drinks;
+  }
+  if (drinkWater > 1) {
+    drinkWater += ' copos de água';
+  } else {
+    drinkWater += ' copo de água';
+  }
+  return drinkWater;
 }
+console.log(hydrate(1));
+console.log(hydrate(1, 5, 1));
+console.log(hydrate(1, 7));
 
 module.exports = {
   generatePhoneNumber,
