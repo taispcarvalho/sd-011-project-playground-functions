b032314b7bb685cc41c4a181db0db415befa60fc
Júlio Mota <juliomotacampos@gmail.com>
2021-04-06 12:00:42 -0300

    Exercício Bônus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,8 +28,81 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  let answer = '';
+  let stringNumber = '';
+
+  if (checkPhoneNumberLenght(phoneNumber) === false) {
+    answer = 'Array com tamanho incorreto';
+  } else if (checkPhoneNumberDigits(phoneNumber) === false || checkPhoneNumberRepetitions(phoneNumber) === false) {
+    answer = 'Não é possível gerar um número de telefone com esses valores.'
+  } else if (checkPhoneNumberLenght(phoneNumber) === true && checkPhoneNumberDigits(phoneNumber) === true && checkPhoneNumberRepetitions(phoneNumber) === true) {
+    for (let number of phoneNumber) {
+      stringNumber += number;
+    }
+
+    stringNumber = stringNumber.replace(/(\d{2})?(\d{5})?(\d{4})/, '($1) $2-$3');
+    answer = stringNumber;
+  }
+ 
+  return answer;
+}
+
+function checkPhoneNumberLenght(phoneNumber) {
+  let isValid = false;
+
+  if (phoneNumber.length !== 11) {
+    isValid = false;
+  } else {
+    isValid = true;
+  }
+
+  return isValid;
+}
+
+function checkPhoneNumberDigits(phoneNumber) {
+  let isValid = false;
+  let count = 0;
+
+  for (let number of phoneNumber) {
+    if (number < 0 || number > 9) {
+      count += 1;
+    }
+  }
+
+  if (count === 0) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+function checkPhoneNumberRepetitions(phoneNumber) {
+  let isValid = false;
+  let repetitions = 0;
+  let mostRepetitions = 0;
+
+  for (let number of phoneNumber) {
+    repetitions = 0;
+    
+    for (let index of phoneNumber) {
+      if (index === number) {
+        repetitions += 1;
+      }
+    }
+
+    if (repetitions > mostRepetitions) mostRepetitions = repetitions;
+  }
+
+  if (mostRepetitions < 3) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  return isValid;
 }
 
 // Desafio 12
