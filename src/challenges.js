b3794235cf941759265a1363c901f58564091d60
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-06 02:09:12 -0300

    Resolve desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -50,8 +50,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 3 === 0) {
+      newArray[i] = 'fizz';
+    } if (array[i] % 5 === 0) {
+      newArray[i] = 'buzz';
+    } if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      newArray[i] = 'fizzBuzz';
+    } if (array[i] % 3 !== 0 && array[i] % 5 !== 0){
+      newArray[i] = 'bug!';
+    }
+  }
+  return newArray
 }
 
 // Desafio 9
