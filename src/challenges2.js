cf4bd51a7d514d690b1457e801eb349cd4682b07
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-06 10:17:54 -0300

    Adiciona Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,22 +8,38 @@ const techList = techNames.sort().map((tech) => ({tech, name}));
 }
 
 // Desafio 11
+function checkNumbers (numbers) {
+  const numberRepeat = arrayOfNumbers.map((number) => {
+    let count = 0;
+    for (let index in arrayOfNumbers) {
+      if (number == arrayOfNumbers[index])
+      count +=1;
+    }
+    return count;
+  })
+  return numberRepeat
+  for (let index in arrayOfNumbers) {
+    if (number == arrayOfNumbers[index])
+    count +=1;
+  }
+  return checkNumbers;
+}
+
+
 function generatePhoneNumber(arrayOfNumbers) {
   let telefone = [];
   telefone = ('(' + arrayOfNumbers[0] + arrayOfNumbers[1] + ') ' + arrayOfNumbers[2] + arrayOfNumbers[3] + arrayOfNumbers[4] + arrayOfNumbers[5] +  arrayOfNumbers[6] + '-' + arrayOfNumbers[7] + arrayOfNumbers[8] + arrayOfNumbers[9] + arrayOfNumbers[10]);
 
   for (let index in arrayOfNumbers) {
-    if (arrayOfNumbers[index] < 0 || arrayOfNumbers[index] > 9) {
+    if (arrayOfNumbers[index] < 0 || arrayOfNumbers[index] > 9 || numberRepeat >= 3) {
       return 'Não é possível gerar um número de telefone com esses valores.'
     } 
-  }
-  
+  }  
+
   if (arrayOfNumbers.length === 11) {
-    false
     return telefone;
   } 
   if (arrayOfNumbers.length !== 11) { 
-    false
     return 'Array com tamanho incorreto.'
   }
 }
@@ -34,9 +50,26 @@ console.log(generatePhoneNumber([1, 2, 3, 4, 5, 11, 7, 8, 9, 0, 1]));
 console.log(generatePhoneNumber([1, 2, 5, 5, 5, 5, 5, 8, 9, 0, 1]));
     
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  const checkSumA = lineB + lineC;
+  const checkDiffA = Math.abs(lineB - lineC);
+  const checkSumB = lineA + lineC;
+  const checkDiffB = Math.abs(lineA - lineC);
+  const checkSumC = lineB + lineA;
+  const checkDiffC = Math.abs(lineB - lineA);
+
+  if (checkSumA && checkDiffA) {
+    return true
+  };
+  if (checkSumB && checkDiffB) {
+    return true
+  };
+  if (checkSumC && checkDiffC) {
+    return true
+  }
+  return false
 }
+console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
