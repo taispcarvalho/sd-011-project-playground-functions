85912e15b2c545daeba8aa180cf5330f9152260e
Luana Souza <luanah.souzza@hotmail.com>
2021-04-05 22:31:36 -0300

    Resolvendo funçao de caça ao rato

--- src/challenges.js
+++ src/challenges.js
@@ -57,8 +57,15 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+  if (dist1 > dist2) {
+    return "cat2";
+  } else if (dist2 > dist1) {
+    return "cat1";
+  }
+  return "os gatos trombam e o rato foge";
 }
 
 // Desafio 8
