912ed6d284fe71d9264b1e1098f7781d216ebda9
Caio-Veloso <caio.veloso@gmail.com>
2021-04-06 09:46:11 -0300

    Resolução da questão vogal para número e vice-versa

--- src/challenges.js
+++ src/challenges.js
@@ -129,13 +129,28 @@ function fizzBuzz(numbers) {
  return array;
 }
 console.log(fizzBuzz([2, 15, 7, 9, 45]))
+
 // Desafio 9
-function encode() {
-  // seu código aqui
+
+function encode(string) {
+  string = string.replace(/a/g, '1');
+  string = string.replace(/e/g, '2');
+  string = string.replace(/i/g, '3');
+  string = string.replace(/o/g, '4');
+  string = string.replace(/u/g, '5');
+  return string;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  string = string.replace(/1/g, 'a');
+  string = string.replace(/2/g, 'e');
+  string = string.replace(/3/g, 'i');
+  string = string.replace(/4/g, 'o');
+  string = string.replace(/5/g, 'u');
+  return string;
 }
+
+console.log(encode('hi there!'))
+console.log(decode('h3 th2r2!'))
 
 module.exports = {
   calcArea,
