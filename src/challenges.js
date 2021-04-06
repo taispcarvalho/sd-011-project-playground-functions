fbaced7cd45fa149fc8566fc6b3708a4db0b8533
Isabella Antunes <bella.antunes2@gmail.com>
2021-04-06 13:50:35 -0300

    Mais uma tentativa de corrigir o exercício 7

--- src/challenges.js
+++ src/challenges.js
@@ -44,13 +44,12 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = Math.abs(cat1 - mouse);
   let distanceCat2 = Math.abs(cat2 - mouse);
 
-  if ((distanceCat1) > (distanceCat2)) {
-    return 'cat1';
-  } else if ((distanceCat1) < (distanceCat2)) {
-    return 'cat2';
-  } else {
+  if (distanceCat1 === distanceCat2) {
     return 'os gatos trombam e o rato foge';
+  } else if (distanceCat1 < distanceCat2) {
+    return 'cat1';
   }
+  return 'cat2';
 }
 
 // Desafio 8
