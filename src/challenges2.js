291814a0af3d4d09d4d4f2f451890e43692bda02
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-05 23:26:55 -0300

    Corrindo erros no arquivo challenges2

--- src/challenges2.js
+++ src/challenges2.js
@@ -27,9 +27,21 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-
+function hydrate(drinks) {
+  let totalWater = 0;
+  let numberDrinks = drinks.match(/\d+/g).map(Number);
+  for (let index = 0; index < numberDrinks.length; index += 1) {
+    totalWater += numberDrinks[index];
+  }
+  if (totalWater === 1) {
+    totalWater += ' copo de água';
+  }
+  if (totalWater > 1) {
+    totalWater += ' copos de água';
+  }
+  return totalWater;
 }
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
