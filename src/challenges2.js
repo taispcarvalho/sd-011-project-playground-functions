ee8135d22dbb50f4a3b952140540b2274502750e
AlyssonSene <alysson_silva@outlook.com>
2021-04-06 15:56:59 -0300

    challenge 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,23 @@
 // Desafio 10
 function techList() {
-  // seu código aqui
+  
 }
-
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+  
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA + lineB > lineC && lineB + lineC > lineA && lineA + lineC > lineB) {
+    return true;
+  } else if (Math.abs(lineB - lineC) < lineA && Math.abs(lineA - lineC) < lineB && Math.abs(lineA - lineB) < lineC) {
+    return true;
+  }
+  return false;
 }
+console.log(triangleCheck(5, 1, 2));
 
 // Desafio 13
 function hydrate() {
