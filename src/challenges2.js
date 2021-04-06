ce3c6c72a446487a104311670807444dfb16d4d1
Genivaldo <cgenivaldo46@gmail.com>
2021-04-06 07:19:40 -0300

    Exercicio10 -Primeiro sempre esta errado rs

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,23 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
+  let guardaValor;
+  if (array.length === 0) {
+    guardaValor = 'Vazio';
+  } else {
+    guardaValor = [];
+    array = array.sort();
+    for (let index = 0; index < array.length; index += 1) {
+      guardaValor.push(
+        { tech: array[index], name },
+      );
+    }
+    return array;
+  }
 }
-
+let array = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+let name = 'lucas';
+techList(array, name);
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
